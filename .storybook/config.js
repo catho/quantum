e9f58d2283527e2d14f0f5b54f76513f39aa907a
Allysson Santos <allysson.santos@catho.com>
2018-03-27 08:43:08 -0300

    Add knob plugin and use on Header Component

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,8 @@
 import { configure } from '@storybook/react';
+import 'babel-polyfill';
 
 // automatically import all files ending in *.stories.js
-const req = require.context('../stories', true, /.stories.js$/);
+const req = require.context('../components', true, /.story.jsx?$/);
 function loadStories() {
   req.keys().forEach((filename) => req(filename));
 }
--- components/Header/Header.story.jsx
+++ components/Header/Header.story.jsx
@@ -3,41 +3,26 @@ import { storiesOf, action } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
 import Header from './Header';
 import data from './data';
+import { withKnobs, select } from '@storybook/addon-knobs/react';
 
 const stories = storiesOf('Header', module);
+stories.addDecorator(withKnobs);
+
+const label = 'Path';
+const options = {
+  '/': 'candidatos',
+  'https://www.catho.com.br/empresa/': 'empresas',
+  'https://www.catho.com.br/educacao/': 'cursos',
+};
+const defaultValue = '/';
+const groupId = 'GROUP-ID1';
 
 stories.add(
   'Header component',
   withInfo('This is a basic usage of the Header component')(() => (
     <Header
       data={data}
-      handleMenuOpen={action('open menu')}
-      handleLoginOpen={action('openLogin')}
-      path="/"
-    />)),
-);
-
-stories.add(
-  'Header component with login opened',
-  withInfo('Header component with login opened')(() => (
-    <Header
-      data={data}
-      handleMenuOpen={action('open menu')}
-      handleLoginOpen={action('openLogin')}
-      loginOpened
-      path="/"
-    />)),
-);
-
-stories.add(
-  'Header component with menu opened',
-  withInfo('Header component with menu opened')(() => (
-    <Header
-      data={data}
-      menuOpened
-      path="/"
-      handleMenuOpen={action('open menu')}
-      handleLoginOpen={action('openLogin')}
+      path={select(label, options, defaultValue, groupId)}
     />)),
 );
 
