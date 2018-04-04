fcbdca19d411c592a9a1a4b557a912ffc48269d6
gabrieldaltoso <gabriel.daltoso@catho.com>
2018-04-04 10:37:41 -0300

    add preview pane

--- .storybook/decorators/Props.jsx
+++ .storybook/decorators/Props.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { Container, Col, Row } from '../../components/Grid';
 
 const wrap = name => children =>
   <span>{name} [{children}]</span>;
@@ -52,41 +53,62 @@ const renderPropType = (type = {}) => {
   return <span>{type.name}</span>;
 };
 
-const Props = ({ component }) => {
+const Preview = styled.div`
+  padding: 60px 20px;
+  margin: auto;
+  border: 1px solid #ccc;
 
-  if (!component.__docgenInfo) {
+  background-color: #eee;
+  background-image: url("https://www.transparenttextures.com/patterns/az-subtle.png");
+`;
+
+const Props = ({ component: Component }) => {
+
+  if (!Component.__docgenInfo) {
     return null;
   }
 
-  const { __docgenInfo: { props } } = component;
+  const { __docgenInfo: { props } } = Component;
 
   return (
     <React.Fragment>
-      <h2>Props</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Type</th>
-            <th>Default</th>
-            <th>Required</th>
-            <th>Description</th>
-          </tr>
-        </thead>
-        <tbody>
-        {
-          Object.entries(props).map(([name, value], i) => (
-            <tr key={i}>
-              <td>{ name }</td>
-              <td>{ renderPropType(value.type) }</td>
-              <td>{ value.defaultValue && value.defaultValue.value }</td>
-              <td>{ value.required && 'Required' }</td>
-              <td>{ value.description }</td>
-            </tr>
-          ))
-        }
-        </tbody>
-      </table>
+      <Container fluid>
+        <Row>
+          <Col desktop="6">
+          <h2>Props</h2>
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Type</th>
+                <th>Default</th>
+                <th>Required</th>
+                <th>Description</th>
+              </tr>
+            </thead>
+            <tbody>
+            {
+              Object.entries(props).map(([name, value], i) => (
+                <tr key={i}>
+                  <td>{ name }</td>
+                  <td>{ renderPropType(value.type) }</td>
+                  <td>{ value.defaultValue && value.defaultValue.value }</td>
+                  <td>{ value.required && 'Required' }</td>
+                  <td>{ value.description }</td>
+                </tr>
+              ))
+            }
+            </tbody>
+          </table>
+          </Col>
+          <Col desktop="6">
+            <h2>Preview</h2>
+            <Preview>
+              <Component />
+            </Preview>
+          </Col>
+        </Row>
+      </Container>
     </React.Fragment>
     )
 }
