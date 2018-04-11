861bf3df52c244ab01a8a95d520c1bc58173ba27
Daniel Silva <daniel5055@gmail.com>
2018-04-11 15:46:13 -0300

    Working in loadgin

--- components/Loading/Loading.jsx
+++ components/Loading/Loading.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
 import theme from '../../theme';
+import Colors from '../Colors';
+
+const animationDuration = '0.4s';
+
+const Overlay = styled.div`
+  // position: fixed;
+  // top: 0;
+  // left: 0;
+  // right: 0;
+  // bottom: 0;
+  display: flex;
+  align-items: flex-end;
+  justify-content: center;
+  background: rgba(${Colors.NEUTRAL.GRAY.WHITE}, .8);
+  z-index: 10;
+  // opacity: 0;
+  animation-duration: ${animationDuration};
+  animation-fill-mode: forwards;
+`;
 
 const LoadingWrapper = styled.div`
   ${theme.mixins.transition()};
@@ -9,26 +29,105 @@ const LoadingWrapper = styled.div`
   width: ${theme.sizes.loading};
   height: ${theme.sizes.loading};
   border-radius: ${theme.sizes.loading};
-  animation-duration: 0.4s;
+  animation-duration: ${animationDuration};
   animation-fill-mode: forwards;
 
-  &.in {
+  ${props => props.in && `
     animation-name: loader-in;
+  `}
+
+  ${props => props.out && `
+    animation-name: loader-out;
+  `}
+
+  @keyframes loader-in {
+    0% {
+      transform: scale(0);
+    }
+
+    60% {
+      transform: scale(1.2);
+    }
   }
 
-  &.out {
-    animation-name: loader-out;
+  @keyframes loader-out {
+    from {
+      transform: scale(1);
+    }
+
+    to {
+      transform: scale(0);
+      opacity: 0;
+    }
   }
 `;
 
 const StyledSvg = styled.svg`
   animation: rotate 2s linear infinite;
+
+  @keyframes rotate {
+    to {
+      transform: rotate(360deg);
+    }
+  }
 `;
 
-export default () => (
-  <LoadingWrapper>
-    <StyledSvg className="circular-loader" viewBox="25 25 50 50" >
-      <circle className="loader-path" cx="50" cy="50" r="12" />
-    </StyledSvg>
-  </LoadingWrapper>
+const StyledCircle = styled.circle`
+  fill: none;
+  stroke: ${Colors.PRIMARY.BLUE.PEACOCK};
+  stroke-width: 3;
+  stroke-dasharray: 110, 200;
+  stroke-dashoffset: -10;
+  animation: dash 2s ease-in-out infinite;
+  stroke-linecap: round;
+
+  @keyframes dash {
+    0% {
+      stroke-dasharray: 1, 200;
+      stroke-dashoffset: 0;
+    }
+
+    50% {
+      stroke-dasharray: 110, 200;
+      stroke-dashoffset: -15;
+    }
+
+    100% {
+      stroke-dasharray: 110, 200;
+      stroke-dashoffset: -72;
+    }
+  }
+`;
+
+const Loading = ({
+  viewBox,
+  cx,
+  cy,
+  r,
+}) => (
+  <Overlay>
+    <LoadingWrapper>
+      <StyledSvg viewBox={viewBox} >
+        <StyledCircle cx={cx} cy={cy} r={r} />
+      </StyledSvg>
+    </LoadingWrapper>
+  </Overlay>
 );
+
+Loading.defaultProps = {
+  viewBox: '25 25 50 50',
+  cx: '50',
+  cy: '50',
+  r: '12',
+  active: true,
+};
+
+Loading.propTypes = {
+  viewBox: PropTypes.string,
+  cx: PropTypes.string,
+  cy: PropTypes.string,
+  r: PropTypes.string,
+  active: PropTypes.bool,
+};
+
+export default Loading;
--- components/Loading/Loading.story.jsx
+++ components/Loading/Loading.story.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import styled from 'styled-components';
 import HowToImport from '../../.storybook/decorators/HowToImport';
 import Loading from './Loading';
+
+const StyledContainer = styled.div`
+  // background-color: yellow;
+`;
 
 storiesOf('6. Loading', module)
   .addDecorator(HowToImport)
   .add('Loading', () => (
-    <React.Fragment>
+    <StyledContainer>
       <Loading />
-    </React.Fragment>
+    </StyledContainer>
   ));
