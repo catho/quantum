b1ebc8c6264b97dffc4ea76c5ce27043bb1e13da
gabrieldaltoso <gabriel.daltoso@catho.com>
2018-04-11 11:56:25 -0300

    fix lint HowToImport

--- .storybook/components/HowToImport/HowToImport.jsx
+++ .storybook/components/HowToImport/HowToImport.jsx
@@ -1,29 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import Highlight from 'react-highlight';
 import CodeToClipboard from '../CodeToClipboard';
 import '../../../stories/static/styleguide-highlight.css';
 
-const HowToImport = ({importModules}) => {
-  const msg = `import ${'{'} ${ importModules } ${'}'} from '@cathodevel/style-guide';`.toString();
-
-  return (
-  <React.Fragment>
-    <h2>Import</h2>
-
-    <HighlightWrapper>
-      <Highlight language="javascript" className="highlight">
-        {msg}
-      </Highlight>
-      
-      <CodeToClipboard code={msg} />
-    </HighlightWrapper>
-  </React.Fragment>
-  )
-};
-
 const HighlightWrapper = styled.div`
   position:relative;
-`
+`;
+
+const HowToImport = ({ importModules }) => {
+  const msg = `import ${'{'} ${importModules} ${'}'} from '@cathodevel/style-guide';`;
+
+  return (
+    <React.Fragment>
+      <h2>Import</h2>
+
+      <HighlightWrapper>
+        <Highlight language="javascript" className="highlight">
+          {msg}
+        </Highlight>
+
+        <CodeToClipboard code={msg} />
+      </HighlightWrapper>
+    </React.Fragment>
+  );
+};
+
+HowToImport.propTypes = {
+  importModules: PropTypes.string.isRequired,
+};
 
 export default HowToImport;
