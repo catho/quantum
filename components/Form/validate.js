96f37bed911ad65936c6e1af017fdda0386b7f43
Daniel Silva <daniel5055@gmail.com>
2018-04-23 11:14:27 -0300

    CPF validate fix

--- components/Form/validate.js
+++ components/Form/validate.js
@@ -12,18 +12,16 @@ const msg = {
 };
 
 const validate = {
-  CPF: (value) => {
-    const val = trimString(value);
-    const errorMsg = 'Deu milho no CPF';
+  CPF: ({ value }, trimed = trimString(value)) => {
     const cpfSize = 11;
 
-    if (!val || val.length !== cpfSize) {
-      return errorMsg;
+    if (!trimed || trimed.length !== cpfSize) {
+      return msg.CPF;
     }
 
     for (let i = 0; i < 10; i += 1) {
-      if (val === String(i).repeat(cpfSize)) {
-        return errorMsg;
+      if (trimed === String(i).repeat(cpfSize)) {
+        return msg.CPF;
       }
     }
 
@@ -32,22 +30,18 @@ const validate = {
       let rev;
 
       for (let i = 0; i < position; i += 1) {
-        sum += Number(val.charAt(i)) * ((position + 1) - i);
+        sum += Number(trimed.charAt(i)) * ((position + 1) - i);
       }
 
       rev = cpfSize - (sum % cpfSize);
       rev = (rev === 10 || rev === cpfSize) ? 0 : rev;
 
-      return rev === Number(val.charAt(position));
+      return rev === Number(trimed.charAt(position));
     };
 
-    if (!validPosition(9) || !validPosition(10)) {
-      return errorMsg;
-    }
-
-    return '';
+    return validPosition(9) && validPosition(10) ? '' : msg.CPF;
   },
-  CEP: (value) => {
+  CEP: ({ value }) => {
     if (String(trimString(value)).length !== 8) {
       return 'Deu milho no CEP';
     }
