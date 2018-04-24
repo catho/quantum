e02c1598c64bfa6d6f7de39c24bacdaca61d357d
Daniel Silva <daniel5055@gmail.com>
2018-04-24 11:02:04 -0300

    Refactor tests

--- components/Form/Form.unit.test.jsx
+++ components/Form/Form.unit.test.jsx
@@ -10,37 +10,42 @@ const onValidSubmitCallback = jest.fn();
 const onSubmitCallback = jest.fn();
 const mockEvent = { preventDefault: jest.fn() };
 
-const formWithoutValidations = (
-  <Form onValidSubmit={onValidSubmitCallback} onSubmit={onSubmitCallback}>
-    <Form.Input name="name" label="Name" />
-  </Form>
-);
+const FormWithoutValidations = () => {
+  return (
+    <Form onValidSubmit={onValidSubmitCallback} onSubmit={onSubmitCallback}>
+      <Form.Input name="name" label="Name" />
+    </Form>
+  )
+};
 
-const formWithValidations = (
-  <Form onValidSubmit={onValidSubmitCallback} onSubmit={onSubmitCallback}>
-    <Form.Input name="name" label="Name" validate={validations.REQUIRED} />
-    <Form.Input.CPF name="cpf" label="CPF" validate={validations.CPF} />
-    <Form.Input.CEP name="cep" label="CEP" validate={validations.CEP} />
-    <Form.Input.Date name="birthday" label="Birthday" validate={validations.DATE} />
-    <Form.Input name="email" label="E-mail" validate={validations.EMAIL} />
-    <Form.Input name="address" label="Address" validate={validations.MINLENGTH} minLength={8} />
-    <Form.Input name="country" label="Country" validate={validations.MAXLENGTH} minLength={3} />
-    <Form.Submit />
-  </Form>
-);
+const FormWithValidations = () => {
+  return (
+    <Form onValidSubmit={onValidSubmitCallback} onSubmit={onSubmitCallback}>
+      <Form.Input name="name" label="Name" validate={validations.REQUIRED} />
+      <Form.Input.CPF name="cpf" label="CPF" validate={validations.CPF} />
+      <Form.Input.CEP name="cep" label="CEP" validate={validations.CEP} />
+      <Form.Input.Date name="birthday" label="Birthday" validate={validations.DATE} />
+      <Form.Input name="email" label="E-mail" validate={validations.EMAIL} />
+      <Form.Input name="address" label="Address" validate={validations.MINLENGTH} minLength={8} />
+      <Form.Input name="country" label="Country" validate={validations.MAXLENGTH} minLength={3} />
+      <Form.Submit />
+    </Form>
+  )
+};
+
+beforeEach(() => {
+  onValidSubmitCallback.mockClear();
+  onSubmitCallback.mockClear();
+});
 
 describe('Form component ', () => {
-  beforeEach(() => {
-    onValidSubmitCallback.mockClear();
-  });
-
   it('should match the snapshot', () => {
-    expect(renderer.create(formWithValidations).toJSON()).toMatchSnapshot();
+    expect(renderer.create(<FormWithValidations />).toJSON()).toMatchSnapshot();
   });
 
   describe('Submit without validations', () => {
     it('Shoud call "onValidSubmit" and "onSubmit" callback on a valid submit', () => {
-      const wrapper = shallow(formWithoutValidations);
+      const wrapper = shallow(<FormWithoutValidations />);
       wrapper.simulate('submit', mockEvent);
 
       expect(onValidSubmitCallback).toHaveBeenCalled();
@@ -49,7 +54,7 @@ describe('Form component ', () => {
   });
 
   describe('Submit With validations', () => {
-    const wrapper = shallow(formWithValidations);
+    const wrapper = shallow(<FormWithValidations />);
 
     it('Shoudn\'t call "onValidSubmit" and "onSubmit" callback on a invalid submit', () => {
       wrapper.simulate('submit', mockEvent);
@@ -57,6 +62,17 @@ describe('Form component ', () => {
       expect(onValidSubmitCallback).not.toHaveBeenCalled();
       expect(onSubmitCallback).toHaveBeenCalled();
     });
+
+    it('Shoud validate Required fields', () => {
+
+    });
+
+    it('Shoud validate CPF fields');
+    it('Shoud validate CEP fields');
+    it('Shoud validate DATE fields');
+    it('Shoud validate EMAIL fields');
+    it('Shoud validate fields with MAXLENGTH');
+    it('Shoud validate fields with MINLENGTH');
   });
 });
 
