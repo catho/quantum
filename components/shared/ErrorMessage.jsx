30b742531869ae42738cb5a2c0f24a032acf4bcc
Allysson Santos <allysson.santos@catho.com>
2018-04-30 11:19:29 -0300

    Change input styles

--- components/shared/ErrorMessage.jsx
+++ components/shared/ErrorMessage.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import Colors from '../Colors';
 
 export default styled.div`
-  color: ${Colors.DANGER['400']};
+  color: ${Colors.DANGER['500']};
   font-size: 12px;
   margin: 5px 0;
 `;
--- components/shared/FieldGroup.jsx
+++ components/shared/FieldGroup.jsx
@@ -2,5 +2,6 @@ import styled from 'styled-components';
 import theme from '../../theme';
 
 export default styled.div`
+  position: relative;
   margin-bottom: ${theme.sizes.spacing};
 `;
