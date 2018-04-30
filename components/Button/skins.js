786ea9350e11eceb6d3424eeb0c5db78d43df705
gabrieldaltoso <gabriel.daltoso@catho.com>
2018-04-30 09:28:11 -0300

    exported Models

--- components/Button/skins.js
+++ components/Button/skins.js
@@ -1,88 +1,5 @@
 import Colors from '../Colors';
-import theme from '../../theme';
-
-class ButtonColor {
-  constructor({
-    unselected,
-    unselectedBorder = unselected,
-    unselectedTextColor,
-    unselectedFontWeight = theme.font.weight,
-    selected,
-    selectedBorder = selected,
-    selectedTextColor,
-    selectedFontWeight = theme.font.weight,
-    disabled,
-    disabledBorder = disabled,
-    disabledTextColor,
-    disabledFontWeight = theme.font.weight,
-  }) {
-    this._unselected = unselected;
-    this._unselectedBorder = unselectedBorder;
-    this._unselectedTextColor = unselectedTextColor;
-    this._unselectedFontWeight = unselectedFontWeight;
-    this._selected = selected;
-    this._selectedBorder = selectedBorder;
-    this._selectedTextColor = selectedTextColor;
-    this._selectedFontWeight = selectedFontWeight;
-    this._disabled = disabled;
-    this._disabledBorder = disabledBorder;
-    this._disabledTextColor = disabledTextColor;
-    this._disabledFontWeight = disabledFontWeight;
-  }
-
-  get unselected() {
-    return {
-      background: this._unselected,
-      border: this._unselectedBorder,
-      color: this._unselectedTextColor,
-      fontWeight: this._unselectedFontWeight,
-    };
-  }
-
-  get selected() {
-    return {
-      background: this._selected,
-      border: this._selectedBorder,
-      color: this._selectedTextColor,
-      fontWeight: this._selectedFontWeight,
-    };
-  }
-
-  get disabled() {
-    return {
-      background: this._disabled,
-      border: this._disabledBorder,
-      color: this._disabledTextColor,
-      fontWeight: this._disabledFontWeight,
-    };
-  }
-}
-
-class DefaultButton extends ButtonColor {
-  constructor({
-    unselected,
-    unselectedBorder = unselected,
-    unselectedTextColor = Colors.WHITE,
-    selected,
-    selectedBorder = selected,
-    selectedTextColor = unselectedTextColor,
-    disabled,
-    disabledBorder = disabled,
-    disabledTextColor = unselectedTextColor,
-  }) {
-    super({
-      unselected,
-      unselectedBorder,
-      unselectedTextColor,
-      selected,
-      selectedBorder,
-      selectedTextColor,
-      disabled,
-      disabledBorder,
-      disabledTextColor,
-    });
-  }
-}
+import DefaultButton from './models/DefaultButton';
 
 const skins = {
   default: new DefaultButton({
@@ -122,6 +39,5 @@ const skins = {
     disabledTextColor: Colors.SECONDARY['200'],
   }),
 };
-
 
 export default ({ skin }) => skins[skin] || skins.default;
