40bfb3569c190ecdd86a2bf86a0b9815c386ff79
Allysson Santos <allysson.santos@catho.com>
2018-05-02 16:31:45 -0300

    Create new tests to input

--- components/Input/Input.unit.test.jsx
+++ components/Input/Input.unit.test.jsx
@@ -12,6 +12,10 @@ describe('Input component ', () => {
     expect(renderer.create(<Input value="foo" error="Error message" />).toJSON()).toMatchSnapshot();
   });
 
+  it('with a passwordLink prop should match the snapshot', () => {
+    expect(renderer.create(<Input value="foo" passwordLink="http://www.catho.com.br/" />).toJSON()).toMatchSnapshot();
+  });
+
   describe('with a label', () => {
     const input = <Input label="Text label" id="input-with-label" value="foo" />;
 
@@ -19,10 +23,25 @@ describe('Input component ', () => {
       expect(renderer.create(input).toJSON()).toMatchSnapshot();
     });
 
+    it('should have withValue prop set to true when input have value', () => {
+      const wrapper = shallow(input);
+      const label = wrapper.childAt(1);
+
+      expect(label.prop('withValue')).toEqual(true);
+    });
+
+    it('should have withValue prop set to false when input doesn\'t have value', () => {
+      const inputWithoutValue = <Input label="Text label" id="withoutValue" />;
+      const wrapper = shallow(inputWithoutValue);
+      const label = wrapper.childAt(1);
+
+      expect(label.prop('withValue')).toEqual(false);
+    });
+
     it('should match label "htmlFor" label param with "id" input param', () => {
       const wrapper = shallow(input);
-      const label = wrapper.childAt(0);
-      const styledInput = wrapper.childAt(1);
+      const styledInput = wrapper.childAt(0);
+      const label = wrapper.childAt(1);
 
       expect(label.prop('htmlFor')).toEqual(styledInput.prop('id'));
     });
