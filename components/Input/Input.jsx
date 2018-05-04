7ef2379dc9dbf837169d3fb75f11e646d10a6667
Daniel Silva <daniel5055@gmail.com>
2018-05-04 11:10:58 -0300

    Remove react-text-mask

--- components/Input/Input.jsx
+++ components/Input/Input.jsx
@@ -1,7 +1,6 @@
 import PropTypes from 'prop-types';
 import styled, { css } from 'styled-components';
 import React from 'react';
-import Masked from 'react-text-mask';
 import InputTypes from './InputTypes';
 
 import { ErrorMessage, Label, FieldGroup } from '../shared';
@@ -95,6 +94,12 @@ class Input extends React.Component {
     this.state = { value };
   }
 
+  componentWillUpdate(nextProps) {
+    if (nextProps.value !== this.state.value) {
+      this.state.value = nextProps.value;
+    }
+  }
+
   onChange = (e) => {
     const { onChange } = this.props;
     const { target: { value } } = e;
@@ -117,22 +122,15 @@ class Input extends React.Component {
 
     return (
       <StyledFieldGroup>
-        <Masked
+        <StyledInput
           {...rest}
           id={id}
-          onChange={this.onChange}
           passwordLink={passwordLink}
+          onChange={this.onChange}
           value={value}
-          render={
-            (ref, props) => (
-              <StyledInput
-                {...props}
-                error={error}
-                innerRef={ref}
-              />
-            )
-          }
+          error={error}
         />
+
         { label &&
           <StyledLabel
             htmlFor={id}
