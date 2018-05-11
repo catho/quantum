8cdd949725decc18f7ddd65c10135780d5aa9ed2
Alan Oliveira <aloliv@localhost.localdomain>
2018-05-11 14:25:55 -0300

    Introduction as default page

---
+++ .storybook/stories.js
@@ -0,0 +1,21 @@
+const path = './'
+
+export default [
+  'Introduction',
+  'Colors',
+  'Grid',
+  'Typography',
+  'Button',
+  'AutoComplete',
+  'Checkbox',
+  'Dropdown',
+  'Input',
+  'Radio',
+  'Range',
+  'Textarea',
+  'Notification',
+  'Modal',
+  'Loading',
+  'Toggle',
+  'Form',
+].map(s => `./${s}/${s}.story.jsx`)
