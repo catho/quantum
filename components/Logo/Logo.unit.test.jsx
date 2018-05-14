25b42fe9153bd968870297d858e8472791c8f46f
Allysson Santos <allysson.santos@catho.com>
2018-05-14 10:33:18 -0300

    Remove useless tests

--- components/Logo/Logo.unit.test.jsx
+++ components/Logo/Logo.unit.test.jsx
@@ -7,29 +7,4 @@ describe('Logo component ', () => {
     const logo = <Logo alt="Example" />;
     expect(renderer.create(logo).toJSON()).toMatchSnapshot();
   });
-
-  it('Should match snapshot with Candidate logo', () => {
-    const logo = <Logo.Candidate />;
-    expect(renderer.create(logo).toJSON()).toMatchSnapshot();
-  });
-
-  it('Should match snapshot with Business logo', () => {
-    const logo = <Logo.Business />;
-    expect(renderer.create(logo).toJSON()).toMatchSnapshot();
-  });
-
-  it('Should match snapshot with Education logo', () => {
-    const logo = <Logo.Education />;
-    expect(renderer.create(logo).toJSON()).toMatchSnapshot();
-  });
-
-  it('Should match snapshot with Facebook logo', () => {
-    const logo = <Logo.Facebook />;
-    expect(renderer.create(logo).toJSON()).toMatchSnapshot();
-  });
-
-  it('Should match snapshot with Google logo', () => {
-    const logo = <Logo.Google />;
-    expect(renderer.create(logo).toJSON()).toMatchSnapshot();
-  });
 });
