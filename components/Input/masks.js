2957a0de0dcc480796e3ddfd80c27dd12ca405f1
Daniel Silva <daniel5055@gmail.com>
2018-05-16 12:21:45 -0300

    Tests and lint fix

--- components/Input/masks.js
+++ components/Input/masks.js
@@ -4,13 +4,4 @@ export default {
   cpf: '999.999.999-99',
   date: '99/99/9999',
   phone: '(99) 9999-99999',
-  // phone: (userInput) => {
-  //   const numbers = userInput.match(/\d/g);
-  //   const numberLength = numbers ? numbers.join('').length : 0;
-
-  //   const cel = ['(', /[1-9]/, /[1-9]/, ')', ' ', /\d/, /\d/, /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/];
-  //   const fix = ['(', /[1-9]/, /[1-9]/, ')', ' ', /\d/, /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/];
-
-  //   return numberLength > 10 ? cel : fix;
-  // },
 };
