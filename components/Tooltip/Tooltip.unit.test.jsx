ee49f9911d06ce52ffd22a3956390d005f50a7fa
Allysson dos Santos <aallysson0@gmail.com>
2018-05-22 10:28:16 -0300

    Improve story and tests

--- components/Tooltip/Tooltip.unit.test.jsx
+++ components/Tooltip/Tooltip.unit.test.jsx
@@ -3,8 +3,47 @@ import { mount } from 'enzyme';
 import Tooltip from './Tooltip';
 
 describe('Tooltip component ', () => {
-  it('Should match the snapshot', () => {
-    const tooltip = mount(<Tooltip />);
-    expect(tooltip.html()).toMatchSnapshot();
+  describe('All positions', () => {
+    it('Should match the snapshot when place is top', () => {
+      const tooltip = mount(<Tooltip place="top" />);
+      expect(tooltip.html()).toMatchSnapshot();
+    });
+
+    it('Should match the snapshot when place is right', () => {
+      const tooltip = mount(<Tooltip place="right" />);
+      expect(tooltip.html()).toMatchSnapshot();
+    });
+
+    it('Should match the snapshot when place is bottom', () => {
+      const tooltip = mount(<Tooltip place="bottom" />);
+      expect(tooltip.html()).toMatchSnapshot();
+    });
+
+    it('Should match the snapshot when place is left', () => {
+      const tooltip = mount(<Tooltip place="left" />);
+      expect(tooltip.html()).toMatchSnapshot();
+    });
+  });
+
+  describe('All skins', () => {
+    it('Should match the snapshot skin is info', () => {
+      const tooltip = mount(<Tooltip skin="info" />);
+      expect(tooltip.html()).toMatchSnapshot();
+    });
+
+    it('Should match the snapshot when skin is danger', () => {
+      const tooltip = mount(<Tooltip skin="danger" />);
+      expect(tooltip.html()).toMatchSnapshot();
+    });
+
+    it('Should match the snapshot when skin is success', () => {
+      const tooltip = mount(<Tooltip skin="success" />);
+      expect(tooltip.html()).toMatchSnapshot();
+    });
+
+    it('Should match the snapshot when skin is warning', () => {
+      const tooltip = mount(<Tooltip skin="warning" />);
+      expect(tooltip.html()).toMatchSnapshot();
+    });
   });
 });
--- stories/Tooltip/Tooltip.story.jsx
+++ stories/Tooltip/Tooltip.story.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import AutoExample from '../../.storybook/components/AutoExample';
+import Example from '../../.storybook/components/Example';
+import { Tab } from '../../.storybook/components/TabbedView';
 import Tooltip from '../../components/Tooltip';
+
+const tabExample = (
+  <Tab title="Example">
+    <h3>Skins</h3>
+    <div>
+      <Example component={<Tooltip skin="info">Info</Tooltip>} />
+      <Example component={<Tooltip skin="danger">Danger</Tooltip>} />
+      <Example component={<Tooltip skin="success">Success</Tooltip>} />
+      <Example component={<Tooltip skin="warning">Warning</Tooltip>} />
+    </div>
+
+    <h3>Placement</h3>
+    <div>
+      <Example component={<Tooltip place="top">Top</Tooltip>} />
+      <Example component={<Tooltip place="right">Right</Tooltip>} />
+      <Example component={<Tooltip place="bottom">Bottom</Tooltip>} />
+      <Example component={<Tooltip place="left">Left</Tooltip>} />
+    </div>
+  </Tab>
+);
 
 storiesOf('9. Tooltip', module)
   .add('Basic', () => (
-    <AutoExample component={Tooltip} />
+    <AutoExample
+      component={Tooltip}
+      additionalTabs={tabExample}
+    />
   ));
