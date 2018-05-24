17d6d6412a0138a7561b3af5d1db4370d3b10539
Daniel Silva <daniel5055@gmail.com>
2018-05-24 17:19:05 -0300

    Add icon prop doc

--- components/Button/Button.jsx
+++ components/Button/Button.jsx
@@ -126,6 +126,8 @@ Button.propTypes = {
   center: PropTypes.bool,
   disabled: PropTypes.bool,
   full: PropTypes.bool,
+  /** Icon name. The full catalogue can be found
+   * [here](/?selectedKind=1.%20Foundation&selectedStory=Icons") */
   icon: PropTypes.string,
   size: PropTypes.oneOf([
     'normal',
