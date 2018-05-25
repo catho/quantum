8c0851821f2afef0279b97d6bf285ca8eafe9ae6
Daniel Silva <daniel5055@gmail.com>
2018-05-25 11:58:46 -0300

    Introduction title

--- stories/Introduction/Introduction.story.jsx
+++ stories/Introduction/Introduction.story.jsx
@@ -4,7 +4,7 @@ import Introduction from './Introduction';
 import Heading from '../../.storybook/components/Heading';
 import Logo from '../static/logo.svg';
 
-storiesOf(' Introduction', module)
+storiesOf('Introduction', module)
   .add('Getting started', () => (
     <Heading image={Logo} title="StyleGuide">
       <Introduction />
