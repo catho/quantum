006602dd16c215fbf2ba34808b7ae561e0ff8828
Marcella Calixto <marcella.calixto@catho.com>
2018-05-25 10:19:04 -0300

    Style adjustment steps

--- components/Steps/Steps.jsx
+++ components/Steps/Steps.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-import Item from './sub-components/Item';
+import Step from './sub-components/Step';
 
 const Container = styled.div`
   padding: 0;
@@ -18,19 +18,22 @@ const ProgressBar = styled.ul`
       position: relative;
       text-align: center;
       color: #7d7d7d;
+      margin-top: 50px;
   }
+
   li:before {
-      width: 32px;
-      height: 32px;
+      width: 36px;
+      height: 36px;
       content: '';
       line-height: 30px;
-      border: 2px solid #a0a0a0;
+      border: 1px solid #cccccc;
       display: block;
       text-align: center;
       margin: 0 auto 10px auto;
       border-radius: 50%;
       background-color: white;
   }
+
    li:after {
       width: 100%;
       height: 1px;
@@ -41,35 +44,43 @@ const ProgressBar = styled.ul`
       left: -50%;
       z-index: -1;
   }
+
   li:first-child:after {
       content: none;
   }
+
   li.active {
-      color: #404040;
+      color: #005da4;      
   }
+
   li.active:before {
-      border-color: #165ea3;
+      background: #005da4;
+      border: 1px solid #005da4;
   }
+
   li.active + li:after {
       background-color: #165ea3;
   }
+
   li p {
     position: relative;
-    top: -60px;
+    top: -80px;
+    width: 90px;
+    margin: 0 auto;
   }
 `;
 
 const stepList = [
-  { title: 'Step 1', active: true, icon: '1' },
-  { title: 'Step 2', active: true, icon: '2' },
-  { title: 'Step 3', active: false, icon: '3' },
+  { title: 'Criação de conta', active: true, icon: '1' },
+  { title: 'Plano e forma de pagamento', active: true, icon: '2' },
+  { title: 'Preencher o currículo', active: false, icon: '3' },
 ];
 
 const Steps = () => (
   <React.Fragment>
     <Container>
       <ProgressBar>
-        {stepList.map(step => <Item {...step} />)}
+        {stepList.map(step => <Step {...step} />)}
       </ProgressBar>
     </Container>
   </React.Fragment>
--- components/Steps/sub-components/Step.jsx
+++ components/Steps/sub-components/Step.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const StepsSubComponent = () => (
-  <React.Fragment>
-    Sub component here!
-  </React.Fragment>
-);
+const Step = ({ title, active }) => {
+  const isActive = active ? 'active' : '';
 
-export default StepsSubComponent;
+  return (
+    <React.Fragment>
+      <li className={isActive}>
+        <p>{title}</p>
+      </li>
+    </React.Fragment>
+  );
+};
+
+Step.propTypes = {
+  title: PropTypes.string.isRequired,
+  // icon: PropTypes.string,
+  active: PropTypes.bool,
+};
+
+Step.defaultProps = {
+  active: false,
+};
+
+export default Step;
