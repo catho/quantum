c27f16e0239512477628a6b3bff5a90f6adb9be5
gabrieldaltoso <gabriel.daltoso@catho.com>
2018-05-25 09:26:32 -0300

    add style props to Icons

--- components/Icon/Icon.jsx
+++ components/Icon/Icon.jsx
@@ -3,16 +3,23 @@ import PropTypes from 'prop-types';
 import Icon from '@material-ui/core/Icon';
 import icons from '../shared/icons';
 
-const FontIcon = ({ name, skin, ...props }) => (
-  <Icon {...props} style={{ color: skin }}>{name}</Icon>
+const FontIcon = ({
+  name,
+  skin,
+  style,
+  ...props
+}) => (
+  <Icon {...props} style={{ ...style, color: skin }}>{name}</Icon>
 );
 
 FontIcon.defaultProps = {
+  style: {},
   skin: '',
 };
 
 FontIcon.propTypes = {
   name: PropTypes.oneOf(icons).isRequired,
+  style: PropTypes.instanceOf(Object),
   skin: PropTypes.string,
 };
 
