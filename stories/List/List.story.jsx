ded6ea96f92fbdb5b5430b60017e549c7621ac53
gabrieldaltoso <gabriel.daltoso@catho.com>
2018-06-05 16:15:46 -0300

    fix List story

--- stories/List/List.story.jsx
+++ stories/List/List.story.jsx
@@ -55,7 +55,9 @@ storiesOf('10. List', module).add('List', () => (
         </Row>
         <Row>
           <Col desktop={12} tablet={12}>
-            <p>Also, you can add some properties to the list such as <code>ordered</code>, <code>divided</code>, <code>bullet</code>, and <code>inline</code></p>
+            <p>Also, you can add some properties to the list such as
+              <code>ordered</code>, <code>divided</code>, <code>bullet</code>, and <code>inline</code>
+            </p>
           </Col>
 
           <Col tablet={4}>
