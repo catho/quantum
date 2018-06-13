c0a8ff4892a951d19121a02e46a2cc2c3e0f49a6
ggdaltoso <ggdaltoso@gmail.com>
2018-06-13 14:13:13 -0300

    fix arrow position

--- components/Tooltip/options.js
+++ components/Tooltip/options.js
@@ -1,16 +1,16 @@
 import Colors from '../Colors';
 
-const arrowSize = '5px';
+const arrowSize = 5;
 const upDownBorders = `
-  border-left: ${arrowSize} solid transparent;
-  border-right: ${arrowSize} solid transparent;
+  border-left: ${arrowSize}px solid transparent;
+  border-right: ${arrowSize}px solid transparent;
   left: 50%;
   transform: translateX(-50%);
 `;
 
 const sideBorders = `
-  border-top: ${arrowSize} solid transparent;
-  border-bottom: ${arrowSize} solid transparent;
+  border-top: ${arrowSize}px solid transparent;
+  border-bottom: ${arrowSize}px solid transparent;
   top: 50%;
   transform: translateY(-50%);
 `;
@@ -19,33 +19,33 @@ const placement = {
   arrowPosition: {
     bottom: `
       ${upDownBorders}
-      border-bottom: ${arrowSize} solid;
+      border-bottom: ${arrowSize}px solid;
       border-bottom-color: inherit;
-      top: -${arrowSize};
+      top: -${arrowSize - 1}px;
     `,
     top: `
       ${upDownBorders}
-      border-top: ${arrowSize} solid;
+      border-top: ${arrowSize}px solid;
       border-top-color: inherit;
-      bottom: -${arrowSize};
+      bottom: -${arrowSize - 1}px;
     `,
     left: `
       ${sideBorders}
-      border-left: ${arrowSize} solid;
+      border-left: ${arrowSize}px solid;
       border-left-color: inherit;
-      right: -${arrowSize};
+      right: -${arrowSize - 1}px;
     `,
     right: `
       ${sideBorders}
-      border-right: ${arrowSize} solid;
+      border-right: ${arrowSize}px solid;
       border-right-color: inherit;
-      left: -${arrowSize};
+      left: -${arrowSize - 1}px;
     `,
   },
 
   tipPosition: ({ place, height, width }) => {
     const position = {
-      top: `top: -${height + 10}px; left: 50%; margin-left: -${Math.floor(width / 2)}px;`,
+      top: `top: -${height + 5}px; left: 50%; margin-left: -${Math.floor(width / 2)}px;`,
       right: `right: -${width + 15}px;top: 50%; margin-top: -${Math.floor(height / 2)}px;`,
       bottom: `bottom: -${height + 10}px; left: 50%; margin-left: -${Math.floor(width / 2)}px;`,
       left: `left: -${width + 15}px;top: 50%; margin-top: -${Math.floor(height / 2)}px;`,
