9b32c1fe8ccb8b7ea99f3cec3f1ee401e5cd1d8c
Allysson Santos <allysson.santos@catho.com>
2018-06-26 10:42:06 -0300

    Refactor theme.js

--- theme.js
+++ theme.js
@@ -1,13 +1,5 @@
 import Colors from './components/Colors';
 
-const hexToRgba = (color, opacity) => {
-  const R = parseInt(color.substring(1, 3), 16);
-  const G = parseInt(color.substring(3, 5), 16);
-  const B = parseInt(color.substring(5, 7), 16);
-
-  return `rgba(${R}, ${G}, ${B}, ${opacity})`;
-};
-
 export default {
   font: {
     weight: 500,
@@ -21,14 +13,21 @@ export default {
     loading: '60px',
   },
   mixins: {
-    hexToRgba,
-    transition: (property = 'all', timing = '0.2s', type = 'ease-in-out') =>
-      `transition: ${property} ${timing} ${type}`,
-    shadow: (size = 1) => {
+    hexToRgba(color, opacity) {
+      const R = parseInt(color.substring(1, 3), 16);
+      const G = parseInt(color.substring(3, 5), 16);
+      const B = parseInt(color.substring(5, 7), 16);
+
+      return `rgba(${R}, ${G}, ${B}, ${opacity})`;
+    },
+    transition(property = 'all', timing = '0.2s', type = 'ease-in-out') {
+      return `transition: ${property} ${timing} ${type}`;
+    },
+    shadow(size = 1) {
       const shadows = [
         'box-shadow: none;',
-        `box-shadow: 0 2px 5px ${hexToRgba(Colors.SECONDARY['600'], 0.3)};`,
-        `box-shadow: 0 2px 6px ${hexToRgba(Colors.SECONDARY['600'], 0.6)};`,
+        `box-shadow: 0 2px 5px ${this.hexToRgba(Colors.SECONDARY['600'], 0.3)};`,
+        `box-shadow: 0 2px 6px ${this.hexToRgba(Colors.SECONDARY['600'], 0.6)};`,
       ];
 
       return shadows[size] || shadows[1];
