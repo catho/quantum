57d6ccbe074841e263f472cbe9cfb875397ca817
Mayara Santos <mayara.santos@catho.com>
2018-06-28 11:37:38 -0300

    badge style

--- components/Badge/Badge.jsx
+++ components/Badge/Badge.jsx
@@ -1,11 +1,50 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import Colors from '../Colors';
 
-const Badge = () => <React.Fragment> Badge </React.Fragment>;
-Badge.propTypes = {
-  skin: PropTypes.oneOf(['primary', 'secondary', 'action', 'cancel', 'modal']),
+const getBgColor = ({ skin }) => {
+  const color = Colors[skin.toUpperCase()];
+  return color[500] || color;
 };
+
+const getFontColor = ({ skin }) =>
+  skin === 'white' ? Colors.BLACK : Colors.WHITE;
+
+const Wrapper = styled.div`
+  background-color: ${getBgColor};
+  border-radius: 2px;
+  color: ${getFontColor};
+  font-size: 10px;
+  padding: 2px 4px;
+  text-align: center;
+`;
+
+const Badge = ({ children, skin }) => (
+  <Wrapper skin={skin}> {children} </Wrapper>
+);
+
+Badge.propTypes = {
+  skin: PropTypes.oneOf([
+    'brand1',
+    'brand2',
+    'primary',
+    'secondary',
+    'success',
+    'action',
+    'warning',
+    'danger',
+    'info',
+    'p2p',
+    'black',
+    'white',
+  ]),
+  children: PropTypes.string,
+};
+
 Badge.defaultProps = {
   skin: 'primary',
+  children: 'Badge',
 };
+
 export default Badge;
