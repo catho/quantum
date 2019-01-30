ccb3b157a5cb1823fad19bc50adb2e4bb3e6b1c0
allyssonsantos <aallysson0@gmail.com>
2019-01-30 16:28:58 -0200

    feat(BTV-941): [WM]: Remove webpack config

---
+++ scripts/copy-files.js
@@ -0,0 +1,41 @@
+import path from 'path';
+import fs from 'fs';
+
+const copyFile = file => {
+  const buildPath = path.resolve(__dirname, '../dist/', path.basename(file));
+  fs.copyFile(file, buildPath, () =>
+    console.log(`Copied ${file} to ${buildPath}`),
+  );
+};
+
+const createPackageJson = () => {
+  const packageData = fs.readFileSync(
+    path.resolve(__dirname, '../package.json'),
+    'utf8',
+  );
+  const {
+    scripts,
+    devDependencies,
+    'lint-staged': lintStaged,
+    jest,
+    ...packageDataOther
+  } = JSON.parse(packageData);
+  const newPackageData = {
+    ...packageDataOther,
+    main: './index.js',
+    private: false,
+  };
+  const buildPath = path.resolve(__dirname, '../dist/package.json');
+
+  fs.writeFileSync(buildPath, JSON.stringify(newPackageData, null, 2), 'utf8');
+  console.log(`Created package.json in ${buildPath}`);
+
+  return newPackageData;
+};
+
+const run = () => {
+  ['CHANGELOG.md', 'README.md', 'theme.js'].map(copyFile);
+  createPackageJson();
+};
+
+run();
