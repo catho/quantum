72900bd1ffb3e6bd2eb0728b6b3f0e95cda9c851
ggdaltoso <gabriel.daltoso@catho.com>
2019-02-05 16:47:10 -0200

    fix(BTV-924): fix typography story

--- stories/Typography/Typography.story.jsx
+++ stories/Typography/Typography.story.jsx
@@ -2,20 +2,10 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Heading } from '@catho-private/quantum-storybook-ui';
 
-// import Typography from './Typography';
-
-import { Title } from '../../components/GlobalStyle';
+import Typography from './Typography';
 
 storiesOf('1. Foundation', module).add('Typography', () => (
   <Heading title="Typography">
-    <Title>Title</Title>
-    <Title as="h1">Title</Title>
-    <Title as="h2">Title</Title>
-    <Title as="h3">Title</Title>
-    <Title as="h4">Title</Title>
-    <Title as="h5">Title</Title>
-    <Title as="h6">Title</Title>
-
-    {/* <Typography /> */}
+    <Typography />
   </Heading>
 ));
