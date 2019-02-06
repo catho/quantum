484b3e4a7834a3d9f427d19694e5713314891abc
rapahaeru <rapahaeru@gmail.com>
2019-02-06 18:22:45 -0200

    feat(BTV-944): [TB] apply colors

--- components/Button/skins.js
+++ components/Button/skins.js
@@ -1,21 +1,25 @@
-import Colors from '../Colors/deprecated';
+import Colors from '../Colors';
 import DefaultButton from './models/DefaultButton';
 
 const skins = {
   primary: new DefaultButton({
-    unselected: Colors.PRIMARY['500'],
-    selected: Colors.PRIMARY['600'],
-    disabled: Colors.PRIMARY['200'],
+    unselected: Colors.BLUE['500'],
+    selected: Colors.COBALT['500'],
+    disabled: Colors.BLACK['400'],
   }),
   secondary: new DefaultButton({
-    unselected: Colors.SECONDARY['500'],
-    selected: Colors.SECONDARY['600'],
-    disabled: Colors.SECONDARY['200'],
+    unselected: Colors.WHITE,
+    selected: Colors.BLUE['200'],
+    disabled: Colors.SHADOW['40'],
+    unselectedTextColor: Colors.BLUE['500'],
+    disabledTextColor: Colors.BLACK['400'],
+    unselectedBorder: Colors.BLUE['500'],
+    selectedBorder: Colors.BLUE['500'],
   }),
   action: new DefaultButton({
-    unselected: Colors.ACTION['500'],
-    selected: Colors.ACTION['600'],
-    disabled: Colors.ACTION['200'],
+    unselected: Colors.PINK['500'],
+    selected: Colors.PINK['900'],
+    disabled: Colors.PINK['500'],
   }),
 };
 
