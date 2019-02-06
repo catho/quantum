045ec65fa3549fc64e42676b3be91ab6806e4065
ggdaltoso <gabriel.daltoso@catho.com>
2019-02-06 09:31:41 -0200

    feat(BTV-924): <Blockquote />

--- components/GlobalStyle/index.js
+++ components/GlobalStyle/index.js
@@ -3,5 +3,13 @@ import Typography from './sub-components/Typography';
 import IconFont from './sub-components/IconFont';
 import Title from './sub-components/Title';
 import Subtitle from './sub-components/Subtitle';
+import Blockquote from './sub-components/Blockquote';
 
-export { GlobalStyle as default, Typography, IconFont, Title, Subtitle };
+export {
+  GlobalStyle as default,
+  Typography,
+  IconFont,
+  Title,
+  Subtitle,
+  Blockquote,
+};
---
+++ components/GlobalStyle/sub-components/Blockquote.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const Blockquote = styled.blockquote`
+  font-size: 12px;
+  line-height: 18px;
+  letter-spacing: 0.2px;
+`;
+
+export default Blockquote;
