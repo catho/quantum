185f27d8f01d18e42f9a4b570b912eb38c815239
allyssonsantos <aallysson0@gmail.com>
2019-02-07 13:26:48 -0200

    feat(BTV-927): [WM] Removing css variables and using js

--- components/Grid/sub-components/Col.jsx
+++ components/Grid/sub-components/Col.jsx
@@ -1,40 +1,56 @@
-import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import {
-  query,
-  CSSVariables,
-  BREAKPOINTS,
-  hideCb,
-  collumnSize,
-} from './shared';
+import { query, BREAKPOINTS, hideCb } from './shared';
 
 const Col = styled.div`
   position: relative;
   width: 100%;
   min-height: 1px;
   box-sizing: border-box;
-  margin: calc(var(--gutter) / 2);
+  margin: calc(${BREAKPOINTS.xsmall.gutter}px / 2);
 
-  @media (min-width: 1px) {
-    --grid-column: 4;
-  }
-
-  @media (min-width: 600px) {
-    --grid-column: 8;
-  }
-
-  @media (min-width: 840px) {
-    --grid-column: 12;
-  }
+  ${props =>
+    query.xsmall`width: calc(${props.xsmall || BREAKPOINTS.xsmall.columns} / ${
+      BREAKPOINTS.xsmall.columns
+    } * 100% - ${BREAKPOINTS.xsmall.gutter}px);`}
+  ${props => query.small`
+    width: calc(${props.small || props.xsmall || BREAKPOINTS.small.columns} / ${
+    BREAKPOINTS.small.columns
+  } * 100% - ${BREAKPOINTS.small.gutter}px);
+    margin: calc(${BREAKPOINTS.small.gutter}px / 2);
+  `}
+  ${props => query.medium`
+    width: calc(${props.medium ||
+      props.small ||
+      props.xsmall ||
+      BREAKPOINTS.medium.columns} / ${BREAKPOINTS.medium.columns} * 100% - ${
+    BREAKPOINTS.medium.gutter
+  }px);
+    margin: calc(${BREAKPOINTS.medium.gutter}px / 2);
+  `}
+  ${props => query.large`
+    width: calc(${props.large ||
+      props.medium ||
+      props.small ||
+      props.xsmall ||
+      BREAKPOINTS.large.columns} / ${BREAKPOINTS.large.columns} * 100% - ${
+    BREAKPOINTS.large.gutter
+  }px);
+    margin: calc(${BREAKPOINTS.large.gutter}px / 2);
+  `}
+  ${props => query.xlarge`
+    width: calc(${props.xlarge ||
+      props.large ||
+      props.medium ||
+      props.small ||
+      props.xsmall ||
+      BREAKPOINTS.xlarge.columns} / ${BREAKPOINTS.xlarge.columns} * 100% - ${
+    BREAKPOINTS.xlarge.gutter
+  }px);
+    margin: calc(${BREAKPOINTS.xlarge.gutter}px / 2);
+  `}
 
   ${hideCb}
-
-  ${props => collumnSize(props.xsmall, query.xsmall)}
-  ${props => collumnSize(props.small, query.small)}
-  ${props => collumnSize(props.medium, query.medium)}
-  ${props => collumnSize(props.large, query.large)}
-  ${props => collumnSize(props.xlarge, query.xlarge)}
 `;
 
 Col.propTypes = {
@@ -51,9 +67,4 @@ Col.propTypes = {
 
 Col.displayName = 'Col';
 
-export default props => (
-  <>
-    <CSSVariables />
-    <Col {...props} />
-  </>
-);
+export default Col;
