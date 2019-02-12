ba5b9dea6b423a86fb3f055e53d76492720ad5cd
Daniel Silva <daniel5055@gmail.com>
2019-02-12 20:07:04 -0200

    feat(BTV-971): [WM] Remove Colors from tag story

--- stories/Tag/Tag.story.jsx
+++ stories/Tag/Tag.story.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { AutoExample } from '@catho-private/quantum-storybook-ui';
-import { Tag, Colors } from '../../components';
+import { Tag } from '../../components';
 
 storiesOf('16. Tag', module).add('Tag', () => <AutoExample component={Tag} />);
