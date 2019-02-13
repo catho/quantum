44e6d748b2012de09a2acd926a446f6f730739a3
allyssonsantos <aallysson0@gmail.com>
2019-02-13 10:27:43 -0200

    feat(BTV-972): [WM] Badge tests

--- components/Badge/Badge.unit.test.jsx
+++ components/Badge/Badge.unit.test.jsx
@@ -1,37 +1,110 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { mount } from 'enzyme';
+
 import Badge from './Badge';
+import Colors from '../Colors';
+import Button from '../Button';
+import 'jest-styled-components';
 
 describe('Badge component', () => {
-  describe('Should match snapshot', () => {
-    it('with different text props', () => {
-      const badges = [
-        <Badge text="Default with text prop" />,
-        <Badge text="Default with text prop"> Default with children </Badge>,
-        <Badge> Default </Badge>,
-      ];
+  describe('Should match snapshot and correct styles', () => {
+    it('With number', () => {
+      const BadgeComponent = <Badge number={10} />;
 
-      badges.forEach(badge => {
-        expect(renderer.create(badge).toJSON()).toMatchSnapshot();
-      });
+      expect(renderer.create(BadgeComponent).toJSON()).toMatchSnapshot();
+      expect(mount(BadgeComponent).find('Tag')).toHaveStyleRule(
+        'background-color',
+        Colors.BLACK[100],
+      );
+      expect(mount(BadgeComponent).find('Tag')).toHaveStyleRule(
+        'color',
+        Colors.BLACK[700],
+      );
     });
 
     it('with different skins', () => {
-      const badges = [
-        <Badge skin="brand1"> Brand1 </Badge>,
-        <Badge skin="brand2"> Brand2 </Badge>,
-        <Badge skin="primary"> Primary </Badge>,
-        <Badge skin="secondary"> Secondary </Badge>,
-        <Badge skin="success"> Text </Badge>,
-        <Badge skin="action"> Text </Badge>,
-        <Badge skin="warning"> Text </Badge>,
-        <Badge skin="danger"> Text </Badge>,
-        <Badge skin="info"> Text </Badge>,
-      ];
+      const badges = {
+        default: mount(<Badge skin="default" number={10} />),
+        black: mount(<Badge skin="black" number={10} />),
+        blue: mount(<Badge skin="blue" number={10} />),
+        white: mount(<Badge skin="white" number={10} />),
+        error: mount(<Badge skin="error" number={10} />),
+        success: mount(<Badge skin="success" number={10} />),
+      };
 
-      badges.forEach(badge => {
-        expect(renderer.create(badge).toJSON()).toMatchSnapshot();
-      });
+      expect(badges.default.find('Wrapper')).toHaveStyleRule(
+        'margin-left',
+        '8px',
+      );
+      expect(badges.default.find('Wrapper')).toHaveStyleRule(
+        'margin-right',
+        '8px',
+      );
+
+      expect(badges.default.find('Tag')).toHaveStyleRule(
+        'background-color',
+        Colors.BLACK[100],
+      );
+      expect(badges.default.find('Tag')).toHaveStyleRule(
+        'color',
+        Colors.BLACK[700],
+      );
+      expect(badges.black.find('Tag')).toHaveStyleRule(
+        'background-color',
+        Colors.BLACK[700],
+      );
+      expect(badges.black.find('Tag')).toHaveStyleRule('color', Colors.WHITE);
+      expect(badges.blue.find('Tag')).toHaveStyleRule(
+        'background-color',
+        Colors.BLUE[500],
+      );
+      expect(badges.blue.find('Tag')).toHaveStyleRule('color', Colors.WHITE);
+      expect(badges.white.find('Tag')).toHaveStyleRule(
+        'background-color',
+        Colors.WHITE,
+      );
+      expect(badges.white.find('Tag')).toHaveStyleRule('color', '#000');
+      expect(badges.white.find('Tag')).toHaveStyleRule(
+        'mix-blend-mode',
+        'lighten',
+      );
+      expect(badges.error.find('Tag')).toHaveStyleRule(
+        'background-color',
+        Colors.ERROR[500],
+      );
+      expect(badges.error.find('Tag')).toHaveStyleRule('color', Colors.WHITE);
+      expect(badges.success.find('Tag')).toHaveStyleRule(
+        'background-color',
+        Colors.SUCCESS[200],
+      );
+      expect(badges.success.find('Tag')).toHaveStyleRule(
+        'color',
+        Colors.SUCCESS[900],
+      );
+    });
+
+    it('with children', () => {
+      const BadgeComponent = (
+        <Badge number={10}>
+          <Button />
+        </Badge>
+      );
+
+      expect(renderer.create(BadgeComponent).toJSON()).toMatchSnapshot();
+      expect(mount(BadgeComponent).find('Wrapper')).toHaveStyleRule(
+        'position',
+        'relative',
+      );
+      expect(mount(BadgeComponent).find('Tag')).toHaveStyleRule(
+        'position',
+        'absolute',
+      );
+      expect(mount(BadgeComponent).find('Tag')).toHaveStyleRule('top', '-10px');
+      expect(mount(BadgeComponent).find('Tag')).toHaveStyleRule(
+        'right',
+        '-10px',
+      );
     });
   });
 });
