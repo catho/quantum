cc8f1ded286d78150d344f3900fb6d1bd6b4eda7
allyssonsantos <aallysson0@gmail.com>
2019-02-13 14:04:41 -0200

    fix(BTV-972): [WM] Refactoring tests

--- components/Badge/Badge.unit.test.jsx
+++ components/Badge/Badge.unit.test.jsx
@@ -7,101 +7,85 @@ import Colors from '../Colors';
 import Button from '../Button';
 import 'jest-styled-components';
 
-describe('Badge component', () => {
-  describe('Should match snapshot and correct styles', () => {
+describe('<Badge />', () => {
+  describe('Should match snapshot', () => {
     it('With number', () => {
-      const BadgeComponent = <Badge number={10} />;
+      expect(renderer.create(<Badge number={10} />).toJSON()).toMatchSnapshot();
+    });
 
-      expect(renderer.create(BadgeComponent).toJSON()).toMatchSnapshot();
-      expect(mount(BadgeComponent).find('Tag')).toHaveStyleRule(
+    it('with children', () => {
+      expect(
+        renderer
+          .create(
+            <Badge number={10}>
+              <Button />
+            </Badge>,
+          )
+          .toJSON(),
+      ).toMatchSnapshot();
+    });
+  });
+
+  describe('Match correct styles', () => {
+    const BadgeComponent = mount(<Badge number={10} />);
+
+    it("should have correct colors when skin prop isn't defined", () => {
+      expect(BadgeComponent.find('StyledBadge')).toHaveStyleRule(
         'background-color',
         Colors.BLACK[100],
       );
-      expect(mount(BadgeComponent).find('Tag')).toHaveStyleRule(
+
+      expect(BadgeComponent.find('StyledBadge')).toHaveStyleRule(
         'color',
         Colors.BLACK[700],
       );
     });
 
-    it('with different skins', () => {
-      const badges = {
-        default: mount(<Badge skin="default" number={10} />),
-        black: mount(<Badge skin="black" number={10} />),
-        blue: mount(<Badge skin="blue" number={10} />),
-        white: mount(<Badge skin="white" number={10} />),
-        error: mount(<Badge skin="error" number={10} />),
-        success: mount(<Badge skin="success" number={10} />),
-      };
-
-      expect(badges.default.find('Wrapper')).toHaveStyleRule(
+    it("Should have spacing around when Badge don't have children", () => {
+      expect(BadgeComponent.find('BadgeWrapper')).toHaveStyleRule(
         'margin-left',
         '8px',
       );
-      expect(badges.default.find('Wrapper')).toHaveStyleRule(
+      expect(BadgeComponent.find('BadgeWrapper')).toHaveStyleRule(
         'margin-right',
         '8px',
       );
+    });
 
-      expect(badges.default.find('Tag')).toHaveStyleRule(
-        'background-color',
-        Colors.BLACK[100],
-      );
-      expect(badges.default.find('Tag')).toHaveStyleRule(
-        'color',
-        Colors.BLACK[700],
-      );
-      expect(badges.black.find('Tag')).toHaveStyleRule(
-        'background-color',
-        Colors.BLACK[700],
-      );
-      expect(badges.black.find('Tag')).toHaveStyleRule('color', Colors.WHITE);
-      expect(badges.blue.find('Tag')).toHaveStyleRule(
-        'background-color',
-        Colors.BLUE[500],
-      );
-      expect(badges.blue.find('Tag')).toHaveStyleRule('color', Colors.WHITE);
-      expect(badges.white.find('Tag')).toHaveStyleRule(
-        'background-color',
-        Colors.WHITE,
-      );
-      expect(badges.white.find('Tag')).toHaveStyleRule('color', '#000');
-      expect(badges.white.find('Tag')).toHaveStyleRule(
-        'mix-blend-mode',
-        'lighten',
-      );
-      expect(badges.error.find('Tag')).toHaveStyleRule(
-        'background-color',
-        Colors.ERROR[500],
-      );
-      expect(badges.error.find('Tag')).toHaveStyleRule('color', Colors.WHITE);
-      expect(badges.success.find('Tag')).toHaveStyleRule(
+    it('Should match colors when skin prop is defined', () => {
+      const BadgeWithSkin = mount(<Badge number={10} skin="success" />);
+
+      expect(BadgeWithSkin.find('StyledBadge')).toHaveStyleRule(
         'background-color',
         Colors.SUCCESS[200],
       );
-      expect(badges.success.find('Tag')).toHaveStyleRule(
+      expect(BadgeWithSkin.find('StyledBadge')).toHaveStyleRule(
         'color',
         Colors.SUCCESS[900],
       );
     });
+  });
 
-    it('with children', () => {
-      const BadgeComponent = (
+  describe('Positioning when have children', () => {
+    it('should be placed at top-right corner', () => {
+      const BadgeComponent = mount(
         <Badge number={10}>
           <Button />
-        </Badge>
+        </Badge>,
       );
-
-      expect(renderer.create(BadgeComponent).toJSON()).toMatchSnapshot();
-      expect(mount(BadgeComponent).find('Wrapper')).toHaveStyleRule(
+      expect(BadgeComponent.find('BadgeWrapper')).toHaveStyleRule(
         'position',
         'relative',
       );
-      expect(mount(BadgeComponent).find('Tag')).toHaveStyleRule(
+      expect(BadgeComponent.find('StyledBadge')).toHaveStyleRule(
         'position',
         'absolute',
       );
-      expect(mount(BadgeComponent).find('Tag')).toHaveStyleRule('top', '-10px');
-      expect(mount(BadgeComponent).find('Tag')).toHaveStyleRule(
+      expect(BadgeComponent.find('StyledBadge')).toHaveStyleRule(
+        'top',
+        '-10px',
+      );
+      expect(BadgeComponent.find('StyledBadge')).toHaveStyleRule(
         'right',
         '-10px',
       );
