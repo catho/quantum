6d19240032247939a8b2995946d95b816ae498aa
Daniel Silva <daniel5055@gmail.com>
2019-02-13 09:05:30 -0200

    feat(BTV-971): [WM] Tag story examples

--- stories/Tag/Tag.story.jsx
+++ stories/Tag/Tag.story.jsx
@@ -1,6 +1,94 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { AutoExample } from '@catho-private/quantum-storybook-ui';
+import {
+  Tab,
+  Example,
+  AutoExample,
+  Title,
+} from '@catho-private/quantum-storybook-ui';
 import { Tag } from '../../components';
 
-storiesOf('16. Tag', module).add('Tag', () => <AutoExample component={Tag} />);
+const codeSkins = `<Tag>Default</Tag>
+<Tag skin="stroked">Stroked</Tag>
+<Tag skin="inverted">Inverted</Tag>
+<Tag skin="blue">Blue</Tag>
+<Tag skin="success">Success</Tag>
+<Tag skin="warning">Warning</Tag>
+<Tag skin="error">Error</Tag>
+<Tag skin="blue-text">Blue Text</Tag>`;
+
+const skinsExample = (
+  <>
+    <Title>Skins</Title>
+    <Example
+      component={
+        <>
+          <Tag>Default</Tag>
+          <Tag skin="stroked">Stroked</Tag>
+          <Tag skin="inverted">Inverted</Tag>
+          <Tag skin="blue">Blue</Tag>
+          <Tag skin="success">Success</Tag>
+          <Tag skin="warning">Warning</Tag>
+          <Tag skin="error">Error</Tag>
+          <Tag skin="blue-text">Blue Text</Tag>
+        </>
+      }
+      code={codeSkins}
+    />
+  </>
+);
+
+const codeClosables = `<Tag closable>Default</Tag>
+<Tag closable skin="stroked">Stroked</Tag>
+<Tag closable skin="inverted">Inverted</Tag>
+<Tag closable skin="blue">Blue</Tag>
+<Tag closable skin="success">Success</Tag>
+<Tag closable skin="warning">Warning</Tag>
+<Tag closable skin="error">Error</Tag>
+<Tag closable skin="blue-text">Blue Text</Tag>`;
+
+const closableExample = (
+  <>
+    <Title>Closables</Title>
+    <Example
+      component={
+        <>
+          <Tag closable>Default</Tag>
+          <Tag closable skin="stroked">
+            Stroked
+          </Tag>
+          <Tag closable skin="inverted">
+            Inverted
+          </Tag>
+          <Tag closable skin="blue">
+            Blue
+          </Tag>
+          <Tag closable skin="success">
+            Success
+          </Tag>
+          <Tag closable skin="warning">
+            Warning
+          </Tag>
+          <Tag closable skin="error">
+            Error
+          </Tag>
+          <Tag closable skin="blue-text">
+            Blue Text
+          </Tag>
+        </>
+      }
+      code={codeClosables}
+    />
+  </>
+);
+
+const exampleTab = (
+  <Tab title="Examples">
+    {skinsExample}
+    {closableExample}
+  </Tab>
+);
+
+storiesOf('16. Tag', module).add('Tag', () => (
+  <AutoExample component={Tag} additionalTabs={exampleTab} />
+));
