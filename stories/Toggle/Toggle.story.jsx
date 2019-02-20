1435779162dfdef8d942d203b617081fc34ef896
allyssonsantos <aallysson0@gmail.com>
2019-02-20 14:53:12 -0300

    feat(BTS-81): [WM] Toggle component story

--- stories/Toggle/Toggle.story.jsx
+++ stories/Toggle/Toggle.story.jsx
@@ -1,8 +1,51 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { AutoExample } from '@catho-private/quantum-storybook-ui';
+import {
+  Tab,
+  Example,
+  AutoExample,
+  Title,
+} from '@catho-private/quantum-storybook-ui';
 import Toggle from '../../components/Toggle';
+import { Container } from '../../components/Grid';
+
+const exampleTab = (
+  <Tab title="Example">
+    <Container fluid>
+      <Title>Left label</Title>
+      <Example
+        component={
+          <div style={{ display: 'flex', alignContent: 'center' }}>
+            <label htmlFor="left" style={{ marginRight: 10 }}>
+              Left label
+            </label>
+            <Toggle checked id="left" />
+          </div>
+        }
+        code={`<div style={{ display: 'flex', alignContent: 'center' }}>
+  <label htmlFor="left" style={{ marginRight: 10 }}>Left label</label>
+  <Toggle checked id="left" />
+</div>`}
+      />
+      <Title>Right label</Title>
+      <Example
+        component={
+          <div style={{ display: 'flex', alignContent: 'center' }}>
+            <Toggle id="right" />
+            <label htmlFor="right" style={{ marginLeft: 10 }}>
+              Right label
+            </label>
+          </div>
+        }
+        code={`<div style={{ display: 'flex', alignContent: 'center' }}>
+  <Toggle id="right" />
+  <label htmlFor="right" style={{ marginLeft: 10 }}>Right label</label>
+</div>`}
+      />
+    </Container>
+  </Tab>
+);
 
 storiesOf('3. Forms', module).add('Toggle', () => (
-  <AutoExample component={Toggle} />
+  <AutoExample component={Toggle} additionalTabs={exampleTab} />
 ));
