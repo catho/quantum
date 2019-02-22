924043162b0fa5c6a0d77dc11d0630cc19f59727
allyssonsantos <aallysson0@gmail.com>
2019-02-22 10:35:22 -0300

    fix(BTS-81): [WM] New tests and update snapshot

--- components/Toggle/Toggle.unit.test.jsx
+++ components/Toggle/Toggle.unit.test.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 import Toggle from './Toggle';
 import 'jest-styled-components';
 
 describe('<Toggle /> ', () => {
-  it('should match the snapshot', () => {
-    expect(renderer.create(<Toggle id="test" />).toJSON()).toMatchSnapshot();
+  describe('Snapshots', () => {
+    it('should match snapshot without props', () => {
+      expect(renderer.create(<Toggle />).toJSON()).toMatchSnapshot();
+    });
+
+    it('should match snapshot with checked props', () => {
+      expect(renderer.create(<Toggle checked />).toJSON()).toMatchSnapshot();
+    });
+  });
+
+  describe('Correctly props', () => {
+    it('should pass onChange prop to input element', () => {
+      const toggle = shallow(<Toggle onChange={() => {}} />);
+      const checkbox = toggle.find('HiddenCheckbox');
+
+      expect(checkbox.props().onChange).toBeTruthy();
+    });
+
+    it('should pass checked prop to input element', () => {
+      const toggle = shallow(<Toggle checked />);
+      const checkbox = toggle.find('HiddenCheckbox');
+
+      expect(checkbox.props().checked).toBe(true);
+    });
   });
 });
