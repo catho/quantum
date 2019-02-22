b14db0cc052fd60cdb503da2f69b3831e1551f2d
Daniel Silva <daniel5055@gmail.com>
2019-02-22 16:20:36 -0300

    feat(BTV-972): [WM] Use check Icon on checkbox

--- components/Checkbox/Checkbox.jsx
+++ components/Checkbox/Checkbox.jsx
@@ -2,78 +2,67 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-import { Label, FieldGroup, ErrorMessage } from '../shared';
+import { Label, ErrorMessage, theme } from '../shared';
 import Colors from '../Colors';
+import Icon from '../Icon';
 
 const CHECKBOX_SIZE = '18px';
-const CONTAINER_SIZE = '24px';
 
-const CheckboxFieldGroup = styled(FieldGroup)`
-  pointer-events: none;
+const Wrapper = styled.div``;
+
+const CheckboxWrapper = styled.div`
+  display: flex;
+  position: relative;
 `;
 
 const CheckboxLabel = styled(Label)`
   color: ${Colors.BLACK['700']};
-  cursor: inherit;
   font-size: 16px;
-  left: 0;
-  margin-bottom: 0;
-  min-height: ${CONTAINER_SIZE};
-  padding-left: ${CONTAINER_SIZE};
-  top: 0;
-
-  :before {
-    background-color: ${Colors.WHITE};
-    border-radius: 2px;
-    border: 2px solid ${Colors.BLACK['400']};
-    box-sizing: border-box;
-    content: ' ';
-    display: inline-block;
-    height: ${CHECKBOX_SIZE};
-    left: 0;
-    position: absolute;
-    top: 3px;
-    transition: all 0.2s ease-in-out;
-    width: ${CHECKBOX_SIZE};
-  }
+  margin: 0 0 0 10px;
 `;
 
 CheckboxLabel.displayName = 'CheckboxLabel';
 
-const HiddenInput = styled.input.attrs({
+const CheckIcon = styled(Icon).attrs({
+  name: 'check',
+})``;
+
+const HiddenCheckbox = styled.input.attrs({
   type: 'checkbox',
 })`
   cursor: pointer;
-  height: ${CHECKBOX_SIZE};
   height: 100%;
   margin: 0;
   opacity: 0;
-  pointer-events: initial;
   position: absolute;
   width: 100%;
 
+  + ${CheckIcon} {
+    align-items: center;
+    background-color: ${Colors.WHITE};
+    border-radius: 2px;
+    border: 2px solid ${Colors.BLACK['400']};
+    box-sizing: border-box;
+    color: transparent;
+    display: flex;
+    font-size: 16px;
+    font-weight: bold;
+    height: ${CHECKBOX_SIZE};
+    justify-content: center;
+    margin-top: 2px;
+    transition: all 0.2s ease-in-out;
+    width: ${CHECKBOX_SIZE};
+  }
+
   :checked {
-    + ${CheckboxLabel}:before {
+    + ${CheckIcon} {
       background-color: ${Colors.BLUE['500']};
       border-width: 0;
-    }
-
-    + ${CheckboxLabel}:after {
-      border-radius: 1px;
-      border: solid ${Colors.WHITE};
-      border-width: 0 2.5px 2.5px 0;
-      content: ' ';
-      display: inline-block;
-      height: 11px;
-      left: 6px;
-      position: absolute;
-      top: 3px;
-      transform: rotate(45deg);
-      width: 4px;
+      color: ${Colors.WHITE};
     }
   }
 
-  :hover + ${CheckboxLabel}:before {
+  :hover + ${CheckIcon} {
     border-color: ${Colors.BLUE['500']};
     box-shadow: 0 2px 6px 0 ${Colors.BLUE['50']};
   }
@@ -81,15 +70,15 @@ const HiddenInput = styled.input.attrs({
   ${({ error }) =>
     error &&
     `
-    + ${CheckboxLabel}:before {
+    + ${CheckIcon} {
       border-color: ${Colors.ERROR['500']};
     }
 
-    :checked + ${CheckboxLabel}:before {
+    :checked + ${CheckIcon} {
       background-color: ${Colors.ERROR['500']};
     }
 
-    :hover +  ${CheckboxLabel}:before {
+    :hover +  ${CheckIcon} {
       border-color: ${Colors.ERROR['500']};
       box-shadow: 0 2px 6px 0 ${Colors.ERROR['500']};
     }
@@ -98,58 +87,52 @@ const HiddenInput = styled.input.attrs({
   &[disabled] {
     cursor: not-allowed;
 
-    + ${CheckboxLabel} {
+    ~ ${CheckboxLabel} {
       color: ${Colors.BLACK['400']};
     }
 
-    + ${CheckboxLabel}:before {
+    + ${CheckIcon} {
       border-color: ${Colors.BLACK['400']};
       background-color: ${Colors.BLACK['200']};
     }
 
-    :checked + ${CheckboxLabel}:before {
+    :checked + ${CheckIcon} {
       background-color: ${Colors.BLACK['400']};
     }
 
-    :hover + ${CheckboxLabel}:before {
+    :hover + ${CheckIcon} {
       box-shadow: none;
     }
   }
 `;
 
-HiddenInput.displayName = 'HiddenInput';
-
-const CheckboxErrorMessage = styled(ErrorMessage)`
-  left: -3px;
-  position: absolute;
-  top: 28px;
-`;
+HiddenCheckbox.displayName = 'HiddenCheckbox';
 
 const Checkbox = ({ label, error, id, ...rest }) => (
-  <CheckboxFieldGroup>
-    <HiddenInput id={id} error={error} {...rest} />
-    <CheckboxLabel htmlFor={id}>{label}</CheckboxLabel>
-    {error && <CheckboxErrorMessage>{error}</CheckboxErrorMessage>}
-  </CheckboxFieldGroup>
+  <Wrapper>
+    <CheckboxWrapper>
+      <HiddenCheckbox id={id} error={error} {...rest} />
+      <CheckIcon />
+      <CheckboxLabel htmlFor={id}>{label}</CheckboxLabel>
+    </CheckboxWrapper>
+    {error && <ErrorMessage>{error}</ErrorMessage>}
+  </Wrapper>
 );
 
 Checkbox.defaultProps = {
-  label: 'Label',
-  id: '',
   checked: undefined,
   disabled: false,
   error: '',
-  onChange: () => {},
+  id: '',
+  label: 'Label',
 };
 
 Checkbox.propTypes = {
-  /** An html identification */
-  label: PropTypes.string,
-  id: PropTypes.string,
   checked: PropTypes.bool,
   disabled: PropTypes.bool,
   error: PropTypes.string,
-  onChange: PropTypes.func,
+  id: PropTypes.string,
+  label: PropTypes.string,
 };
 
 export default Checkbox;
--- components/Checkbox/Checkbox.unit.test.jsx
+++ components/Checkbox/Checkbox.unit.test.jsx
@@ -15,30 +15,47 @@ describe('<Checkbox />', () => {
     expect(
       renderer.create(<Checkbox error="error message" />).toJSON(),
     ).toMatchSnapshot();
+    expect(
+      renderer.create(<Checkbox error="error message" checked />).toJSON(),
+    ).toMatchSnapshot();
     expect(renderer.create(<Checkbox checked />).toJSON()).toMatchSnapshot();
     expect(renderer.create(<Checkbox disabled />).toJSON()).toMatchSnapshot();
+    expect(
+      renderer.create(<Checkbox label="Some text" />).toJSON(),
+    ).toMatchSnapshot();
+    expect(
+      renderer.create(<Checkbox checked disabled />).toJSON(),
+    ).toMatchSnapshot();
   });
 
   it('should pass onChange prop to checkbox component', () => {
     const wrapper = mount(<Checkbox onChange={mockFn} />);
-    const checkbox = wrapper.find('HiddenInput');
+    const checkbox = wrapper.find('HiddenCheckbox');
     expect(wrapper.prop('onChange')).toEqual(checkbox.prop('onChange'));
   });
 
   it('should pass checked prop to checkbox component', () => {
     const wrapper = shallow(<Checkbox checked />);
-    const checkbox = wrapper.find('HiddenInput');
+    const checkbox = wrapper.find('HiddenCheckbox');
     expect(checkbox.prop('checked')).toEqual(true);
   });
 
   it('should apply id prop', () => {
-    const wrapper = shallow(<Checkbox onChange={mockFn} id="test" />);
+    const wrapper = shallow(<Checkbox id="test" />);
 
     const label = wrapper.find('CheckboxLabel');
-    const checkbox = wrapper.find('HiddenInput');
+    const checkbox = wrapper.find('HiddenCheckbox');
     const htmlFor = label.prop('htmlFor');
 
     expect(htmlFor).not.toBeUndefined();
     expect(htmlFor).toEqual(checkbox.prop('id'));
+  });
+
+  it('should apply label prop', () => {
+    const wrapper = mount(<Checkbox label="test" />);
+    const labelText = wrapper.find('CheckboxLabel').text();
+
+    expect(labelText).not.toBeUndefined();
+    expect(labelText).toEqual(wrapper.prop('label'));
   });
 });
