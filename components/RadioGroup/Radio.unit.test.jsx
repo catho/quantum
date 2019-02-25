7665098a7008b4e403131ad796fd5b4c6feb26e4
ggdaltoso <gabriel.daltoso@catho.com>
2019-02-25 14:50:45 -0300

    test(BTV-965-radio-button): Radio

--- components/RadioGroup/Radio.unit.test.jsx
+++ components/RadioGroup/Radio.unit.test.jsx
@@ -1,125 +1,27 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { shallow, mount } from 'enzyme';
 import Radio from './Radio';
 
 describe('<RadioGroup.Radio />', () => {
-  const checkMock = jest.fn();
-  const checkPreviousMock = jest.fn();
-  const checkNextMock = jest.fn();
-
-  const KEYS = Object.freeze({
-    SPACE: 32,
-    LEFT: 37,
-    UP: 38,
-    RIGHT: 39,
-    DOWN: 40,
-  });
-
-  const radioExample = (
-    <Radio
-      value="Hello"
-      label="Hello"
-      check={checkMock}
-      checkNext={checkNextMock}
-      checkPrevious={checkPreviousMock}
-    />
-  );
-
-  const wrapper = mount(radioExample);
-  const shalloWrapper = shallow(radioExample);
-
-  it('Should match the snapshot', () => {
-    const tree = renderer.create(radioExample).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-
-  describe('properties', () => {
-    it('Should have defaults', () => {
-      const {
-        type: {
-          attrs: [attributes],
-        },
-      } = shalloWrapper.get(0);
-      expect(attributes).toHaveProperty('role', 'radio');
-      expect(attributes).toHaveProperty('aria-checked', 'false');
-      expect(attributes).toHaveProperty('tabIndex', -1);
-    });
-  });
-
-  describe('events', () => {
-    const eventDefault = {
-      stopPropagation() {},
-      preventDefault() {},
-    };
-
-    describe('click', () => {
-      it('Should check when clicked and when pressing space bar', () => {
-        const space = {
-          ...eventDefault,
-          keyCode: KEYS.SPACE,
-        };
-
-        wrapper.simulate('click');
-        expect(checkMock).toBeCalledWith(wrapper.props());
-
-        wrapper.simulate('keydown', space);
-        expect(checkMock).toBeCalledWith(wrapper.props());
-      });
-
-      it('Should not check when disabled', () => {
-        wrapper.setProps({ disabled: true });
-        wrapper.simulate('click');
-
-        expect(checkMock).not.toBeCalled();
-      });
+  describe('snapshot', () => {
+    it('simple', () => {
+      const component = <Radio value="Foo" label="Foo" />;
+      expect(renderer.create(component).toJSON()).toMatchSnapshot();
     });
 
-    describe('arrows', () => {
-      it('Should check next radio when arrow right or down is triggered', () => {
-        const right = {
-          ...eventDefault,
-          keyCode: KEYS.RIGHT,
-        };
-
-        const down = {
-          ...eventDefault,
-          keyCode: KEYS.DOWN,
-        };
-
-        wrapper.simulate('keydown', right);
-        expect(checkNextMock).toBeCalledWith(wrapper.props());
-
-        wrapper.simulate('keydown', down);
-        expect(checkNextMock).toBeCalledWith(wrapper.props());
-      });
-
-      it('Should check previous radio when arrow left or up is triggered', () => {
-        const up = {
-          ...eventDefault,
-          keyCode: KEYS.UP,
-        };
-
-        const left = {
-          ...eventDefault,
-          keyCode: KEYS.LEFT,
-        };
-
-        wrapper.simulate('keydown', up);
-        expect(checkPreviousMock).toBeCalledWith(wrapper.props());
-
-        wrapper.simulate('keydown', left);
-        expect(checkPreviousMock).toBeCalledWith(wrapper.props());
-      });
+    it('disabled', () => {
+      const component = <Radio value="Foo" label="Foo" disabled />;
+      expect(renderer.create(component).toJSON()).toMatchSnapshot();
     });
 
-    it('Should focus when aria-checked change', () => {
-      const radio = wrapper.instance().radioRef;
-      jest.spyOn(radio, 'focus');
+    it('error', () => {
+      const component = <Radio value="Foo" label="Foo" error />;
+      expect(renderer.create(component).toJSON()).toMatchSnapshot();
+    });
 
-      wrapper.setProps({ 'aria-checked': true });
-
-      expect(radio.focus).toHaveBeenCalledTimes(1);
+    it('children', () => {
+      const component = <Radio value="Foo">Foo</Radio>;
+      expect(renderer.create(component).toJSON()).toMatchSnapshot();
     });
   });
 });
