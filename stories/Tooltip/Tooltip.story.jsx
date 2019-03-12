6fea701e472934a323a6ecddf761b293915c75fc
Allysson Santos <aallysson0@gmail.com>
2019-03-12 07:17:13 -0300

    docs(BTS-85-storybook-looks) Update Tooltip story

--- stories/Tooltip/Tooltip.story.jsx
+++ stories/Tooltip/Tooltip.story.jsx
@@ -3,8 +3,12 @@ import { storiesOf } from '@storybook/react';
 import { AutoExample } from '@catho-private/quantum-storybook-ui';
 import Tooltip from '../../components/Tooltip';
 
+const description = `Tooltips provide additional information upon hover or focus.
+They often contain helper text that is contextual to an element.`;
+
 storiesOf('Tooltip', module).add('Basic', () => (
   <AutoExample
+    description={description}
     component={Tooltip}
     componentProps={{
       children: 'Hover me',
