5b4533fd2d36a89511b1d4e6746570cae9bc2d2d
Allysson Santos <aallysson0@gmail.com>
2019-03-12 07:15:02 -0300

    docs(BTS-85-storybook-looks) Update Button story

--- stories/Button/Button.story.jsx
+++ stories/Button/Button.story.jsx
@@ -106,9 +106,18 @@ const exampleTab = (
   </Tab>
 );
 
+const description = `Buttons express what action will occur when the user clicks 
+or touches it. Buttons are used to initialize an action, either in the
+background or foreground of an experience.
+`;
+
 storiesOf('Buttons', module).add('Button', () => (
   <>
     <IconFont />
-    <AutoExample component={Button} additionalTabs={exampleTab} />
+    <AutoExample
+      description={description}
+      component={Button}
+      additionalTabs={exampleTab}
+    />
   </>
 ));
