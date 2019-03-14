d9c6e2c7ecac3b192c8aebe5e8231d9bdb972b23
Alan Oliveira <alan.oliveira@catho.com>
2019-03-14 20:43:23 -0300

    feat(BTS-85-storybook-looks): quantum logo

--- stories/Introduction/Introduction.story.jsx
+++ stories/Introduction/Introduction.story.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Heading } from '@catho-private/quantum-storybook-ui';
+import Logo from '../../.storybook/static/logo.svg';
 import Introduction from './Introduction';
 
 storiesOf('Introduction', module).add('Getting started', () => (
   <>
-    <Heading title="Quantum">
+    <Heading title="Quantum" image={Logo}>
       CSS in JS based reusable components, are the core of Quantum
       design-system: a library for developing consistent UI/UX at Catho.
     </Heading>
