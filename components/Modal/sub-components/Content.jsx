3da29b38b36dbed54d50d3506e0f6e2c406b7de8
allyssonsantos <aallysson0@gmail.com>
2019-03-26 13:10:01 -0300

    fix(Modal.Content): Fix font-size value

--- components/Modal/sub-components/Content.jsx
+++ components/Modal/sub-components/Content.jsx
@@ -2,9 +2,10 @@ import styled from 'styled-components';
 import Card from '../../Card';
 
 const Content = styled(Card.Content)`
-  padding: 16px 24px;
+  font-size: 16px;
   max-height: 70vh;
   overflow-y: auto;
+  padding: 16px 24px;
 `;
 
 export default Content;
