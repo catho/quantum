f1e67216ef4799376369a33a22a7170a9bddfd78
allyssonsantos <aallysson0@gmail.com>
2019-04-02 15:43:43 -0300

    feat(Page): Add active/focus style to Page component

--- components/Pagination/sub-components/Page.jsx
+++ components/Pagination/sub-components/Page.jsx
@@ -7,7 +7,8 @@ const PageButton = styled.button`
   align-items: center;
   background-color: ${({ active }) =>
     active ? Colors.BLUE['500'] : Colors.WHITE};
-  border: 1px solid ${({ active }) => (active ? Colors.BLUE['500'] : '#d3d3d3')};
+  border: 1px solid
+    ${({ active }) => (active ? Colors.BLUE['500'] : Colors.BLACK['200'])};
   border-radius: 10px;
   box-sizing: border-box;
   cursor: pointer;
@@ -15,20 +16,23 @@ const PageButton = styled.button`
   font-size: 16px;
   font-weight: ${({ active }) => (active ? '700' : 'normal')};
   justify-content: center;
+  margin: 0 4px;
   min-height: 40px;
   min-width: 40px;
   outline: none;
   padding: 5px 8px;
   transition: background-color 0.2s ease-in-out, border-color 0.2s ease-in-out,
-    color 0.2s ease-in-out;
+    color 0.2s ease-in-out, font-weight 0.2s ease-in-out;
 
   ${({ active }) => active && `color: ${Colors.WHITE};`};
 
   :hover,
   :focus {
-    background-color: ${Colors.BLUE['200']};
-    border-color: ${Colors.BLUE['500']};
-    color: ${Colors.BLUE['500']};
+    background-color: ${({ active }) =>
+      active ? Colors.COBALT['500'] : Colors.BLUE['200']};
+    border-color: ${({ active }) =>
+      active ? Colors.COBALT['500'] : Colors.BLUE['500']};
+    color: ${({ active }) => (active ? Colors.WHITE : Colors.BLUE['500'])};
   }
 `;
 
