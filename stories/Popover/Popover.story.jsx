dcf6a1524f1b700a1c055884eae11bd14ff9e9a1
rapahaeru <rapahaeru@gmail.com>
2019-04-08 14:46:48 -0300

    fix(BTS-166): update linter

--- stories/Popover/Popover.story.jsx
+++ stories/Popover/Popover.story.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { AutoExample } from '@catho/quantum-storybook-ui';
 import Popover from '../../components/Popover';
-import Tooltip from '../../components/Tooltip';
 
 const description = `Popovers provide additional information upon hover or focus.
 They often contain helper text that is contextual to an element.`;
