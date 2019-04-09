6ab40a66a4ef39fe62a4d7de26e6b2e31862342c
ggdaltoso <gabriel.daltoso@catho.com>
2019-04-09 13:31:02 -0300

    refactor(Pagination): destructuring some lines

--- components/Pagination/utils/pagination.js
+++ components/Pagination/utils/pagination.js
@@ -14,7 +14,7 @@ const centerRule = ({ total, active }) => {
   return active - 1;
 };
 
-const visiblePages = (active, total) => {
+const getNumberPages = (active, total) => {
   if (active <= 3 || active === total) {
     return 4;
   }
@@ -27,46 +27,48 @@ const pagination = ({ totalPages = 1, activePage = 1 } = {}) => {
     return Array.from({ length: totalPages }, (_, i) => i + 1);
   }
 
-  let pages = [
+  const initialPages = [
     1,
     ...Array.from(
-      { length: visiblePages(activePage, totalPages) },
+      { length: getNumberPages(activePage, totalPages) },
       (_, i) => i + centerRule({ total: totalPages, active: activePage }),
     ),
     totalPages,
   ];
 
-  pages = pages.filter((page, index, array) => array.indexOf(page) === index);
+  let visiblePages = initialPages.filter(
+    (page, index, array) => array.indexOf(page) === index,
+  );
 
-  let firstPage = pages[0];
-  let secondPage = pages[1];
+  let [firstPage, secondPage] = visiblePages;
 
   if (secondPage === firstPage + 2) {
-    pages = [firstPage, firstPage + 1, ...pages.slice(1)];
+    visiblePages = [firstPage, firstPage + 1, ...visiblePages.slice(1)];
   }
 
-  let penultimatePage = pages[pages.length - 2];
-  let lastPage = pages[pages.length - 1];
+  let {
+    [visiblePages.length - 2]: penultimatePage,
+    [visiblePages.length - 1]: lastPage,
+  } = visiblePages;
 
   if (penultimatePage === lastPage - 2) {
-    pages = [...pages.slice(0, -1), lastPage - 1, lastPage];
+    visiblePages = [...visiblePages.slice(0, -1), lastPage - 1, lastPage];
   }
 
-  firstPage = pages[0]; // eslint-disable-line
-  secondPage = pages[1]; // eslint-disable-line
+  [firstPage, secondPage] = visiblePages;
 
   if (secondPage > firstPage + 2) {
-    pages = [firstPage, '...', ...pages.slice(1)];
+    visiblePages = [firstPage, '…', ...visiblePages.slice(1)];
   }
 
-  penultimatePage = pages[pages.length - 2];
-  lastPage = pages[pages.length - 1];
+  penultimatePage = visiblePages[visiblePages.length - 2];
+  lastPage = visiblePages[visiblePages.length - 1];
 
   if (penultimatePage < lastPage - 2) {
-    pages = [...pages.slice(0, -1), '...', lastPage];
+    visiblePages = [...visiblePages.slice(0, -1), '…', lastPage];
   }
 
-  return pages;
+  return visiblePages;
 };
 
 export default pagination;
