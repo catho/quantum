44284dfe629c4aba263225c359245f74025424ac
allyssonsantos <aallysson0@gmail.com>
2019-04-16 08:44:04 -0300

    style(Pagination): Remove unused import

--- components/Pagination/Pagination.unit.test.jsx
+++ components/Pagination/Pagination.unit.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { shallow, mount } from 'enzyme';
+import { mount } from 'enzyme';
 import Pagination from './Pagination';
 
 describe('<Pagination />', () => {
