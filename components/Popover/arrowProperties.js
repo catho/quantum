d6285f55aad8b4885f36d0cb6949a275e01051ee
allyssonsantos <aallysson0@gmail.com>
2019-04-22 14:48:53 -0300

    refactor(Popover): Refactor arrowProperties

--- components/Popover/arrowProperties.js
+++ components/Popover/arrowProperties.js
@@ -9,52 +9,41 @@ const getColorBySkin = skin => {
   return skin === 'default' ? Colors.WHITE : Colors[indexColor][200];
 };
 
-const getArrow = (placement, skin) => {
-  let placementPosition = '';
-  const placementPositionTop = `
-  content: '▼';
-  left: ${CENTER_POSITION};
-  text-shadow: 0px 4px 4px ${Colors.SHADOW[50]};
-  bottom: 0;
-  transform: translateX(-${CENTER_POSITION});`;
-
-  switch (placement) {
-    case 'top':
-      placementPosition = placementPositionTop;
-      break;
-    case 'bottom':
-      placementPosition = `
-      top: -4px;
-      content: '▲';
-      left: ${CENTER_POSITION};
-      text-shadow: 0px -3px 4px ${Colors.SHADOW[50]};
-      transform: translateX(-${CENTER_POSITION});`;
-      break;
-    case 'right':
-      placementPosition = `
-      bottom: ${CENTER_POSITION};
-      content: '◀';
-      left: ${SIDE_POSITION_X}px;
-      text-shadow: -3px 1px 4px ${Colors.SHADOW[50]};`;
-      break;
-    case 'left':
-      placementPosition = `
-      bottom: ${CENTER_POSITION};
-      content: '▶';
-      right: ${SIDE_POSITION_X}px;
-      text-shadow: 3px 1px 4px ${Colors.SHADOW[50]};`;
-      break;
-    default:
-      placementPosition = placementPositionTop;
-  }
-
-  return `
-    color: ${getColorBySkin(skin)}
-    font-size: ${ARROW_SIZE};
-    position: absolute;
-
-    ${placementPosition}
-  `;
+const placementPosition = {
+  top: `
+    content: '▼';
+    left: ${CENTER_POSITION};
+    text-shadow: 0px 4px 4px ${Colors.SHADOW[50]};
+    bottom: 0;
+    transform: translateX(-${CENTER_POSITION});
+  `,
+  bottom: `
+    top: -4px;
+    content: '▲';
+    left: ${CENTER_POSITION};
+    text-shadow: 0px -3px 4px ${Colors.SHADOW[50]};
+    transform: translateX(-${CENTER_POSITION});
+  `,
+  right: `
+    bottom: ${CENTER_POSITION};
+    content: '◀';
+    left: ${SIDE_POSITION_X}px;
+    text-shadow: -3px 1px 4px ${Colors.SHADOW[50]};
+  `,
+  left: `
+    bottom: ${CENTER_POSITION};
+    content: '▶';
+    right: ${SIDE_POSITION_X}px;
+    text-shadow: 3px 1px 4px ${Colors.SHADOW[50]};
+  `,
 };
+
+const getArrow = (placement, skin) => `
+  color: ${getColorBySkin(skin)}
+  font-size: ${ARROW_SIZE};
+  position: absolute;
+
+  ${placementPosition[placement]}
+`;
 
 export default getArrow;
