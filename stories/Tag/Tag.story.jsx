64a712b856e0b340686fa7d705d79967173d5699
Daniel Silva <daniel5055@gmail.com>
2019-05-24 09:17:51 -0300

    docs(Tag): Story

--- stories/Tag/Tag.story.jsx
+++ stories/Tag/Tag.story.jsx
@@ -1,112 +1,21 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import {
-  Tab,
-  Example,
-  AutoExample,
-  StoryContainer,
-  Title,
-} from '@catho/quantum-storybook-ui';
+import { Tab, AutoExample, StoryContainer } from '@catho/quantum-storybook-ui';
 import { Tag } from '../../components';
-
-const skinsCode = `<Tag>Default</Tag>
-<Tag skin="stroked">Stroked</Tag>
-<Tag skin="inverted">Inverted</Tag>
-<Tag skin="blue">Blue</Tag>
-<Tag skin="success">Success</Tag>
-<Tag skin="warning">Warning</Tag>
-<Tag skin="error">Error</Tag>
-<Tag skin="blue-text">Blue Text</Tag>`;
-
-const skinsExample = (
-  <>
-    <Title as="h2">Skins</Title>
-    <Example
-      component={
-        <>
-          <Tag>Default</Tag>
-          <Tag skin="stroked">Stroked</Tag>
-          <Tag skin="inverted">Inverted</Tag>
-          <Tag skin="blue">Blue</Tag>
-          <Tag skin="success">Success</Tag>
-          <Tag skin="warning">Warning</Tag>
-          <Tag skin="error">Error</Tag>
-          <Tag skin="blue-text">Blue Text</Tag>
-        </>
-      }
-      code={skinsCode}
-    />
-  </>
-);
-
-const closableCodes = `<Tag onClose={() => {}}>Default</Tag>
-<Tag onClose={() => {}} skin="stroked">Stroked</Tag>
-<Tag onClose={() => {}} skin="inverted">Inverted</Tag>
-<Tag onClose={() => {}} skin="blue">Blue</Tag>
-<Tag onClose={() => {}} skin="success">Success</Tag>
-<Tag onClose={() => {}} skin="warning">Warning</Tag>
-<Tag onClose={() => {}} skin="error">Error</Tag>
-<Tag onClose={() => {}} skin="blue-text">Blue Text</Tag>`;
-
-const closableExample = (
-  <>
-    <Title as="h2">Closables</Title>
-    <Example
-      component={
-        <>
-          <Tag onClose={() => {}}>Default</Tag>
-          <Tag onClose={() => {}} skin="stroked">
-            Stroked
-          </Tag>
-          <Tag onClose={() => {}} skin="inverted">
-            Inverted
-          </Tag>
-          <Tag onClose={() => {}} skin="blue">
-            Blue
-          </Tag>
-          <Tag onClose={() => {}} skin="success">
-            Success
-          </Tag>
-          <Tag onClose={() => {}} skin="warning">
-            Warning
-          </Tag>
-          <Tag onClose={() => {}} skin="error">
-            Error
-          </Tag>
-          <Tag onClose={() => {}} skin="blue-text">
-            Blue Text
-          </Tag>
-        </>
-      }
-      code={closableCodes}
-    />
-  </>
-);
-
-const sizesCode = `<Tag skin="small">Small</Tag>
-<Tag>Medium (Default)</Tag>
-<Tag skin="large">Large</Tag>`;
-
-const sizesExample = (
-  <>
-    <Title as="h2">Sizes</Title>
-    <Example
-      component={
-        <>
-          <Tag size="small">Small</Tag>
-          <Tag>Medium (Default)</Tag>
-          <Tag size="large">Large</Tag>
-        </>
-      }
-      code={sizesCode}
-    />
-  </>
-);
+import {
+  skinsExample,
+  invertedExample,
+  strokedExample,
+  closableExample,
+  sizesExample,
+} from './TagExamples';
 
 const exampleTab = (
   <Tab title="Examples">
     <StoryContainer>
       {skinsExample}
+      {invertedExample}
+      {strokedExample}
       {closableExample}
       {sizesExample}
     </StoryContainer>
