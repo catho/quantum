8c67daadf74b108cd13ca00b18da6fa3c0d91e1d
Daniel Silva <daniel5055@gmail.com>
2019-05-24 16:24:48 -0300

    refactor(Colors): Rename parse function

--- components/shared/hexToRgba.js
+++ components/shared/hexToRgba.js
@@ -5,10 +5,10 @@ const hexToRgba = (hex, opacity = 1) => {
   const _step = _shortened ? 1 : 2;
   const _bIndex = _shortened ? 2 : 4;
 
-  const _p = from =>
+  const _parse = from =>
     parseInt(_h.slice(from, from + _step).repeat(_shortened ? 2 : 1), 16);
 
-  return `rgba(${_p(0)},${_p(_step)},${_p(_bIndex)},${opacity})`;
+  return `rgba(${_parse(0)},${_parse(_step)},${_parse(_bIndex)},${opacity})`;
 };
 
 export default hexToRgba;
