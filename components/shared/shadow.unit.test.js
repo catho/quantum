db2a587e13c01b78b0484f59950ec13e21a57a67
Daniel Silva <daniel5055@gmail.com>
2019-05-28 15:07:14 -0300

    feat(Shadow): Elevation helper

--- components/shared/shadow.unit.test.js
+++ components/shared/shadow.unit.test.js
@@ -1,30 +1,88 @@
 import shadow from './shadow';
 import { colors } from './theme';
 import hexToRgba from './hexToRgba';
+import { normalizeCSS } from '../GlobalStyle/test.utils';
+
+const [opacity0, opacity1, opacity2] = [0.2, 0.14, 0.12];
+const normalize = str => normalizeCSS(str.trimStart().split('\n'));
 
 describe('Shadow helper', () => {
-  it('should get the correct elevation style', () => {
-    // expect(hexToRgba('#f1c245')).toBe('rgba(241,194,69,1)');
+  describe('without a color', () => {
+    it('should get the correct elevation style', () => {
+      const {
+        neutral: { 700: defaultColor },
+      } = colors;
 
-    
-    const opacities = [0.2, 0.14, 0.12];
-    const { neutral:  colorNeutral }  = colors;
-    const neutralRGBA = hexToRgba(colorNeutral[700], 0.2);
+      expect(normalize(shadow())).toBe(
+        normalize(`
+          box-shadow:
+            0px 0px 0px 0px ${hexToRgba(defaultColor, opacity0)},
+            0px 0px 0px 0px ${hexToRgba(defaultColor, opacity1)},
+            0px 0px 0px 0px ${hexToRgba(defaultColor, opacity2)};
+          `),
+      );
+      expect(normalize(shadow())).toBe(
+        normalize(`
+          box-shadow:
+            0px 0px 0px 0px ${hexToRgba(defaultColor, opacity0)},
+            0px 0px 0px 0px ${hexToRgba(defaultColor, opacity1)},
+            0px 0px 0px 0px ${hexToRgba(defaultColor, opacity2)};
+          `),
+      );
+      expect(normalize(shadow(0))).toBe(
+        normalize(`
+          box-shadow:
+            0px 0px 0px 0px ${hexToRgba(defaultColor, opacity0)},
+            0px 0px 0px 0px ${hexToRgba(defaultColor, opacity1)},
+            0px 0px 0px 0px ${hexToRgba(defaultColor, opacity2)};
+          `),
+      );
+      expect(normalize(shadow(2))).toBe(
+        normalize(`
+          box-shadow:
+            0px 3px 1px -2px ${hexToRgba(defaultColor, opacity0)},
+            0px 2px 2px 0px ${hexToRgba(defaultColor, opacity1)},
+            0px 1px 5px 0px ${hexToRgba(defaultColor, opacity2)};
+          `),
+      );
+    });
+  });
 
-    opacities.map( (opacity) =>  console.log(hexToRgba(colorNeutral[700], opacity)))
+  describe('with a color', () => {
+    it('should get the correct elevation style', () => {
+      const {
+        primary: { 100: first },
+        secondary: { 300: second },
+        success: { 500: third },
+      } = colors;
 
-    
-    console.log(neutralRGBA);
-
-    // expect(shadow(0)).toBe('box-shadow: 0px 0px 0px 0px rgba(0, 0, 0, 0.2), 0px 0px 0px 0px rgba(0, 0, 0, 0.14), 0px 0px 0px 0px rgba(0, 0, 0, 0.12);')
-
-    // box-shadow: 0px 0px 0px 0px RGBA();
-    // 0px 0px 0px 0px RGBA()
-    // 0px 0px 0px 0px
+      expect(normalize(shadow(5, first))).toBe(
+        normalize(
+          `box-shadow:
+            0px 3px 5px -1px ${hexToRgba(first, opacity0)},
+            0px 5px 8px 0px ${hexToRgba(first, opacity1)},
+            0px 1px 14px 0px ${hexToRgba(first, opacity2)};
+          `,
+        ),
+      );
+      expect(normalize(shadow(9, second))).toBe(
+        normalize(
+          `box-shadow:
+            0px 5px 6px -3px ${hexToRgba(second, opacity0)},
+            0px 9px 12px 1px ${hexToRgba(second, opacity1)},
+            0px 3px 16px 2px ${hexToRgba(second, opacity2)};
+          `,
+        ),
+      );
+      expect(normalize(shadow(15, third))).toBe(
+        normalize(
+          `box-shadow:
+            0px 8px 9px -5px ${hexToRgba(third, opacity0)},
+            0px 15px 22px 2px ${hexToRgba(third, opacity1)},
+            0px 6px 28px 5px ${hexToRgba(third, opacity2)};
+          `,
+        ),
+      );
+    });
   });
 });
-
-// .mdc-elevation--z0 {
-//   -webkit-box-shadow: 0px 0px 0px 0px rgba(0, 0, 0, 0.2), 0px 0px 0px 0px rgba(0, 0, 0, 0.14), 0px 0px 0px 0px rgba(0, 0, 0, 0.12);
-//           box-shadow: 0px 0px 0px 0px rgba(0, 0, 0, 0.2), 0px 0px 0px 0px rgba(0, 0, 0, 0.14), 0px 0px 0px 0px rgba(0, 0, 0, 0.12);
-// }
