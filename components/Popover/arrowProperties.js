29326f330d28b5fe7ddb6addb44f3b76bba42a20
rapahaeru <rapahaeru@gmail.com>
2019-05-29 15:53:48 -0300

    refactor(Popover): Update colors in arrowProperties

--- components/Popover/arrowProperties.js
+++ components/Popover/arrowProperties.js
@@ -1,14 +1,14 @@
-import Colors from '../Colors';
+import { hexToRgba } from '../shared';
 
 const ARROW_SIZE = '16px';
 const SIDE_POSITION_X = -8;
 const CENTER_POSITION = '50%';
 
-const placementPosition = {
+const placementPosition = ({ neutral: { 700: shadowColor } }) => ({
   top: `
     content: '▼';
     left: ${CENTER_POSITION};
-    text-shadow: 0px 8px 4px ${Colors.SHADOW[50]};
+    text-shadow: 0px 8px 4px ${hexToRgba(shadowColor, 0.2)};
     bottom: 0;
     transform: translateX(-${CENTER_POSITION});
   `,
@@ -16,22 +16,22 @@ const placementPosition = {
     top: -4px;
     content: '▲';
     left: ${CENTER_POSITION};
-    text-shadow: 0px -3px 4px ${Colors.SHADOW[50]};
+    text-shadow: 0px -3px 4px ${hexToRgba(shadowColor, 0.2)};
     transform: translateX(-${CENTER_POSITION});
   `,
   right: `
     bottom: ${CENTER_POSITION};
     content: '◀';
     left: ${SIDE_POSITION_X}px;
-    text-shadow: -4px 1px 4px ${Colors.SHADOW[50]};
+    text-shadow: -4px 1px 4px ${hexToRgba(shadowColor, 0.2)};
   `,
   left: `
     bottom: ${CENTER_POSITION};
     content: '▶';
     right: ${SIDE_POSITION_X}px;
-    text-shadow: 4px 1px 4px ${Colors.SHADOW[50]};
+    text-shadow: 4px 1px 4px ${hexToRgba(shadowColor, 0.2)};
   `,
-};
+});
 
 const getArrow = ({
   placement,
@@ -42,13 +42,14 @@ const getArrow = ({
         skins: { [skin]: popoverSkin },
       },
     },
+    colors,
   },
   inverted,
 }) => ` color: ${inverted ? popoverSkin.text : popoverSkin.background}
   font-size: ${ARROW_SIZE};
   position: absolute;
 
-  ${placementPosition[placement]}
+  ${placementPosition(colors)[placement]}
 `;
 
 export default getArrow;
