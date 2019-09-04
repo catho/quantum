9f733c227601ecd1c3749953606d9352243aea30
pedro morais <pedro.morais@catho.com>
2019-09-04 14:40:32 -0300

    style(card): fix card on IE

--- components/Card/sub-components/HeaderText.jsx
+++ components/Card/sub-components/HeaderText.jsx
@@ -2,6 +2,9 @@ import styled from 'styled-components';
 
 const HeaderText = styled.div`
   flex-grow: 1;
+  max-width: 100%;
+  display: inline-block;
+  word-break: break-all;
 `;
 
 HeaderText.displayName = 'Card.HeaderText';
