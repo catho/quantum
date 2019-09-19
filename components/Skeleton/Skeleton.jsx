95612b82759bb8dc258033b904a6ffcee055aa72
Pedro <pedro_hmorais@hotmail.com>
2019-09-19 15:31:09 -0300

    feat: skeletons (#129)
    * feat: create 2 new colors in pallete neutral and change in some components
    
    * add new colors in remaining themes
    
    * fix(theme): removes 0 and 1000 from skins except neutral
    
    * fix(colors): fix theme colors and add new colors to documentation
    
    * test(tests): updating tests
    
    * feat(circularLoader): Creates CircularLoader component
    
    * fix(button): fix buttons issues
    
    * revert(button): revert margin alteration from last commit
    
    * feat(circularloader): removes theme parameter and include color
    
    * fix(tests): updates tests snapshots
    
    * feat(progressbar): Creates progressBar component
    
    * fix(test): updating tests
    
    * feat(skeleton): creates skeleton component
    
    * feat(Creates skeleton subcomponents):
    
    * feat(skeleton): Creates new component to skeleton and adds examples
    
    * feat(tests): updating tests
    
    * fix(RegressionTest): Removes unused files
    
    * feat(fix props):
    
    * feat(skeletons): fix some props
    
    * feat(skeleton): changes type name
    
    * feat(skeleton): fix stories
    
    * docs(Skeleton): fix the description props of the component

---
+++ components/Skeleton/Skeleton.jsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import SkeletonTypes from './SkeletonTypes';
+import { spacing, baseFontSize as defaultBaseFontSize } from '../shared/theme';
+import {
+  SkeletonRect,
+  SkeletonCircle,
+  SkeletonText,
+  SkeletonButton,
+  SkeletonTag,
+} from './sub-components';
+
+const Skeleton = props => {
+  const { type, size, theme, height, width } = props;
+
+  const rectProps = {
+    height,
+    width,
+  };
+  const circleProps = {
+    height,
+    width,
+  };
+
+  const textProps = {
+    height,
+    width,
+  };
+
+  const buttonProps = {
+    size,
+    theme,
+    width,
+  };
+
+  const tagProps = {
+    size,
+    theme,
+    width,
+  };
+
+  return (
+    <>
+      {type === 'rect' && <SkeletonRect {...rectProps} />}
+      {type === 'circle' && <SkeletonCircle {...circleProps} />}
+      {type === 'text' && <SkeletonText {...textProps} />}
+      {type === 'button' && <SkeletonButton {...buttonProps} />}
+      {type === 'tag' && <SkeletonTag {...tagProps} />}
+    </>
+  );
+};
+
+Skeleton.defaultProps = {
+  type: 'rect',
+  width: '100%',
+  height: '14px',
+  size: 'medium',
+  theme: {
+    baseFontSize: defaultBaseFontSize,
+    spacing,
+  },
+};
+
+Skeleton.propTypes = {
+  /** Sets the component behavior */
+  type: PropTypes.oneOf(['rect', 'circle', 'text', 'button', 'tag']),
+  width: PropTypes.string,
+  /** Will affect only types that not uses the prop `size` */
+  height: PropTypes.string,
+  /** Will affect only `Tag` and `Button` types.  */
+  size: PropTypes.oneOf(['xsmall', 'small', 'medium', 'large']),
+  /** Used only for themification.  */
+  theme: PropTypes.shape({
+    spacing: PropTypes.object,
+    baseFontSize: PropTypes.number,
+  }),
+};
+
+// Types
+Skeleton.Button = SkeletonTypes.Button;
+Skeleton.Circle = SkeletonTypes.Circle;
+Skeleton.Text = SkeletonTypes.Text;
+Skeleton.Tag = SkeletonTypes.Tag;
+
+export default Skeleton;
---
+++ components/Skeleton/sub-components/SkeletonRect.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import SkeletonBase from './SkeletonBase';
+
+const SkeletonRect = styled(SkeletonBase)``;
+
+SkeletonRect.displayName = 'SkeletonRect';
+
+export default SkeletonRect;
