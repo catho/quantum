f5e14ec36d7be996733f3b1afcd2ffe11c207a62
Raphael Oliveira <rapahaeru@gmail.com>
2019-09-23 17:44:17 -0300

    docs(Colors): Update the color palette info

--- stories/Colors/Colors.jsx
+++ stories/Colors/Colors.jsx
@@ -1,14 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import {
-  Title,
-  StoryContainer,
-  SimpleHighlight,
-} from '@catho/quantum-storybook-ui';
-import Colors from '../../components/Colors';
-
-const importColors = `import { Colors } from '@catho/quantum';`;
+import { Title, StoryContainer } from '@catho/quantum-storybook-ui';
+import { colors } from '../../components/shared/theme';
 
 const ColorBackground = styled.div`
   background-color: ${({ hex }) => hex};
@@ -64,7 +58,7 @@ const ColorSample = ({ hex, number, name, fontColor }) => (
     <ColorDescription>
       <ColorProperties>
         <ColorName>
-          Colors.{name.toUpperCase()}[{number}]
+          Colors.{name}[{number}]
         </ColorName>
         <ColorHex>{hex}</ColorHex>
       </ColorProperties>
@@ -83,179 +77,126 @@ ColorSample.defaultProps = {
   fontColor: '#fff',
 };
 
-const exampleCode = `console.log(Colors.BLUE['500']);
-/*returns: #1355d0;*/
-
-console.log(Colors.PINK['500'])
-/*returns: #de0059/*
-`;
-
 export default () => (
   <StoryContainer>
-    <Title as="h2">Importing colors</Title>
-    <SimpleHighlight>{importColors}</SimpleHighlight>
-
-    <Title as="h3">Usage</Title>
-    <p>
-      Colors is a object that expose the pallete and can be used in multiple
-      places
-    </p>
-    <SimpleHighlight>{exampleCode}</SimpleHighlight>
-
-    <Title as="h2">Brand colors</Title>
     <Title as="h3" style={{ paddingTop: 0 }}>
-      Blue
+      Primary
     </Title>
 
-    <p>
-      It is the main color brand. This color passes in AAA in the contrast
-      checker of the WCAG guidelines so it can be used in various contexts like
-      buttons, texts and headings, backgrounds, graphic elements and default
-      states.
-    </p>
-
-    <ColorSample hex={Colors.BLUE[500]} number={500} name="Blue" />
+    <ColorSample hex={colors.primary['900']} number={900} name="primary" />
+    <ColorSample hex={colors.primary['700']} number={700} name="primary" />
     <ColorSample
-      hex={Colors.BLUE[200]}
-      number={200}
-      name="Blue"
+      hex={colors.primary['500']}
+      number={500}
       fontColor="black"
+      name="primary"
     />
-    <ColorSample hex={Colors.BLUE[50]} number={50} name="Blue" />
-
-    <Title as="h3">Sky</Title>
-    <p>
-      It is a secondary color that can be used only in support elements such as
-      backgrounds, graphic elements, icons, strokes and informational states. It
-      is not recommended to use this color in texts and headings because it does
-      not contrast checker from WCAG.
-    </p>
-    <ColorSample hex={Colors.SKY[500]} number={500} name="Sky" />
-
-    <Title as="h3">Cobalt</Title>
-    <p>
-      It is another secondary color that can be used in support elements like
-      backgrounds, graphic elements, icons, texts and headings. It goes through
-      AAA in the WCAG guidelines and so it is also suitable for texts.
-    </p>
-    <ColorSample hex={Colors.COBALT[500]} number={500} name="Cobalt" />
-
-    <Title as="h3">Pink</Title>
-    <p>
-      It is used mainly in contexts of great prominence as CTAs and highlights
-      Avoid using in contexts where they do not incite action or highlight.
-    </p>
-    <ColorSample hex={Colors.PINK[500]} number={500} name="Pink" />
-
-    <br />
-
-    <Title as="h2">Support colors</Title>
-    <p>
-      Tones 500 are used for icons, input and button strokes, but not are
-      indicated for texts by not passing the contrast checker of WCAG.
-      <br />
-      The 200 tones are used exclusively for backgrounds.
-      <br />
-      The 900 tones are used exclusively for texts because they pass in contrast
-      WCAG checker.
-    </p>
-
-    <Title as="h3" style={{ paddingTop: 0 }}>
-      Success
-    </Title>
-    <ColorSample hex={Colors.SUCCESS[900]} number={900} name="Success" />
-    <ColorSample hex={Colors.SUCCESS[500]} number={500} name="Success" />
     <ColorSample
-      hex={Colors.SUCCESS[200]}
-      number={200}
-      name="Success"
+      hex={colors.primary['300']}
+      number={300}
       fontColor="black"
+      name="primary"
+    />
+    <ColorSample
+      hex={colors.primary['100']}
+      number={100}
+      fontColor="black"
+      name="primary"
+    />
+
+    <Title as="h3">Secondary</Title>
+
+    <ColorSample hex={colors.secondary['900']} number={900} name="secondary" />
+    <ColorSample hex={colors.secondary['700']} number={700} name="secondary" />
+    <ColorSample hex={colors.secondary['500']} number={500} name="secondary" />
+    <ColorSample hex={colors.secondary['300']} number={300} name="secondary" />
+    <ColorSample hex={colors.secondary['100']} number={100} name="secondary" />
+
+    <Title as="h3">Success</Title>
+
+    <ColorSample hex={colors.success['900']} number={900} name="success" />
+    <ColorSample hex={colors.success['700']} number={700} name="success" />
+    <ColorSample hex={colors.success['500']} number={500} name="success" />
+    <ColorSample
+      hex={colors.success['300']}
+      number={300}
+      fontColor="black"
+      name="success"
+    />
+    <ColorSample
+      hex={colors.success['100']}
+      number={100}
+      fontColor="black"
+      name="success"
     />
 
     <Title as="h3">Warning</Title>
-    <ColorSample hex={Colors.WARNING[900]} number={900} name="Warning" />
-    <ColorSample hex={Colors.WARNING[500]} number={500} name="Warning" />
+
+    <ColorSample hex={colors.warning['900']} number={900} name="warning" />
+    <ColorSample hex={colors.warning['700']} number={700} name="warning" />
     <ColorSample
-      hex={Colors.WARNING[200]}
-      number={200}
-      name="Warning"
+      hex={colors.warning['500']}
+      number={500}
       fontColor="black"
+      name="warning"
     />
-
-    <Title as="h3">Error</Title>
-    <ColorSample hex={Colors.ERROR[900]} number={900} name="Error" />
-    <ColorSample hex={Colors.ERROR[500]} number={500} name="Error" />
     <ColorSample
-      hex={Colors.ERROR[200]}
-      number={200}
-      name="Error"
+      hex={colors.warning['300']}
+      number={300}
       fontColor="black"
+      name="warning"
     />
-
-    <br />
-
-    <Title as="h2">Neutral colors</Title>
-    <Title as="h3" style={{ paddingTop: 0 }}>
-      Neutral
-    </Title>
-    <p>
-      It is used only in situations that Neutral 900 is not sufficient. This is
-      the absolute black.
-    </p>
-    <ColorSample hex={Colors.BLACK[1000]} number={1000} name="Neutral" />
-    <p>
-      It is mainly used for texts and headings. Passes on AAA in the contrast
-      checker.
-    </p>
-    <ColorSample hex={Colors.BLACK[900]} number={900} name="Neutral" />
-    <p>
-      It is used for general texts and can also indicate active states. Pass in
-      AAA in contrast checker.
-    </p>
-    <ColorSample hex={Colors.BLACK[700]} number={700} name="Neutral" />
-    <p>
-      It is used for strokes and can also indicate inactive states. Reproves in
-      accessibility.
-    </p>
-    <ColorSample hex={Colors.BLACK[400]} number={400} name="Neutral" />
-    <p>
-      It is an auxiliary neutral tone and can be used for inactive states.
-      Reproves in accessibility.
-    </p>
-    <ColorSample hex={Colors.BLACK[200]} number={200} name="Neutral" />
-    <p>
-      It is used to indicate input fields disabled. Reproves in accessibility.
-    </p>
     <ColorSample
-      hex={Colors.BLACK[100]}
+      hex={colors.warning['100']}
       number={100}
-      name="Neutral"
       fontColor="black"
-    />
-    <p>
-      It is used only in situations that Neutral 200 is not clear enought. This
-      is the absolute white.
-    </p>
-    <ColorSample
-      hex={Colors.BLACK[0]}
-      number={0}
-      name="Neutral"
-      fontColor={Colors.BLACK[1000]}
+      name="warning"
     />
 
-    <Title as="h3">Shadow</Title>
+    <Title as="h3">error</Title>
+
+    <ColorSample hex={colors.error['900']} number={900} name="error" />
+    <ColorSample hex={colors.error['700']} number={700} name="error" />
     <ColorSample
-      hex={Colors.SHADOW[40]}
-      number={40}
-      name="Shadow"
+      hex={colors.error['500']}
+      number={500}
       fontColor="black"
+      name="error"
+    />
+    <ColorSample
+      hex={colors.error['300']}
+      number={300}
+      fontColor="black"
+      name="error"
+    />
+    <ColorSample
+      hex={colors.error['100']}
+      number={100}
+      fontColor="black"
+      name="error"
     />
 
+    <Title as="h3">Neutral</Title>
+
+    <ColorSample hex={colors.neutral['900']} number={900} name="neutral" />
+    <ColorSample hex={colors.neutral['700']} number={700} name="neutral" />
     <ColorSample
-      hex={Colors.SHADOW[50]}
-      number={50}
-      name="Shadow"
+      hex={colors.neutral['500']}
+      number={500}
       fontColor="black"
+      name="neutral"
+    />
+    <ColorSample
+      hex={colors.neutral['300']}
+      number={300}
+      fontColor="black"
+      name="neutral"
+    />
+    <ColorSample
+      hex={colors.neutral['100']}
+      number={100}
+      fontColor="black"
+      name="neutral"
     />
   </StoryContainer>
 );
