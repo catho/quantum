db855f734997ddc2c30e775f6488219f4feca492
Pedro <pedro_hmorais@hotmail.com>
2019-10-09 08:19:12 -0300

    fix(input): fix input divergences from style guide (#150)
    * fix(input): fix input divergences from style guide
    
    * fix(input): removes clear button from microsoft browsers
    
    * fix(input): Fix border radius on iphone device and the padding in search inputs
    
    * test(input): create unit test to input change event
    
    * feat(input): change the name of defaultValue prop

--- stories/Input/Input.regression-test.story.jsx
+++ stories/Input/Input.regression-test.story.jsx
@@ -5,6 +5,7 @@ import Input from '../../components/Input';
 
 storiesOf('Input', module)
   .add('default', () => <Input />)
+  .add('with default value', () => <Input value="a default value" />)
   .add('search', () => <Input type="search" />)
   .add('error', () => <Input error="message" />)
   .add('disabled', () => <Input disabled />)
