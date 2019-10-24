dfbec190313db0350ace6cc0c582b007236f5b3a
Raphael Oliveira <rapahaeru@gmail.com>
2019-10-24 14:41:19 -0300

    fix(Popover): update colors and implements new example

---
+++ stories/Popover/examples/PopoverExampleChildren.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Popover from '../../../components/Popover';
+import Button from '../../../components/Button';
+
+const PopoverExampleChildren = () => (
+  <Popover
+    trigger={<Button>This is a Popover example with chilren</Button>}
+    onClose={() => console.log('onClose prop triggered')}
+  >
+    <h5>Title of the Popover</h5>
+    Here comes a new Popover children text
+  </Popover>
+);
+
+PopoverExampleChildren.code = `import { Popover, Button } from '@catho/quantum';
+
+  <Popover
+    trigger={<Button>This is a Popover example with chilren</Button>}
+    onClose={() => console.log('onClose prop triggered')}
+  >
+    <h5>Title of the Popover</h5>
+    Here comes a new Popover children text
+  </Popover>
+`;
+
+export default PopoverExampleChildren;
