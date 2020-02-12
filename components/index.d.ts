de41208bc2aa16e92367d0c098395d1d7201b9d5
Luiz Jaccao <luiz.jaccao@catho.com>
2020-02-12 17:07:51 -0300

    fix(AUTH-153): [MD] files path

--- components/index.d.ts
+++ components/index.d.ts
@@ -1,18 +1,20 @@
 declare module "@catho/quantum"
 
+import * as React from 'react';
+
 export { default as Accordion } from "./Accordion"
 export { default as Alert } from "./Alert"
 export { default as Badge } from "./Badge"
-export { default as Breakpoints } from "./Breakpoints"
+export { default as BREAKPOINTS } from './Breakpoints';
 export { default as Button } from "./Button"
 export { default as Card } from "./Card"
-export { Checkbox, CheckboxGroup } from './components/Checkbox';
+export { Checkbox, CheckboxGroup } from './Checkbox';
 export { default as CircularLoader } from "./CircularLoader"
 export { default as Colors } from "./Colors"
 export { default as Dropdown } from "./Dropdown"
-export { Form, Validations } from './components/Form';
+export { Form, Validations } from './Form';
 export { default as GlobalStyle, IconFont, Typography } from "./GlobalStyle"
-export { Col, Container, Hide, Row } from './components/Grid';
+export { Col, Container, Hide, Row } from './Grid';
 export { default as Hamburguer } from "./Hamburguer"
 export { default as Icon } from "./Icon"
 export { default as Input } from "./Input"
@@ -27,7 +29,7 @@ export { default as shared } from "./shared"
 export { default as Skeleton } from "./Skeleton"
 export { default as SnackBar } from "./SnackBar"
 export { default as Socials } from "./Socials"
-export { Tab, TabbedView } from './components/TabbedView';
+export { Tab, TabbedView } from './TabbedView';
 export { default as Tag } from "./Tag"
 export { default as TextArea } from "./TextArea"
 export { default as Toggle } from "./Toggle"
