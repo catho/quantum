adb7eab1a0a918b48ccf3a688da60e21cf4b8b8c
Michel Araujo <araujo_michel@yahoo.com.br>
2020-02-19 13:27:11 -0300

    style(normalize-char): Rename props in normalizeChars function

--- components/shared/normalizeChars.js
+++ components/shared/normalizeChars.js
@@ -1,15 +1,15 @@
 const normalizeChars = value =>
   value.replace(
     /([àáâãäå])|([çčć])|([èéêë])|([ìíîï])|([òóôõöø])|([ùúûü])|([-_])|(['])/g,
-    (str, a, c, e, i, o, u, hifen, otherToRemove) => {
+    (str, a, c, e, i, o, u, convertToSpace, removeFromString) => {
       if (a) return 'a';
       if (c) return 'c';
       if (e) return 'e';
       if (i) return 'i';
       if (o) return 'o';
       if (u) return 'u';
-      if (hifen) return ' ';
-      if (otherToRemove) return '';
+      if (convertToSpace) return ' ';
+      if (removeFromString) return '';
       return '';
     },
   );
