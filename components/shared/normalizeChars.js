e63fa846b2017f2275de7086b4488ea65ceeb7d7
Michel Araujo <araujo_michel@yahoo.com.br>
2020-02-19 10:22:27 -0300

    feat(normalize-char): Add new char in normalizeChars function

--- components/shared/normalizeChars.js
+++ components/shared/normalizeChars.js
@@ -1,7 +1,7 @@
 const normalizeChars = value =>
   value.replace(
-    /([àáâãäå])|([çčć])|([èéêë])|([ìíîï])|([òóôõöø])|([ùúûü])|([-_])/g,
-    (str, a, c, e, i, o, u, hifen) => {
+    /([àáâãäå])|([çčć])|([èéêë])|([ìíîï])|([òóôõöø])|([ùúûü])|([-_])|(['])/g,
+    (str, a, c, e, i, o, u, hifen, otherToRemove) => {
       if (a) return 'a';
       if (c) return 'c';
       if (e) return 'e';
@@ -9,6 +9,7 @@ const normalizeChars = value =>
       if (o) return 'o';
       if (u) return 'u';
       if (hifen) return ' ';
+      if (otherToRemove) return '';
       return '';
     },
   );
