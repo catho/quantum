d835903749bf2b73e29736d413900085c6d84977
Renato Galvão <galvon@gmail.com>
2020-03-18 15:51:59 -0300

    feat(ButtonGroup): Adds to interface the new size prop

--- components/RadioGroup/index.d.ts
+++ components/RadioGroup/index.d.ts
@@ -16,47 +16,49 @@ export interface RadioProps {
 export type Radio = React.ComponentType<RadioProps>;
 
 export interface RadioButtonProps {
-    checked?: boolean;
-    children?: string;
-    skin?: 'neutral' | 'primary' | 'success' | 'warning' | 'error';
-    disabled?: boolean;
-    error?: boolean;
-    icon?: string;
-    id?: string;
-    inline?: boolean;
-    label?: string;
-    onChange?: React.ChangeEventHandler<HTMLInputElement>;
-    theme?: {
-        baseFontSize?: number;
-        spacing?: object;
-        colors?: object;
-        components?: {
-            button?: object;
-        };
+  checked?: boolean;
+  children?: string;
+  skin?: 'neutral' | 'primary' | 'success' | 'warning' | 'error';
+  size?: 'xsmall' | 'small' | 'medium' | 'large' | 'xlarge';
+  disabled?: boolean;
+  error?: boolean;
+  icon?: string;
+  id?: string;
+  inline?: boolean;
+  label?: string;
+  onChange?: React.ChangeEventHandler<HTMLInputElement>;
+  theme?: {
+    baseFontSize?: number;
+    spacing?: object;
+    colors?: object;
+    components?: {
+      button?: object;
     };
-    value: string;
+  };
+  value: string;
 }
 
 export type RadioButton = React.ComponentType<RadioButtonProps>;
 
 export interface RadioGroupProps {
-    type?: 'radio' | 'button';
-    options?: Array<{
-        label?: React.ReactNode;
-        value: string;
-        disabled?: boolean;
-    }>;
+  type?: 'radio' | 'button';
+  size?: 'xsmall' | 'small' | 'medium' | 'large' | 'xlarge';
+  options?: Array<{
+    label?: React.ReactNode;
+    value: string;
+    disabled?: boolean;
+  }>;
 
-    children?: React.ReactNode[] | React.ReactNode;
-    inline?: boolean;
-    onChange?: React.ChangeEventHandler<HTMLInputElement>;
-    value?: string;
-    error?: string;
-    theme?: {
-        colors?: object;
-        spacing?: object;
-    };
-    name: string;
+  children?: React.ReactNode[] | React.ReactNode;
+  inline?: boolean;
+  onChange?: React.ChangeEventHandler<HTMLInputElement>;
+  value?: string;
+  error?: string;
+  theme?: {
+    colors?: object;
+    spacing?: object;
+  };
+  name: string;
 }
 
 export default class RadioGroup extends React.Component<RadioGroupProps> {
