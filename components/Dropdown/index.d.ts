27bbe9f2ef43792d4ec90f513118f5336d729ad8
Pedro Mutter <pedro.mutter@gmail.com>
2020-03-30 15:20:52 -0300

    fix(Dropdown): Fix the type of onChange handler
    Added corrected types for the handler function params
    
    Co-authored-by: Pedro Souza <pedro.souza@catho.com>
    Co-authored-by: Raphael Oliveira <rapahaeru@gmail.com>

--- components/Dropdown/index.d.ts
+++ components/Dropdown/index.d.ts
@@ -1,26 +1,30 @@
 import React from 'react';
+import { ControllerStateAndHelpers } from 'downshift';
 
 export type ItemPropType = string | { value: string | number; label: string };
 
 export interface DropdownProps {
-    autocomplete?: boolean;
-    disabled?: boolean;
-    required?: boolean;
-    ignoreSpecialChars?: boolean;
-    error?: string;
-    id?: string;
-    label?: string;
-    placeholder?: string;
-    selectedItem?: ItemPropType;
-    helperText?: string;
-    /** More about stateAndHelpers parameter here https://github.com/downshift-js/downshift#children-function */
-    onChange?: (selectedItem?: React.ElementType | null, stateAndHelpers?: any) => void;
-    items?: ItemPropType[];
-    theme?: {
-        colors?: object;
-        spacing?: object;
-        baseFontSize?: number;
-    };
+  autocomplete?: boolean;
+  disabled?: boolean;
+  required?: boolean;
+  ignoreSpecialChars?: boolean;
+  error?: string;
+  id?: string;
+  label?: string;
+  placeholder?: string;
+  selectedItem?: ItemPropType;
+  helperText?: string;
+  /** More about stateAndHelpers parameter here https://github.com/downshift-js/downshift#children-function */
+  onChange?: (
+    selectedItem: ItemPropType | null,
+    stateAndHelpers: ControllerStateAndHelpers<ItemPropType>,
+  ) => void;
+  items?: ItemPropType[];
+  theme?: {
+    colors?: object;
+    spacing?: object;
+    baseFontSize?: number;
+  };
 }
 
-export default class Dropdown extends React.Component<DropdownProps> { }
+export default class Dropdown extends React.Component<DropdownProps> {}
