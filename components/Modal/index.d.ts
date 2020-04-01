83f68bb68dc63aaae1803a1c97fad034590dc75c
Tiago Celestino <190265+tcelestino@users.noreply.github.com>
2020-04-01 19:46:01 +0000

    fix(Modal): add types to static components
    #219

--- components/Modal/index.d.ts
+++ components/Modal/index.d.ts
@@ -1,17 +1,33 @@
 import React from 'react';
+import HeaderText from '../Card/HeaderText';
+import Footer from '../Card/Footer';
 
-export interface ModalProps {
-    children?: React.ReactNode[] | React.ReactNode;
-    onClose?: React.MouseEventHandler<HTMLButtonElement>;
-    closeButtonAriaLabel?: string;
-    theme?: {
-        breakpoints?: object;
-        colors?: object;
-        spacing?: object;
-        components?: {
-            button?: object;
-        };
-    };
+interface StaticProps {
+  theme?: {
+    spacing?: object;
+  };
 }
 
-export default class Modal extends React.Component<ModalProps> {}
+export type ModalHeader = React.ComponentType<StaticProps>;
+export type ModalContent = React.ComponentType<StaticProps>;
+
+export interface ModalProps {
+  children?: React.ReactNode[] | React.ReactNode;
+  onClose?: React.MouseEventHandler<HTMLButtonElement>;
+  closeButtonAriaLabel?: string;
+  theme?: {
+    breakpoints?: object;
+    colors?: object;
+    spacing?: object;
+    components?: {
+      button?: object;
+    };
+  };
+}
+
+export default class Modal extends React.Component<ModalProps> {
+  static Header: ModalHeader;
+  static HeaderText: HeaderText;
+  static Content: ModalContent;
+  static Footer: Footer;
+}
