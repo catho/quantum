3413f8a7a494a34cf81039eda7a8f92045982879
Raphael Oliveira <rapahaeru@gmail.com>
2020-11-10 12:05:44 -0300

    refactor(Form validation): update linter alerts

--- components/Form/Form.unit.test.jsx
+++ components/Form/Form.unit.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import validations from './validations';
 import Form from './Form';
 import Input from '../Input';
