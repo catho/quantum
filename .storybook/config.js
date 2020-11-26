ba3d8d10e854aa2ea4218f8661a0c30262356f64
Pedro Souza <pedroaesouza@gmail.com>
2020-11-26 17:15:17 -0300

    build(storybook): WIP - update storybook version

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,19 +2,9 @@ import React from 'react';
 import 'babel-polyfill';
 import 'loki/configure-react';
 import { configure, addDecorator } from '@storybook/react';
-import { withOptions } from '@storybook/addon-options';
 import { UiStyle } from '@catho/quantum-storybook-ui';
 import GlobalStyle from '../components/GlobalStyle';
 import stories from './stories';
-
-addDecorator(
-  withOptions({
-    name: 'Quantum',
-    showAddonPanel: false,
-    addonPanelInRight: false,
-    url: 'https://github.com/catho/quantum/',
-  }),
-);
 
 const CSSDecorator = storyFn => <>{storyFn()}</>;
 const FontDecorator = storyFn => (
---
+++ .storybook/manager.js
@@ -0,0 +1,8 @@
+import { addons } from '@storybook/addons';
+
+addons.setConfig({
+  name: 'Quantum',
+  showAddonPanel: false,
+  addonPanelInRight: false,
+  url: 'https://github.com/catho/quantum/',
+});
