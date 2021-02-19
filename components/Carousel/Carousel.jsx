40e47c1147f0e642ff91f5c19e75a714728994e9
Raphael Oliveira <rapahaeru@gmail.com>
2021-02-19 16:14:24 -0300

    chore(Carousel component): exports arrow to another component

--- components/Carousel/Carousel.jsx
+++ components/Carousel/Carousel.jsx
@@ -1,33 +1,17 @@
 import React from 'react';
 import Slider from "react-slick";
 import styled from 'styled-components';
+import Proptypes from 'prop-types';
 import { SlickSliderWrapper } from './slick'
 import  Card from "../Card";
-import Icon from '../Icon';
-import { theme } from '../shared'
-
-const { colors } = theme;
+import { colors } from '../shared/theme';
+import Arrow from './sub-components/Arrow'
 
 const AdaptedCard = styled(Card)`
   margin: 2px;
 `;
 
-const SampleNextArrow = (props) => {
-  const { className, style, onClick } = props;
-  return (
-    <Icon name="keyboard_arrow_right" skin={colors.secondary[700]} className={className} onClick={onClick}/>
-  );
-}
-
-const SamplePrevArrow = (props) => {
-  const { className, style, onClick } = props;
-  return (
-    <Icon name="keyboard_arrow_left" skin={colors.secondary[700]} className={className} onClick={onClick}/>
-  );
-}
-
-
-const Carousel = () => {
+const Carousel = ({theme}) => {
 
   const settings = {
       dots: true,
@@ -35,8 +19,8 @@ const Carousel = () => {
       speed: 500,
       slidesToShow: 3,
       slidesToScroll: 1,
-      nextArrow: <SampleNextArrow />,
-      prevArrow: <SamplePrevArrow />,
+      nextArrow: <Arrow theme={theme} targeting="right" />,
+      prevArrow: <Arrow theme={theme} targeting="left" />,
     };
 
   return (
@@ -87,5 +71,62 @@ const Carousel = () => {
     </Slider>
   </SlickSliderWrapper>
   )};
+
+    // content = [
+    //   {
+    //     image: 'dddd',
+    //     title: 'sfdsf',
+    //     description: 'htrhtrghtr',
+    //   },
+    //   {
+    //     image: 'dddd',
+    //     title: 'sfdsf',
+    //     description: 'htrhtrghtr',
+    //   },
+    // ]
+
+  Carousel.defaultProps = {
+    dots: true,
+    infinite: true,
+    cardsToShow: 3,
+    cardSize: 'medium',
+    cards: [
+      {
+        image: 'dddd',
+        title: 'sfdsf',
+        description: 'htrhtrghtr',
+      }
+    ],
+    theme: {
+      colors,
+    },
+  };
+  
+  Carousel.propTypes = {
+    /** this prop enable dots pagination */
+    dots: Proptypes.bool,
+    /** this prop enable infinite horizontal scroll */
+    infinite: Proptypes.bool,
+    /** Number of cards to show */
+    cardsToShow: Proptypes.number,
+    /** this prop sets what size of card will render */
+    cardSize: Proptypes.oneOf([
+      'small',
+      'medium',
+      'large',
+    ]),
+    /** this prop receives a object with all content to render in component */
+    cards: Proptypes.arrayOf(
+      Proptypes.shape({
+        image: Proptypes.string,
+        title: Proptypes.string,
+        description: Proptypes.string,
+      }),
+    ),
+    theme: Proptypes.shape({
+      colors: Proptypes.object,
+    }),
+  };
+  
 
 export default Carousel;
---
+++ components/Carousel/sub-components/Arrow.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Proptypes from 'prop-types';
+import Icon from '../../Icon';
+
+const Arrow = (props) => {
+  const { className, onClick, theme, targeting } = props;
+  const {colors: themeColors} = theme
+  return (
+    <Icon name={`keyboard_arrow_${targeting}`} skin={themeColors.secondary[700]} className={className} onClick={onClick}/>
+  );
+}
+  
+Arrow.defaultProps = {
+  className: undefined,
+  onClick: () => {},
+}
+  
+Arrow.propTypes = {
+  className: Proptypes.string,
+  onClick: Proptypes.func,
+  targeting: Proptypes.string.isRequired,
+  theme: Proptypes.shape({
+    colors: Proptypes.object,
+  }).isRequired,
+}
+
+export default Arrow;
