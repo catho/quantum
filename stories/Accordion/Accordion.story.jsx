df1504dc7a5d368fa08fc15654baac854ad6d213
Cael <rapahaeru@gmail.com>
2021-04-27 16:57:31 -0300

    fix(Accordion): updates documentation

--- stories/Accordion/Accordion.story.jsx
+++ stories/Accordion/Accordion.story.jsx
@@ -1,24 +1,46 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { AutoExample, Tab, SimpleHighlight } from '@catho/quantum-storybook-ui';
-import mockedProps from '../../components/Accordion/mock';
+import mockedProps, { mockCode } from '../../components/Accordion/mock';
 
 import { Accordion, Container, Row, Col } from '../../components';
 import accordionExamples from './examples';
 
-const description = 'Accordion';
+const description =
+  "An accordion is a vertical stack of interactive headings used to toggle the display of further information; each item can be 'collapsed', with just a short label visible, or 'expanded' to show the complete content.";
 const renderExampleTab = componentExamples => (
   <Tab title="Example">
+    <Container no-gutters>
+      <Row key="code-example">
+        <Col xsmall={2} small={4} medium={6}>
+          <SimpleHighlight>{mockCode}</SimpleHighlight>
+        </Col>
+        <Col xsmall={2} small={4} medium={6}>
+          <h3>Example of Object passed in Items</h3>
+          <p> This is a sample of object items passed by Items prop</p>
+        </Col>
+      </Row>
+    </Container>
     <Container>
+      <h4>
+        {' '}
+        passing this object to the items prop, the component will render this
+        way:{' '}
+      </h4>
+    </Container>
+    <Container no-gutters>
       {componentExamples.map(example => (
-        <Row key={example.code}>
-          <Col xsmall={2} small={4} medium={6}>
-            <SimpleHighlight>{example.code}</SimpleHighlight>
-          </Col>
-          <Col xsmall={2} small={4} medium={6}>
-            {example.component}
-          </Col>
-        </Row>
+        <Container no-gutters key={example.code}>
+          <h4>{example.title}</h4>
+          <Row key={example.code}>
+            <Col xsmall={2} small={4} medium={6}>
+              <SimpleHighlight>{example.code}</SimpleHighlight>
+            </Col>
+            <Col xsmall={2} small={4} medium={6}>
+              {example.component}
+            </Col>
+          </Row>
+        </Container>
       ))}
     </Container>
   </Tab>
