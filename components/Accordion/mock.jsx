7bbe4be8eef250417279d6a4fc2ccc543d056abd
Cael <rapahaeru@gmail.com>
2021-05-03 15:12:04 -0300

    fix(Fixes of code review):

--- components/Accordion/mock.jsx
+++ components/Accordion/mock.jsx
@@ -1,92 +1,119 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 
+const institucionalItem = {
+  title: 'Institucional',
+  content: (
+    <ul>
+      <li>
+        <a href="#">Blog</a>
+      </li>
+      <li>
+        <a href="#">Contatos</a>
+      </li>
+      <li>
+        <a href="#">Certificação</a>
+      </li>
+      <li>
+        <a href="#">Imprensa</a>
+      </li>
+      <li>
+        <a href="#">Mapa do site</a>
+      </li>
+    </ul>
+  ),
+};
+
+const candidateItemOpened = {
+  title: 'Para candidato',
+  content: (
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      <br />
+      Ut orci metus, auctor et viverra nec, consectetur vel diam.
+      <br />
+      Pellentesque non dui id ex fringilla posuere. Donec at hendrerit leo.
+      <br />
+      Aenean sit amet velit est.Aliquam erat volutpat.
+      <br />
+      In eu luctus ipsum. Etiam nibh neque, posuere sit amet euismod sit amet,
+      <br />
+      fringilla non nisi. Proin in pharetra tellus.
+    </p>
+  ),
+  opened: true,
+};
+
+const companiesItem = {
+  title: 'Para Empresa',
+  content: (
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      <br />
+      Ut orci metus, auctor et viverra nec, consectetur vel diam.
+      <br />
+      Pellentesque non dui id ex fringilla posuere. Donec at hendrerit leo.
+      <br />
+      Aenean sit amet velit est.Aliquam erat volutpat.
+      <br />
+      In eu luctus ipsum. Etiam nibh neque, posuere sit amet euismod sit amet,
+      <br />
+      fringilla non nisi. Proin in pharetra tellus.
+    </p>
+  ),
+};
+
+const educationItem = {
+  title: 'Educação',
+  content: (
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      <br />
+      Ut orci metus, auctor et viverra nec, consectetur vel diam.
+      <br />
+      Pellentesque non dui id ex fringilla posuere. Donec at hendrerit leo.
+      <br />
+      Aenean sit amet velit est.Aliquam erat volutpat.
+      <br />
+      In eu luctus ipsum. Etiam nibh neque, posuere sit amet euismod sit amet,
+      <br />
+      fringilla non nisi. Proin in pharetra tellus.
+    </p>
+  ),
+  onClick: () => {
+    console.log('Educação event triggered');
+  },
+};
+
 const mock = {
   items: [
-    {
-      title: 'Institucional',
-      content: (
-        <ul>
-          <li>
-            <a href="#">Blog</a>
-          </li>
-          <li>
-            <a href="#">Contatos</a>
-          </li>
-          <li>
-            <a href="#">Certificação</a>
-          </li>
-          <li>
-            <a href="#">Imprensa</a>
-          </li>
-          <li>
-            <a href="#">Mapa do site</a>
-          </li>
-        </ul>
-      ),
-    },
-    {
-      title: 'Para candidato',
-      content: (
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          <br />
-          Ut orci metus, auctor et viverra nec, consectetur vel diam.
-          <br />
-          Pellentesque non dui id ex fringilla posuere. Donec at hendrerit leo.
-          <br />
-          Aenean sit amet velit est.Aliquam erat volutpat.
-          <br />
-          In eu luctus ipsum. Etiam nibh neque, posuere sit amet euismod sit
-          amet,
-          <br />
-          fringilla non nisi. Proin in pharetra tellus.
-        </p>
-      ),
-      opened: true,
-    },
-    {
-      title: 'Para Empresa',
-      content: (
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          <br />
-          Ut orci metus, auctor et viverra nec, consectetur vel diam.
-          <br />
-          Pellentesque non dui id ex fringilla posuere. Donec at hendrerit leo.
-          <br />
-          Aenean sit amet velit est.Aliquam erat volutpat.
-          <br />
-          In eu luctus ipsum. Etiam nibh neque, posuere sit amet euismod sit
-          amet,
-          <br />
-          fringilla non nisi. Proin in pharetra tellus.
-        </p>
-      ),
-    },
-    {
-      title: 'Educação',
-      content: (
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          <br />
-          Ut orci metus, auctor et viverra nec, consectetur vel diam.
-          <br />
-          Pellentesque non dui id ex fringilla posuere. Donec at hendrerit leo.
-          <br />
-          Aenean sit amet velit est.Aliquam erat volutpat.
-          <br />
-          In eu luctus ipsum. Etiam nibh neque, posuere sit amet euismod sit
-          amet,
-          <br />
-          fringilla non nisi. Proin in pharetra tellus.
-        </p>
-      ),
-      onClick: () => {
-        console.log('Educação event triggered');
-      },
-    },
+    { ...institucionalItem },
+    { ...candidateItemOpened },
+    { ...companiesItem },
+    { ...educationItem },
   ],
+};
+
+const mockWithFirstItemOpened = {
+  items: [
+    { ...candidateItemOpened },
+    { ...institucionalItem },
+    { ...companiesItem },
+    { ...educationItem },
+  ],
+};
+
+const mockWithLastItemOpened = {
+  items: [
+    { ...institucionalItem },
+    { ...companiesItem },
+    { ...educationItem },
+    { ...candidateItemOpened },
+  ],
+};
+
+const mockWithAllItemsClosed = {
+  items: [{ ...institucionalItem }, { ...companiesItem }, { ...educationItem }],
 };
 
 const mockCode = `
@@ -144,4 +171,10 @@ const mockCode = `
   ];
 `;
 
-export { mock as default, mockCode };
+export {
+  mock as default,
+  mockCode,
+  mockWithFirstItemOpened,
+  mockWithLastItemOpened,
+  mockWithAllItemsClosed,
+};
