77dca364f2dab12405efd6f1a74a5bca7cd28801
toncabral88 <washington.cabral@catho.com>
2021-07-07 17:33:52 -0300

    chore(dependencies): Adjusts on import of babel-polyfill

--- components/shared/uniqId.js
+++ components/shared/uniqId.js
@@ -1,4 +1,4 @@
-// import 'babel-polyfill';
+import 'babel-polyfill';
 
 export default function*(prefix = '') {
   let _counter = -1;
--- jest-config.js
+++ jest-config.js
@@ -1,6 +1,5 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import 'jest-styled-components';
-import 'babel-polyfill';
 
 Enzyme.configure({ adapter: new Adapter() });
