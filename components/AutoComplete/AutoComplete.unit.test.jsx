12afe55cac2a57b7add1a37c56f2b3d56258ac14
matheusrpp <matheus.pires@catho.com>
2022-01-06 19:22:17 -0300

    test(Autocomplete): Adding more tests to improve coverage

--- components/AutoComplete/AutoComplete.unit.test.jsx
+++ components/AutoComplete/AutoComplete.unit.test.jsx
@@ -43,9 +43,74 @@ describe('AutoComplete', () => {
     component
       .find('input')
       .simulate('change', { target: { value: 'melanci' } });
+
+    expect(component.find('ul').exists()).toEqual(true);
+
     component.find('li').simulate('click');
 
     expect(component.find('ul').exists()).toEqual(false);
+    component.unmount();
+  });
+
+  it('should close the options when there is no valid option', () => {
+    const component = mount(<AutoComplete suggestions={Examples} />);
+    component.find('input').simulate('change', { target: { value: 'mon' } });
+
+    expect(component.find('ul').exists()).toEqual(false);
+    component.unmount();
+  });
+
+  it('should clean input value when clicked in the x button', () => {
+    const component = mount(<AutoComplete suggestions={Examples} />);
+    component.find('input').simulate('change', { target: { value: 'mon' } });
+
+    component.find('svg').simulate('click');
+
+    expect(component.find('input').prop('value').length).toEqual(0);
+    component.unmount();
+  });
+
+  it('should has an error icon when "error" prop is set', () => {
+    const component = mount(
+      <AutoComplete suggestions={Examples} error="error text" />,
+    );
+    const InputErrorIconElement = component.find('svg');
+
+    expect(InputErrorIconElement).toHaveLength(1);
+    component.unmount();
+  });
+
+  it('should call selectedItemMock callback when prop is setted', () => {
+    const selectedItemMock = jest.fn();
+    const component = mount(
+      <AutoComplete suggestions={Examples} selectedItem={selectedItemMock} />,
+    );
+    component.find('input').simulate('change', { target: { value: 'mora' } });
+
+    expect(selectedItemMock).toHaveBeenCalled();
+  });
+
+  it('should call selectedItemMock callback when prop is setted and user selected a option', () => {
+    const selectedItemMock = jest.fn();
+    const component = mount(
+      <AutoComplete suggestions={Examples} selectedItem={selectedItemMock} />,
+    );
+    component
+      .find('input')
+      .simulate('change', { target: { value: 'melanci' } });
+
+    component.find('li').simulate('click');
+
+    expect(selectedItemMock).toHaveBeenCalled();
+    component.unmount();
+  });
+
+  it('should has an helper text prop is set', () => {
+    const component = mount(
+      <AutoComplete suggestions={Examples} helperText="helper text" />,
+    );
+
+    expect(component.find('HelperText').exists()).toEqual(true);
     component.unmount();
   });
 });
