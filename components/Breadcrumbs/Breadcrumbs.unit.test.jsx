2c2422f2ffa7bc0467b633f4214a7c07bcc581b1
toncabral88 <washington.cabral@catho.com>
2022-01-20 13:57:18 -0400

    test(breadcrumbs): Sets unit tests for component

--- components/Breadcrumbs/Breadcrumbs.unit.test.jsx
+++ components/Breadcrumbs/Breadcrumbs.unit.test.jsx
@@ -1,11 +1,65 @@
-// Generated with scripts/create-component.js
 import React from 'react';
-import { create } from 'react-test-renderer';
+import { render, fireEvent } from '@testing-library/react';
 
 import Breadcrumbs from './Breadcrumbs';
 
+const items = [
+  {
+    label: 'Home',
+    url: 'catho.com',
+  },
+  {
+    label: 'Quantum',
+    url: 'catho.com',
+  },
+  {
+    label: 'Components',
+    url: 'catho.com',
+  },
+  {
+    label: 'Breadcrumbs',
+    url: 'catho.com',
+  },
+];
+
+const mockScrollWidth = (value = 450) => {
+  // eslint-disable-next-line no-undef
+  Object.defineProperty(HTMLElement.prototype, 'scrollWidth', {
+    configurable: true,
+    value,
+  });
+};
+
+const mockWindowInnerWidth = (value = 360) => {
+  Object.assign(window, { innerWidth: value });
+};
+
 describe('<Breadcrumbs />', () => {
   it('should match the snapshots', () => {
-    expect(create(<Breadcrumbs />).toJSON()).toMatchSnapshot();
+    const { asFragment } = render(<Breadcrumbs items={items} />);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('should match the snapshots when breadcrumbs is collapsed', () => {
+    mockScrollWidth();
+    mockWindowInnerWidth();
+
+    const { asFragment } = render(<Breadcrumbs items={items} />);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('should match the snapshots when collapsed breadcrumbs is expanded', () => {
+    mockScrollWidth();
+    mockWindowInnerWidth();
+
+    const { getByText, asFragment } = render(<Breadcrumbs items={items} />);
+
+    const firstRender = asFragment();
+
+    fireEvent.click(getByText('...'));
+
+    expect(firstRender).toMatchDiffSnapshot(asFragment());
   });
 });
