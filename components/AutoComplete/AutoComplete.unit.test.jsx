fe2e9212474804005b2a7ed4baac0e6fe178fbd4
matheusrpp <matheus.pires@catho.com>
2022-01-27 18:15:42 -0300

    test(Jest config): Adding jest-dom import to jest config file

--- components/AutoComplete/AutoComplete.unit.test.jsx
+++ components/AutoComplete/AutoComplete.unit.test.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
 import AutoComplete from './AutoComplete';
 
--- components/Button/Button.unit.test.jsx
+++ components/Button/Button.unit.test.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Button from './Button';
-import '@testing-library/jest-dom';
 import { colors } from '../shared/theme';
 
 describe('Button component', () => {
