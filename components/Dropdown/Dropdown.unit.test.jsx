fdf242353aee9e81cf20becf5c6d047141639751
matheusrpp <matheus.pires@catho.com>
2022-01-31 18:01:41 -0300

    test(Dropdown): Migrating unit tests

--- components/Dropdown/Dropdown.unit.test.jsx
+++ components/Dropdown/Dropdown.unit.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react';
 import { shallow, mount } from 'enzyme';
 import Dropdown from './Dropdown';
 
@@ -49,61 +49,73 @@ describe('Dropdown component ', () => {
   const withName = <Dropdown name={INPUT_NAME} items={ITEMS} />;
 
   it('should match the snapshot', () => {
-    expect(renderer.create(<Dropdown />).toJSON()).toMatchSnapshot();
-    expect(
-      renderer.create(<Dropdown placeholder="Dropdown placeholder" />).toJSON(),
-    ).toMatchSnapshot();
-    expect(
-      renderer.create(<Dropdown placeholder="" />).toJSON(),
-    ).toMatchSnapshot();
-    expect(
-      renderer.create(<Dropdown error="Error message" />).toJSON(),
-    ).toMatchSnapshot();
-    expect(renderer.create(<Dropdown disabled />).toJSON()).toMatchSnapshot();
-    expect(
-      renderer.create(<Dropdown label="Dropdown label" />).toJSON(),
-    ).toMatchSnapshot();
-    expect(
-      renderer.create(<Dropdown required label="Dropdown label" />).toJSON(),
-    ).toMatchSnapshot();
-    expect(
-      renderer.create(<Dropdown autocomplete />).toJSON(),
-    ).toMatchSnapshot();
-    expect(renderer.create(withItems).toJSON()).toMatchSnapshot();
-    expect(renderer.create(withSelectedItem).toJSON()).toMatchSnapshot();
-    expect(renderer.create(withImage).toJSON()).toMatchSnapshot();
-    expect(renderer.create(withName).toJSON()).toMatchSnapshot();
+    const { container: defaultDropdown } = render(<Dropdown />);
+    const { container: withPlaceHolder } = render(
+      <Dropdown placeholder="Dropdown placeholder" />,
+    );
+    const { container: withPlaceHolderNull } = render(
+      <Dropdown placeholder="" />,
+    );
+    const { container: withError } = render(<Dropdown error="Error message" />);
+    const { container: disabled } = render(<Dropdown disabled />);
+    const { container: withLabel } = render(
+      <Dropdown label="Dropdown label" />,
+    );
+    const { container: required } = render(
+      <Dropdown required label="Dropdown label" />,
+    );
+    const { container: autocomplete } = render(<Dropdown autocomplete />);
+    const { container: withItemsExample } = render(withItems);
+    const { container: withSelectedItemExample } = render(withSelectedItem);
+    const { container: withImageExample } = render(withImage);
+    const { container: withNameExample } = render(withName);
+
+    expect(defaultDropdown.firstChild).toMatchSnapshot();
+    expect(withPlaceHolder.firstChild).toMatchSnapshot();
+    expect(withPlaceHolderNull.firstChild).toMatchSnapshot();
+    expect(withError.firstChild).toMatchSnapshot();
+    expect(disabled.firstChild).toMatchSnapshot();
+    expect(withLabel.firstChild).toMatchSnapshot();
+    expect(required.firstChild).toMatchSnapshot();
+    expect(autocomplete.firstChild).toMatchSnapshot();
+    expect(withItemsExample.firstChild).toMatchSnapshot();
+    expect(withSelectedItemExample.firstChild).toMatchSnapshot();
+    expect(withImageExample.firstChild).toMatchSnapshot();
+    expect(withNameExample.firstChild).toMatchSnapshot();
   });
 
   it('should match the snapshot with dark skin', () => {
-    expect(
-      renderer.create(<Dropdown dark="skin" />).toJSON(),
-    ).toMatchSnapshot();
-    expect(
-      renderer
-        .create(<Dropdown placeholder="Dropdown placeholder" skin="dark" />)
-        .toJSON(),
-    ).toMatchSnapshot();
-    expect(
-      renderer.create(<Dropdown placeholder="" skin="dark" />).toJSON(),
-    ).toMatchSnapshot();
-    expect(
-      renderer.create(<Dropdown error="Error message" skin="dark" />).toJSON(),
-    ).toMatchSnapshot();
-    expect(
-      renderer.create(<Dropdown disabled skin="dark" />).toJSON(),
-    ).toMatchSnapshot();
-    expect(
-      renderer.create(<Dropdown label="Dropdown label" skin="dark" />).toJSON(),
-    ).toMatchSnapshot();
-    expect(
-      renderer
-        .create(<Dropdown required label="Dropdown label" skin="dark" />)
-        .toJSON(),
-    ).toMatchSnapshot();
-    expect(
-      renderer.create(<Dropdown autocomplete skin="dark" />).toJSON(),
-    ).toMatchSnapshot();
+    const { container: darkDefaultDropdown } = render(<Dropdown dark="skin" />);
+    const { container: darkWithPlaceHolder } = render(
+      <Dropdown placeholder="Dropdown placeholder" dark="skin" />,
+    );
+    const { container: darkWithPlaceHolderNull } = render(
+      <Dropdown placeholder="" dark="skin" />,
+    );
+    const { container: darkWithError } = render(
+      <Dropdown error="Error message" dark="skin" />,
+    );
+    const { container: darkDisabled } = render(
+      <Dropdown disabled dark="skin" />,
+    );
+    const { container: darkWithLabel } = render(
+      <Dropdown label="Dropdown label" dark="skin" />,
+    );
+    const { container: darkRequired } = render(
+      <Dropdown required label="Dropdown label" dark="skin" />,
+    );
+    const { container: darkAutocomplete } = render(
+      <Dropdown autocomplete dark="skin" />,
+    );
+
+    expect(darkDefaultDropdown.firstChild).toMatchSnapshot();
+    expect(darkWithPlaceHolder.firstChild).toMatchSnapshot();
+    expect(darkWithPlaceHolderNull.firstChild).toMatchSnapshot();
+    expect(darkWithError.firstChild).toMatchSnapshot();
+    expect(darkDisabled.firstChild).toMatchSnapshot();
+    expect(darkWithLabel.firstChild).toMatchSnapshot();
+    expect(darkRequired.firstChild).toMatchSnapshot();
+    expect(darkAutocomplete.firstChild).toMatchSnapshot();
   });
 
   it('should find the selected item label when its is selected', () => {
