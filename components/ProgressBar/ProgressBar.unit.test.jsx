b24c57e0d0042ac64b1151cc0cc4ad1968098272
matheusrpp <matheus.pires@catho.com>
2022-02-14 17:03:22 -0300

    test(ProgressBar component unit tests): Migrating unit tests from enzyme to react testing library

--- components/ProgressBar/ProgressBar.unit.test.jsx
+++ components/ProgressBar/ProgressBar.unit.test.jsx
@@ -1,49 +1,47 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
-import { mount } from 'enzyme';
+import { render, screen } from '@testing-library/react';
 import ProgressBar from './ProgressBar';
 
 describe('ProgressBar component', () => {
   it('Should match the snapshot of a simple progress bar', () => {
-    const simpleProgressBar = <ProgressBar />;
-    expect(renderer.create(simpleProgressBar).toJSON()).toMatchSnapshot();
+    const { container } = render(<ProgressBar />);
+    expect(container.firstChild).toMatchSnapshot();
   });
   it('Should match the snapshot of a progress bar in progress with same progressText', () => {
-    const simpleProgressBar = <ProgressBar progressPercent={30} />;
-    expect(renderer.create(simpleProgressBar).toJSON()).toMatchSnapshot();
+    const { container } = render(<ProgressBar progressPercent={30} />);
+    expect(container.firstChild).toMatchSnapshot();
   });
   it('Should match the snapshot of a progress bar in progress with same progressText', () => {
-    const simpleProgressBar = (
-      <ProgressBar progressPercent={30} progressText={30} />
+    const { container } = render(
+      <ProgressBar progressPercent={30} progressText={30} />,
     );
-    expect(renderer.create(simpleProgressBar).toJSON()).toMatchSnapshot();
+    expect(container.firstChild).toMatchSnapshot();
   });
   it('Should match the snapshot of a progressbar in progress with custom label', () => {
-    const simpleProgressBar = (
+    const { container } = render(
       <ProgressBar
         progressPercent={30}
         progressText={30}
         label=" percent loaded"
-      />
+      />,
     );
-    expect(renderer.create(simpleProgressBar).toJSON()).toMatchSnapshot();
+    expect(container.firstChild).toMatchSnapshot();
   });
   it('Should match the snapshot of a progressbar in progress with custom label and progress text', () => {
-    const simpleProgressBar = (
+    const { container } = render(
       <ProgressBar
         progressPercent={30}
         progressText={300}
         label=" files loaded from 1000"
-      />
+      />,
     );
-    expect(renderer.create(simpleProgressBar).toJSON()).toMatchSnapshot();
+    expect(container.firstChild).toMatchSnapshot();
   });
 
   it('should the label match with de props and shows correctly', () => {
     const label = ' percent processed';
-    const component = mount(<ProgressBar progressPercent={30} label={label} />);
-    const progressBarLabel = component.find('ProgressLabel').text();
+    render(<ProgressBar progressPercent={30} label={label} />);
     const labelShouldBe = `30${label}`;
-    expect(progressBarLabel).toBe(labelShouldBe);
+    expect(screen.getByText(labelShouldBe)).toBeInTheDocument();
   });
 });
