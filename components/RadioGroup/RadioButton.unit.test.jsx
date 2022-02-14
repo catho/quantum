b9eefc1180fcd9602dd2124fef82afe319f5f13c
matheusrpp <matheus.pires@catho.com>
2022-02-14 17:05:03 -0300

    test(RadioGroup component Unit tests): Migrating unit tests from enzyme to react testing library

--- components/RadioGroup/RadioButton.unit.test.jsx
+++ components/RadioGroup/RadioButton.unit.test.jsx
@@ -1,47 +1,59 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react';
 import RadioButton from './RadioButton';
 
 describe('<RadioGroup.Radio />', () => {
   describe('snapshot', () => {
     it('simple', () => {
-      const component = <RadioButton value="Foo" label="Foo" />;
-      expect(renderer.create(component).toJSON()).toMatchSnapshot();
+      const { container } = render(<RadioButton value="Foo" label="Foo" />);
+      expect(container.firstChild).toMatchSnapshot();
     });
 
     it('checked', () => {
-      const component = <RadioButton value="Foo" label="Foo" checked />;
-      expect(renderer.create(component).toJSON()).toMatchSnapshot();
+      const { container } = render(
+        <RadioButton value="Foo" label="Foo" checked />,
+      );
+      expect(container.firstChild).toMatchSnapshot();
     });
 
     it('inline', () => {
-      const component = <RadioButton value="Foo" label="Foo" inline />;
-      expect(renderer.create(component).toJSON()).toMatchSnapshot();
+      const { container } = render(
+        <RadioButton value="Foo" label="Foo" inline />,
+      );
+      expect(container.firstChild).toMatchSnapshot();
     });
 
     it('icon', () => {
-      const component = <RadioButton value="Foo" label="Foo" icon="check" />;
-      expect(renderer.create(component).toJSON()).toMatchSnapshot();
+      const { container } = render(
+        <RadioButton value="Foo" label="Foo" icon="check" />,
+      );
+      expect(container.firstChild).toMatchSnapshot();
     });
 
     it('disabled', () => {
-      const component = <RadioButton value="Foo" label="Foo" disabled />;
-      expect(renderer.create(component).toJSON()).toMatchSnapshot();
+      const { container } = render(
+        <RadioButton value="Foo" label="Foo" disabled />,
+      );
+      expect(container.firstChild).toMatchSnapshot();
     });
 
     it('error', () => {
-      const component = <RadioButton value="Foo" label="Foo" error />;
-      expect(renderer.create(component).toJSON()).toMatchSnapshot();
+      const { container } = render(
+        <RadioButton value="Foo" label="Foo" error />,
+      );
+      expect(container.firstChild).toMatchSnapshot();
     });
 
     it('disabled with an error', () => {
-      const component = <RadioButton value="Foo" label="Foo" error disabled />;
-      expect(renderer.create(component).toJSON()).toMatchSnapshot();
+      const { container } = render(
+        <RadioButton value="Foo" label="Foo" error disabled />,
+      );
+      expect(container.firstChild).toMatchSnapshot();
     });
 
     it('children', () => {
-      const component = <RadioButton value="Foo">Foo</RadioButton>;
-      expect(renderer.create(component).toJSON()).toMatchSnapshot();
+      const { container } = render(<RadioButton value="Foo">Foo</RadioButton>);
+      expect(container.firstChild).toMatchSnapshot();
     });
   });
 });
