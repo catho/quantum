06ac2e1619a37c8a172c34341a076fc27d34cdf1
toncabral88 <washington.cabral@catho.com>
2022-02-14 13:40:50 -0400

    test(pagination): Migrates unit tests to testing-library/react

--- components/Pagination/Pagination.unit.test.jsx
+++ components/Pagination/Pagination.unit.test.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
-import { mount } from 'enzyme';
+import { fireEvent, render, screen } from '@testing-library/react';
+
 import Pagination from './Pagination';
+
+const PREVIOUS_BUTTON_TEXT = 'Previous';
+const NEXT_BUTTON_TEXT = 'Next';
 
 describe('<Pagination />', () => {
   beforeEach(() => {
@@ -11,51 +14,49 @@ describe('<Pagination />', () => {
   describe('Snapshots', () => {
     it('should match snapshot without props', () => {
       expect(
-        renderer.create(<Pagination totalPages={1} />).toJSON(),
+        render(<Pagination totalPages={1} />).asFragment(),
       ).toMatchSnapshot();
     });
 
     it('should match snapshot with 5 pages', () => {
       expect(
-        renderer.create(<Pagination totalPages={5} />).toJSON(),
+        render(<Pagination totalPages={5} />).asFragment(),
       ).toMatchSnapshot();
     });
 
     it('should match snapshot with 6 pages', () => {
       expect(
-        renderer.create(<Pagination totalPages={6} />).toJSON(),
+        render(<Pagination totalPages={6} />).asFragment(),
       ).toMatchSnapshot();
     });
 
     it('should match snapshot with 7 pages', () => {
       expect(
-        renderer.create(<Pagination totalPages={7} />).toJSON(),
+        render(<Pagination totalPages={7} />).asFragment(),
       ).toMatchSnapshot();
     });
 
     it('should match snapshot with 7 pages and activePage equals 4', () => {
       expect(
-        renderer.create(<Pagination totalPages={7} activePage={4} />).toJSON(),
+        render(<Pagination totalPages={7} activePage={4} />).asFragment(),
       ).toMatchSnapshot();
     });
 
     it('should match snapshot with 7 pages and activePage equals 5', () => {
       expect(
-        renderer.create(<Pagination totalPages={7} activePage={5} />).toJSON(),
+        render(<Pagination totalPages={7} activePage={5} />).asFragment(),
       ).toMatchSnapshot();
     });
 
     it('should match snapshot with 7 pages and activePage equals 7', () => {
       expect(
-        renderer.create(<Pagination totalPages={7} activePage={5} />).toJSON(),
+        render(<Pagination totalPages={7} activePage={5} />).asFragment(),
       ).toMatchSnapshot();
     });
 
     it('should match snapshot with 100 pages and activePage equals 50', () => {
       expect(
-        renderer
-          .create(<Pagination totalPages={100} activePage={50} />)
-          .toJSON(),
+        render(<Pagination totalPages={100} activePage={50} />).asFragment(),
       ).toMatchSnapshot();
     });
 
@@ -63,28 +64,26 @@ describe('<Pagination />', () => {
       window.innerWidth = 400;
 
       expect(
-        renderer.create(<Pagination totalPages={10} />).toJSON(),
+        render(<Pagination totalPages={10} />).asFragment(),
       ).toMatchSnapshot();
     });
   });
 
   describe('onPageClick prop', () => {
     describe('Page button', () => {
-      it('should call onPageClick when button is clicked', () => {
+      it('should call onPageClick when page button is clicked', () => {
         const onPageClickMock = jest.fn();
-        const wrapper = mount(
-          <Pagination totalPages={10} onPageClick={onPageClickMock} />,
-        );
+        render(<Pagination totalPages={10} onPageClick={onPageClickMock} />);
 
-        const page = wrapper.find('PageButton');
-        page.at(0).simulate('click', { preventDefault: () => {} });
+        const page1 = screen.getByText('1');
+        fireEvent.click(page1);
 
         expect(onPageClickMock).toHaveBeenCalled();
       });
 
       it('should receive clicked page as onClick parameter when button is clicked', () => {
         const onPageClickMock = jest.fn();
-        const wrapper = mount(
+        render(
           <Pagination
             totalPages={10}
             activePage={2}
@@ -92,19 +91,17 @@ describe('<Pagination />', () => {
           />,
         );
 
-        wrapper
-          .find('PageButton')
-          .at(2)
-          .simulate('click');
+        const page2 = screen.getByText('2');
+        fireEvent.click(page2);
 
-        expect(onPageClickMock).toHaveBeenCalledWith(3, expect.any(Object));
+        expect(onPageClickMock).toHaveBeenCalledWith(2, expect.any(Object));
       });
     });
 
     describe('Action buttons', () => {
       it('should call onClick when previous button is clicked', () => {
         const onPageClickMock = jest.fn();
-        const wrapper = mount(
+        render(
           <Pagination
             totalPages={10}
             activePage={2}
@@ -112,27 +109,51 @@ describe('<Pagination />', () => {
           />,
         );
 
-        const page = wrapper.find('ActionButton');
-        page.at(0).simulate('click', { preventDefault: () => {} });
+        const previousButton = screen.getAllByText(PREVIOUS_BUTTON_TEXT);
+        fireEvent.click(previousButton[1]);
 
         expect(onPageClickMock).toHaveBeenCalled();
       });
 
-      it('should call onClick when next button is clicked', () => {
+      it(`shouldn't call onClick when previous button is clicked on the first page`, () => {
         const onPageClickMock = jest.fn();
-        const wrapper = mount(
-          <Pagination totalPages={10} onPageClick={onPageClickMock} />,
+        render(<Pagination totalPages={10} onPageClick={onPageClickMock} />);
+
+        const previousButton = screen.getAllByText(PREVIOUS_BUTTON_TEXT);
+        fireEvent.click(previousButton[1]);
+
+        expect(onPageClickMock).not.toHaveBeenCalled();
+      });
+
+      it(`shouldn't call onClick when next button is clicked on the last page`, () => {
+        const onPageClickMock = jest.fn();
+        render(
+          <Pagination
+            totalPages={10}
+            activePage={10}
+            onPageClick={onPageClickMock}
+          />,
         );
 
-        const page = wrapper.find('ActionButton');
-        page.at(1).simulate('click', { preventDefault: () => {} });
+        const NextButton = screen.getAllByText(NEXT_BUTTON_TEXT);
+        fireEvent.click(NextButton[1]);
+
+        expect(onPageClickMock).not.toHaveBeenCalled();
+      });
+
+      it('should call onClick when next button is clicked', () => {
+        const onPageClickMock = jest.fn();
+        render(<Pagination totalPages={10} onPageClick={onPageClickMock} />);
+
+        const NextButton = screen.getAllByText(NEXT_BUTTON_TEXT);
+        fireEvent.click(NextButton[1]);
 
         expect(onPageClickMock).toHaveBeenCalled();
       });
 
       it('should receive previous page as onClick parameter when previous button is clicked', () => {
         const onPageClickMock = jest.fn();
-        const wrapper = mount(
+        render(
           <Pagination
             totalPages={10}
             activePage={2}
@@ -140,17 +161,15 @@ describe('<Pagination />', () => {
           />,
         );
 
-        wrapper
-          .find('ActionButton')
-          .at(0)
-          .simulate('click');
+        const previousButton = screen.getAllByText(PREVIOUS_BUTTON_TEXT);
+        fireEvent.click(previousButton[1]);
 
         expect(onPageClickMock).toHaveBeenCalledWith(1, expect.any(Object));
       });
 
       it('should receive next page as onClick parameter when next button is clicked', () => {
         const onPageClickMock = jest.fn();
-        const wrapper = mount(
+        render(
           <Pagination
             totalPages={10}
             activePage={2}
@@ -158,10 +177,8 @@ describe('<Pagination />', () => {
           />,
         );
 
-        wrapper
-          .find('ActionButton')
-          .at(1)
-          .simulate('click');
+        const NextButton = screen.getAllByText(NEXT_BUTTON_TEXT);
+        fireEvent.click(NextButton[1]);
 
         expect(onPageClickMock).toHaveBeenCalledWith(3, expect.any(Object));
       });
@@ -170,30 +187,28 @@ describe('<Pagination />', () => {
 
   describe('Page href prop', () => {
     it('should set href value with correct value', () => {
-      const wrapper = mount(
+      render(
         <Pagination
           totalPages={10}
           activePage={2}
           pageHref={page => `/?page=${page}`}
         />,
       );
-      expect(
-        wrapper
-          .find('PageButton')
-          .at(0)
-          .prop('href'),
-      ).toBe('/?page=1');
+
+      const pageButton1 = screen.getByText('1');
+
+      expect(pageButton1.getAttribute('href')).toBe('/?page=1');
     });
   });
 
   describe('Mobile Pagination', () => {
-    beforeEach(() => {
+    beforeAll(() => {
       window.innerWidth = 400;
     });
     describe('infoFormatter prop', () => {
       it('should receive activePage and totalPages as arguments', () => {
         const infoFormatterMock = jest.fn();
-        mount(
+        render(
           <Pagination
             totalPages={10}
             activePage={1}
@@ -203,8 +218,9 @@ describe('<Pagination />', () => {
 
         expect(infoFormatterMock).toBeCalledWith(1, 10);
       });
+
       it('should format the info text of mobile pagination component', () => {
-        const wrapper = mount(
+        render(
           <Pagination
             totalPages={10}
             activePage={6}
@@ -213,72 +229,42 @@ describe('<Pagination />', () => {
             }
           />,
         );
-        expect(wrapper.find('Info').text()).toBe('6 of 10');
+
+        const info = screen.getByText('6 of 10');
+
+        expect(info).toBeInTheDocument();
+        expect(info.tagName).toBe('SPAN');
       });
     });
   });
 
   describe('Follow only first page', () => {
-    const wrapper = mount(
-      <Pagination
-        activePage={2}
-        totalPages={10}
-        pageHref={page => `/?page=${page}`}
-        followOnlyFirstPage
-      />,
-    );
+    beforeEach(() => {
+      render(
+        <Pagination
+          activePage={2}
+          totalPages={10}
+          pageHref={page => `/?page=${page}`}
+          followOnlyFirstPage
+        />,
+      );
+    });
 
     it('should not add rel="nofollow" to page 1 button', () => {
-      expect(
-        wrapper
-          .find('ActionButton')
-          .at(0)
-          .prop('rel'),
-      ).toBeUndefined();
+      const previousButton = screen.getAllByText(PREVIOUS_BUTTON_TEXT);
+      expect(previousButton[0].getAttribute('rel')).toBeNull();
 
-      expect(
-        wrapper
-          .find('PageButton')
-          .at(0)
-          .prop('rel'),
-      ).toBeUndefined();
+      const page1Button = screen.getByText('1');
+      expect(page1Button.getAttribute('rel')).toBeNull();
     });
 
     it('should add rel="nofollow" to all pagination buttons except the first one', () => {
-      expect(
-        wrapper
-          .find('PageButton')
-          .at(1)
-          .prop('rel'),
-      ).toBe('nofollow');
+      // eslint-disable-next-line no-plusplus
+      for (let i = 2; i <= 4; i++) {
+        expect(screen.getByText(`${i}`).getAttribute('rel')).toBe('nofollow');
+      }
 
-      expect(
-        wrapper
-          .find('PageButton')
-          .at(2)
-          .prop('rel'),
-      ).toBe('nofollow');
-
-      expect(
-        wrapper
-          .find('PageButton')
-          .at(3)
-          .prop('rel'),
-      ).toBe('nofollow');
-
-      expect(
-        wrapper
-          .find('PageButton')
-          .at(4)
-          .prop('rel'),
-      ).toBe('nofollow');
-
-      expect(
-        wrapper
-          .find('ActionButton')
-          .at(1)
-          .prop('rel'),
-      ).toBe('nofollow');
+      expect(screen.getByText('1').getAttribute('rel')).toBeNull();
     });
   });
 });
