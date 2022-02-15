d1703ed8edd667b749e35887e201dfdb0291d0cc
matheusrpp <matheus.pires@catho.com>
2022-02-15 11:25:18 -0300

    test(QTM-311 - Skeleton component unit tests migration): Migrating unit tests from enzyme to react t

--- components/Skeleton/Skeleton.unit.test.jsx
+++ components/Skeleton/Skeleton.unit.test.jsx
@@ -1,30 +1,32 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react';
 import Skeleton from './Skeleton';
 
 describe('Skeleton component', () => {
   it('Should match the snapshot of a simple skeleton', () => {
-    const simpleSkeleton = <Skeleton />;
-    expect(renderer.create(simpleSkeleton).toJSON()).toMatchSnapshot();
+    const { container } = render(<Skeleton />);
+    expect(container.firstChild).toMatchSnapshot();
   });
 
   it('Should match the snapshot of a text skeleton', () => {
-    const textSkeleton = <Skeleton.Text />;
-    expect(renderer.create(textSkeleton).toJSON()).toMatchSnapshot();
+    const { container } = render(<Skeleton.Text />);
+    expect(container.firstChild).toMatchSnapshot();
   });
 
   it('Should match the snapshot of a circular skeleton', () => {
-    const circularSkeleton = <Skeleton.Circle height="56px" width="56px" />;
-    expect(renderer.create(circularSkeleton).toJSON()).toMatchSnapshot();
+    const { container } = render(
+      <Skeleton.Circle height="56px" width="56px" />,
+    );
+    expect(container.firstChild).toMatchSnapshot();
   });
 
   it('Should match the snapshot of a Button component skeleton', () => {
-    const buttonSkeleton = <Skeleton.Button width="100px" />;
-    expect(renderer.create(buttonSkeleton).toJSON()).toMatchSnapshot();
+    const { container } = render(<Skeleton.Button width="100px" />);
+    expect(container.firstChild).toMatchSnapshot();
   });
 
   it('Should match the snapshot of a Tag component loader', () => {
-    const tagSkeleton = <Skeleton.Tag width="100px" />;
-    expect(renderer.create(tagSkeleton).toJSON()).toMatchSnapshot();
+    const { container } = render(<Skeleton.Tag width="100px" />);
+    expect(container.firstChild).toMatchSnapshot();
   });
 });
