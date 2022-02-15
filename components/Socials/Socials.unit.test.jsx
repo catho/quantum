d90d37fdec2b5cbe1ac9fc6d1354d63183760db5
toncabral88 <washington.cabral@catho.com>
2022-02-15 10:41:51 -0400

    test(socials): Migrates unit tests to testing-library

--- components/Socials/Socials.unit.test.jsx
+++ components/Socials/Socials.unit.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { mount } from 'enzyme';
-import toJson from 'enzyme-to-json';
+import { render } from '@testing-library/react';
+
 import Socials from './Socials';
 
 const allSocialsList = [
@@ -30,19 +30,20 @@ describe('<Socials />', () => {
     ];
 
     SOCIALS.forEach(social => {
-      const SocialComponent = mount(social);
-      expect(toJson(SocialComponent)).toMatchSnapshot();
-      SocialComponent.unmount();
+      expect(render(social).asFragment()).toMatchSnapshot();
     });
   });
 
   it('should pass title to <Title /> on SVG ', () => {
-    const component = mount(<Socials items={[allSocialsList[0]]} />);
-    expect(component.find('title').text()).toBe(allSocialsList[0].title);
+    const { container } = render(<Socials items={[allSocialsList[0]]} />);
+
+    const svg = container.querySelector('svg');
+    expect(svg.querySelector('title').innerHTML).toBe(allSocialsList[0].title);
   });
 
   it('should have the same number of social types passed in object items ', () => {
-    const wrapper = mount(<Socials items={allSocialsList} />);
-    expect(wrapper.find('ListItem').length).toBe(allSocialsList.length);
+    const { getAllByRole } = render(<Socials items={allSocialsList} />);
+
+    expect(getAllByRole('listitem')).toHaveLength(allSocialsList.length);
   });
 });
