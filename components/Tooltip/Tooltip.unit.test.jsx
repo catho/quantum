ab254b827187fa3187a6a779a05e5e61f1a09aba
toncabral88 <washington.cabral@catho.com>
2022-02-15 14:37:39 -0400

    test(tooltip): Migrates unit tests to testing-library

--- components/Tooltip/Tooltip.unit.test.jsx
+++ components/Tooltip/Tooltip.unit.test.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { fireEvent, render, screen } from '@testing-library/react';
+
 import Tooltip from './Tooltip';
 
+const TOOLTIP_TRIGGER = 'Hover me';
 const TOOLTIP_TEXT = 'This is a hint';
 const MULTILINE_TOOLTIP_TEXT = `Lorem ipsum dolor sit amet, 
 consectetur adipiscing elit. 
@@ -9,57 +11,77 @@ Aenean bibendum facilisis sem viverra fringilla.
 Suspendisse finibus libero nec justo semper.`;
 
 describe('Tooltip component ', () => {
-  describe('All positions', () => {
-    it('Should match the snapshot when place is top', () => {
-      const tooltip = mount(
-        <Tooltip place="top" text={TOOLTIP_TEXT}>
-          Hover Me
+  it('Should match the snapshot when place is top', () => {
+    expect(
+      render(
+        <Tooltip visible place="top" text={TOOLTIP_TEXT}>
+          {TOOLTIP_TRIGGER}
         </Tooltip>,
-      );
-      expect(tooltip.html()).toMatchSnapshot();
-    });
-
-    it('Should match the snapshot when place is right', () => {
-      const tooltip = mount(
-        <Tooltip place="right" text={TOOLTIP_TEXT}>
-          Hover Me
-        </Tooltip>,
-      );
-      expect(tooltip.html()).toMatchSnapshot();
-    });
-
-    it('Should match the snapshot when place is bottom', () => {
-      const tooltip = mount(
-        <Tooltip place="bottom" text={TOOLTIP_TEXT}>
-          Hover Me
-        </Tooltip>,
-      );
-      expect(tooltip.html()).toMatchSnapshot();
-    });
-
-    it('Should match the snapshot when place is left', () => {
-      const tooltip = mount(
-        <Tooltip place="left" text={TOOLTIP_TEXT}>
-          Hover Me
-        </Tooltip>,
-      );
-      expect(tooltip.html()).toMatchSnapshot();
-    });
+      ).asFragment(),
+    ).toMatchSnapshot();
   });
-  describe('Multiline', () => {
-    it('Should match the snapshot when is multiline', () => {
-      const tooltip = mount(
-        <Tooltip text={MULTILINE_TOOLTIP_TEXT} multiline>
-          Hover Me
+
+  it('Should match the snapshot when place is right', () => {
+    expect(
+      render(
+        <Tooltip visible place="right" text={TOOLTIP_TEXT}>
+          {TOOLTIP_TRIGGER}
         </Tooltip>,
-      );
-      expect(tooltip.html()).toMatchSnapshot();
-    });
-    it('Should match the snapshot when is not multiline', () => {
-      const tooltip = mount(
-        <Tooltip text={MULTILINE_TOOLTIP_TEXT}>Hover Me</Tooltip>,
-      );
-      expect(tooltip.html()).toMatchSnapshot();
-    });
+      ).asFragment(),
+    ).toMatchSnapshot();
+  });
+
+  it('Should match the snapshot when place is bottom', () => {
+    expect(
+      render(
+        <Tooltip visible place="bottom" text={TOOLTIP_TEXT}>
+          {TOOLTIP_TRIGGER}
+        </Tooltip>,
+      ).asFragment(),
+    ).toMatchSnapshot();
+  });
+
+  it('Should match the snapshot when place is left', () => {
+    expect(
+      render(
+        <Tooltip visible place="left" text={TOOLTIP_TEXT}>
+          {TOOLTIP_TRIGGER}
+        </Tooltip>,
+      ).asFragment(),
+    ).toMatchSnapshot();
+  });
+
+  it('Should match the snapshot when is multiline', () => {
+    expect(
+      render(
+        <Tooltip visible text={MULTILINE_TOOLTIP_TEXT} multiline>
+          {TOOLTIP_TRIGGER}
+        </Tooltip>,
+      ).asFragment(),
+    ).toMatchSnapshot();
+  });
+  it('Should match the snapshot when is not multiline', () => {
+    expect(
+      render(
+        <Tooltip visible text={MULTILINE_TOOLTIP_TEXT}>
+          {TOOLTIP_TRIGGER}
+        </Tooltip>,
+      ).asFragment(),
+    ).toMatchSnapshot();
+  });
+
+  it('should toggle visibility when mouse enter and mouse leave', () => {
+    render(<Tooltip text={TOOLTIP_TEXT}>{TOOLTIP_TRIGGER}</Tooltip>);
+
+    const tooltipTrigger = screen.getByText(TOOLTIP_TRIGGER);
+    fireEvent.mouseEnter(tooltipTrigger);
+
+    const tip = screen.getByText(TOOLTIP_TEXT);
+
+    expect(tip.parentNode).toHaveStyleRule('visibility', 'visible');
+
+    fireEvent.mouseLeave(tooltipTrigger);
+
+    expect(tip.parentNode).toHaveStyleRule('visibility', 'hidden');
   });
 });
