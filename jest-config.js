53f41418f4461079fee6fb3a6938847315fedf07
toncabral88 <washington.cabral@catho.com>
2022-02-16 16:07:30 -0400

    chore(dependencies): Removes enzyme from project

--- jest-config.js
+++ jest-config.js
@@ -1,10 +1,7 @@
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
 import 'jest-styled-components';
 import '@testing-library/jest-dom';
 import 'snapshot-diff/extend-expect';
 
 import matchMediaMock from './components/shared/helpers';
 
-Enzyme.configure({ adapter: new Adapter() });
 matchMediaMock();
