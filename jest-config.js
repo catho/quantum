c10a1d56e8459558fc2372f8427521115496b64a
toncabral88 <washington.cabral@catho.com>
2022-02-17 07:23:27 -0400

    chore(dependencies): Updates jest to latest version

--- jest-config.js
+++ jest-config.js
@@ -1,10 +1,7 @@
-import React from 'react';
 import 'jest-styled-components';
 import '@testing-library/jest-dom';
 import 'snapshot-diff/extend-expect';
 
 import matchMediaMock from './components/shared/helpers';
-
-global.React = React;
 
 matchMediaMock();
