f99bf2794ceef2c24e5155ea8ec4d9da859360d9
matheusrpp <matheus.pires@catho.com>
2022-04-12 14:18:13 -0300

    feat(TextArea): Adding autoResize function to textarea

--- components/TextArea/TextArea.jsx
+++ components/TextArea/TextArea.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 import { Component } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
@@ -13,16 +14,21 @@ import { spacing, colors, baseFontSize } from '../shared/theme';
 
 const ID_GENERATOR = uniqId('textarea-');
 
+const CUSTOM_HEIGHT = 108;
+
 const TextAreaTag = styled(TextInput)`
   && {
     display: block;
-    min-height: 108px;
     ${({
       theme: {
         spacing: { xsmall },
       },
+      autoResize,
     }) => `
-      margin-top: ${xsmall}px;`}
+      margin-top: ${xsmall}px;
+      min-height: ${!autoResize ? `${CUSTOM_HEIGHT}px` : 'inherit'};
+      resize: ${!autoResize ? 'auto' : 'none'};
+      `}
     transition: border 0.2s ease-in-out, box-shadow 0.2s ease-in-out;
   }
 `;
@@ -31,22 +37,50 @@ class TextArea extends Component {
   constructor(props) {
     super(props);
 
-    const { id, value } = props;
+    const {
+      id,
+      value,
+      autoResizeConfig: { rowsConfig, minRowsConfig, maxRowsConfig },
+    } = props;
 
     this.state = {
       hasDefaultValue: value !== null && value[0],
       currentValue: value,
+      rows: rowsConfig,
+      minRows: minRowsConfig,
+      maxRows: maxRowsConfig,
     };
 
     this._id = id || ID_GENERATOR.next().value;
+    this._fontSize = 16;
+    this._linHeight = 1.5;
   }
 
   onChangeTextArea = ev => {
     const { onChange } = this.props;
+    const { minRows, maxRows } = this.state;
+    const textareaLineHeightInPx = this._fontSize * this._linHeight;
+    const previousRows = ev.target.rows;
+    ev.target.rows = minRows;
+
+    const currentRows = Math.floor(
+      ev.target.scrollHeight / textareaLineHeightInPx,
+    );
+
+    if (currentRows === previousRows) {
+      ev.target.rows = currentRows;
+    }
+
+    if (currentRows >= maxRows) {
+      ev.target.rows = maxRows;
+      ev.target.scrollTop = ev.target.scrollHeight;
+    }
+
     const inputValue = ev.currentTarget.value;
     this.setState({
       currentValue: inputValue,
       hasDefaultValue: null,
+      rows: currentRows < maxRows ? currentRows : maxRows,
     });
 
     onChange(ev);
@@ -61,9 +95,11 @@ class TextArea extends Component {
       id,
       theme,
       skin,
+      autoResize,
+      autoResizeConfig,
       ...rest
     } = this.props;
-    const { hasDefaultValue, currentValue } = this.state;
+    const { hasDefaultValue, currentValue, rows } = this.state;
 
     return (
       <FieldGroup theme={theme} skin={skin}>
@@ -75,6 +111,8 @@ class TextArea extends Component {
         )}
         <TextAreaTag
           {...rest}
+          autoResize={autoResize}
+          rows={autoResize && rows}
           hasDefaultValue={hasDefaultValue}
           value={currentValue}
           error={error}
@@ -97,6 +135,13 @@ class TextArea extends Component {
 }
 
 TextArea.defaultProps = {
+  /** Disables the default resize and activates the auto resize */
+  autoResize: false,
+  autoResizeConfig: {
+    rowsConfig: 1,
+    minRowsConfig: 1,
+    maxRowsConfig: 5,
+  },
   disabled: false,
   error: '',
   helperText: '',
@@ -111,6 +156,12 @@ TextArea.defaultProps = {
 };
 
 TextArea.propTypes = {
+  autoResize: PropTypes.bool,
+  autoResizeConfig: PropTypes.shape({
+    rowsConfig: PropTypes.number,
+    minRowsConfig: PropTypes.number,
+    maxRowsConfig: PropTypes.number,
+  }),
   disabled: PropTypes.bool,
   error: PropTypes.string,
   helperText: PropTypes.string,
--- components/TextArea/index.d.ts
+++ components/TextArea/index.d.ts
@@ -2,6 +2,12 @@ import { Component, ChangeEventHandler, TextareaHTMLAttributes } from 'react';
 
 export interface TextAreaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
     disabled?: boolean;
+    autoResize?: boolean;
+    autoResizeConfig?: {
+        rowsConfig: number,
+        minRowsConfig: number,
+		maxRowsConfig: number,
+    };
     error?: string;
     helperText?: string;
     label?: string;
