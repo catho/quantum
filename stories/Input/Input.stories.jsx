699ff30d81b9fce8e537f6a02bb8d1414ebbffe9
toncabral88 <washington.cabral@catho.com>
2022-08-04 16:09:13 -0400

    docs(Input): updates documentation template

---
+++ stories/Input/Input.stories.jsx
@@ -0,0 +1,99 @@
+import { useState } from 'react';
+import { Input } from '../../components';
+
+const Template = args => <Input {...args} />;
+
+const TemplateWithType = ({ staticType, ...otherArgs }) => {
+  const InputWithType = Input[staticType];
+
+  return <InputWithType {...otherArgs} />;
+};
+
+export const Default = Template.bind({});
+
+export const Label = Template.bind({});
+Label.args = {
+  label: 'With Label',
+};
+
+export const Error = Template.bind({});
+Error.args = {
+  error: 'Some error',
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  disabled: true,
+};
+
+export const DescriptionLabel = Template.bind({});
+DescriptionLabel.args = {
+  descriptionLabel: 'Some description label',
+};
+
+export const HelperText = Template.bind({});
+HelperText.args = {
+  helperText: 'Some helper text',
+};
+
+export const Searchable = Template.bind({});
+Searchable.args = {
+  type: 'search',
+};
+
+export const CEP = TemplateWithType.bind({});
+CEP.args = {
+  staticType: 'CEP',
+};
+
+export const CNPJ = TemplateWithType.bind({});
+CNPJ.args = {
+  staticType: 'CNPJ',
+};
+
+export const CPF = TemplateWithType.bind({});
+CPF.args = {
+  staticType: 'CPF',
+};
+
+export const Date = TemplateWithType.bind({});
+Date.args = {
+  staticType: 'Date',
+};
+
+export const Phone = TemplateWithType.bind({});
+Phone.args = {
+  staticType: 'Phone',
+};
+
+export const Password = TemplateWithType.bind({});
+Password.args = {
+  staticType: 'Password',
+};
+
+export const Skin = Template.bind({});
+Skin.args = {
+  skin: 'dark',
+};
+
+export const ControlledExample = props => {
+  const [value, setValue] = useState('This is an example text');
+
+  const onClean = () => {
+    setValue('');
+  };
+
+  const onChange = event => {
+    setValue(event.target.value);
+  };
+
+  return (
+    <Input
+      {...props}
+      label="Controlled example"
+      value={value}
+      onClean={onClean}
+      onChange={onChange}
+    />
+  );
+};
