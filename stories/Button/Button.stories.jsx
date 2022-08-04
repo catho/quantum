a78058256665d0052e9d0558924c07624e83b78e
Marcos Conceicao <marcos.conceicao@catho.com>
2022-08-04 14:47:02 -0300

    docs: Refactored example button list and other tweaks

--- stories/Button/Button.stories.jsx
+++ stories/Button/Button.stories.jsx
@@ -7,21 +7,25 @@ const Template = args => {
 
 export const Default = Template.bind({});
 Default.args = {
-  full: true,
   chidlren: 'Default',
 };
 
 export const FullWidth = Template.bind({});
 FullWidth.args = {
   full: true,
-  chidlren: 'FullWidth',
+  chidlren: 'Full width',
 };
 
 export const Centered = Template.bind({});
 Centered.args = {
   center: true,
-  skin: 'success',
   chidlren: 'Centered',
+};
+
+export const Success = Template.bind({});
+Success.args = {
+  skin: 'success',
+  chidlren: 'Success',
 };
 
 export const Secondary = Template.bind({});
@@ -30,48 +34,19 @@ Secondary.args = {
   chidlren: 'Secondary',
 };
 
-export const WithInfoIcon = Template.bind({});
-WithInfoIcon.args = {
-  icon: 'info',
-  chidlren: 'Info',
-};
-
-export const WithSearchIcon = Template.bind({});
-WithSearchIcon.args = {
-  icon: 'search',
-  chidlren: 'Search',
-};
-
-export const WithAccessibleIcon = Template.bind({});
-WithAccessibleIcon.args = {
-  icon: 'accessible_forward',
-  skin: 'secondary',
-  chidlren: 'Accessible',
-};
-
-export const WithErrorIcon = Template.bind({});
-WithErrorIcon.args = {
-  icon: 'error',
+export const Error = Template.bind({});
+Error.args = {
   skin: 'error',
   chidlren: 'Error',
 };
 
-export const WithCreditCardIcon = Template.bind({});
-WithCreditCardIcon.args = {
-  icon: 'credit_card',
-  chidlren: 'Credit Card',
-};
-
-export const WithLockedIcon = Template.bind({});
-WithLockedIcon.args = {
-  icon: 'lock',
+export const Disabled = Template.bind({});
+Disabled.args = {
   disabled: true,
-  chidlren: 'Locked',
+  chidlren: 'Disabled',
 };
 
-export const WithDoneIcon = Template.bind({});
-WithDoneIcon.args = {
-  icon: 'done',
-  chidlren: 'Done',
-  skin: 'warning',
+export const WithIcon = Template.bind({});
+WithIcon.args = {
+  icon: 'info',
 };
