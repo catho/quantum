72667a491b26b45770716afc5b0a1b4eab56ec0c
Marcos Conceicao <marcos.conceicao@catho.com>
2022-08-04 15:14:23 -0300

    docs: Included examples for different sizes and fits

--- stories/Button/Button.stories.jsx
+++ stories/Button/Button.stories.jsx
@@ -1,49 +1,79 @@
 import Button from '../../components/Button';
 
 const Template = args => {
-  const { chidlren, ...rest } = args;
-  return <Button {...rest}>{chidlren}</Button>;
+  const { children, ...rest } = args;
+  return <Button {...rest}>{children}</Button>;
 };
 
 export const Default = Template.bind({});
 Default.args = {
-  chidlren: 'Default',
+  children: 'Default',
 };
 
 export const FullWidth = Template.bind({});
 FullWidth.args = {
   full: true,
-  chidlren: 'Full width',
+  children: 'Full width',
 };
 
 export const Centered = Template.bind({});
 Centered.args = {
   center: true,
-  chidlren: 'Centered',
+  children: 'Centered',
 };
 
 export const Success = Template.bind({});
 Success.args = {
   skin: 'success',
-  chidlren: 'Success',
+  children: 'Success',
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
   skin: 'secondary',
-  chidlren: 'Secondary',
+  children: 'Secondary',
 };
 
 export const Error = Template.bind({});
 Error.args = {
   skin: 'error',
-  chidlren: 'Error',
+  children: 'Error',
 };
 
 export const Disabled = Template.bind({});
 Disabled.args = {
   disabled: true,
-  chidlren: 'Disabled',
+  children: 'Disabled',
+};
+
+export const Xsmall = Template.bind({});
+Xsmall.args = {
+  size: 'xsmall',
+  children: 'xsmall',
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  size: 'small',
+  children: 'small',
+};
+
+export const Medium = Template.bind({});
+Medium.args = {
+  size: 'medium',
+  children: 'medium',
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: 'large',
+  children: 'large',
+};
+
+export const Xlarge = Template.bind({});
+Xlarge.args = {
+  size: 'xlarge',
+  children: 'xlarge',
 };
 
 export const WithIcon = Template.bind({});
