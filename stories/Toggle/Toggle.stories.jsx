161abea8038e8be6a74f0e9c05d25f4a689bda8f
Marcos Conceicao <marcos.conceicao@catho.com>
2022-08-05 12:00:00 -0300

    docs: Updated documentation template

---
+++ stories/Toggle/Toggle.stories.jsx
@@ -0,0 +1,41 @@
+import Toggle from '../../components/Toggle';
+
+const Template = args => {
+  const { label, labelPosition, id, ...rest } = args;
+  return (
+    <div style={{ display: 'flex', alignContent: 'center' }}>
+      {labelPosition === 'left' && (
+        <label htmlFor={id} style={{ marginRight: 10 }}>
+          {label}
+        </label>
+      )}
+      <Toggle id={id} {...rest} />
+      {labelPosition === 'right' && (
+        <label htmlFor={id} style={{ marginLeft: 10 }}>
+          {label}
+        </label>
+      )}
+    </div>
+  );
+};
+
+export const Default = Template.bind({});
+
+export const Checked = Template.bind({});
+Checked.args = {
+  checked: true,
+};
+
+export const WithLeftLabel = Template.bind({});
+WithLeftLabel.args = {
+  id: 'left-toggle',
+  label: 'Left label',
+  labelPosition: 'left',
+};
+
+export const WithRightLabel = Template.bind({});
+WithRightLabel.args = {
+  id: 'right-toggle',
+  label: 'Right label',
+  labelPosition: 'right',
+};
