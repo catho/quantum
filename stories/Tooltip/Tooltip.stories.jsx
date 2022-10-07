2f3cb88ff585d25d71fc49491ee7e3218d40c9e9
Marcos Conceicao <marcos.conceicao@catho.com>
2022-10-07 12:29:19 -0300

    docs: Tooltip documentation template

---
+++ stories/Tooltip/Tooltip.stories.jsx
@@ -0,0 +1,29 @@
+import { Row, Col, Tooltip } from '../../components';
+
+const Template = args => (
+  <Row style={{ paddingTop: '72px' }}>
+    <Col>
+      <Tooltip text="Text..." {...args}>
+        Tooltip trigger
+      </Tooltip>
+    </Col>
+  </Row>
+);
+
+export const Default = Template.bind({});
+
+export const Multiline = Template.bind({});
+Multiline.args = {
+  text: 'Multiline\ntext...',
+  multiline: true,
+};
+
+export const WithPlacement = Template.bind({});
+WithPlacement.args = {
+  placement: 'right',
+};
+
+export const Visible = Template.bind({});
+Visible.args = {
+  visible: true,
+};
