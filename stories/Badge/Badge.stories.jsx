8d5e383b6417018162a7bddfd684358419043439
Marcos Conceicao <marcos.conceicao@catho.com>
2022-10-10 16:48:54 -0300

    docs(Badge): Updated documentation template

---
+++ stories/Badge/Badge.stories.jsx
@@ -0,0 +1,35 @@
+import Badge from '../../components/Badge';
+import Button from '../../components/Button';
+
+const Template = args => {
+  const { children: Component, ...rest } = args;
+  return <Badge {...rest}>{Component}</Badge>;
+};
+
+export const Default = Template.bind({});
+
+export const WithChildren = Template.bind({});
+WithChildren.args = {
+  children: <Button>Test</Button>,
+};
+
+export const WithSkin = Template.bind({});
+WithSkin.args = {
+  skin: 'secondary',
+};
+
+export const CustomNumber = Template.bind({});
+CustomNumber.args = {
+  number: 10,
+};
+
+export const Dot = Template.bind({});
+Dot.args = {
+  dot: true,
+  skin: 'secondary',
+};
+
+export const Inverted = Template.bind({});
+Inverted.args = {
+  inverted: true,
+};
