e9b96ff9a227090a02e5b376d06fe2a68b54b83f
douglasjr97 <falcao.douglas97@gmail.com>
2022-10-20 17:14:04 -0300

    chore: updated documentation

--- stories/TabbedView/TabbedView.stories.jsx
+++ stories/TabbedView/TabbedView.stories.jsx
@@ -1,12 +1,43 @@
-import { TabbedView, Tab } from '../../components';
+import { TabbedView, Tab, Icon, Badge } from '../../components';
 
-const Template = args => (
-  <TabbedView {...args}>
-    <Tab title="Candidates">Candidates content</Tab>
-    <Tab title="Companies">Companies content</Tab>
-    <Tab title="Education">Education content</Tab>
-  </TabbedView>
-);
+const itemsMock = [
+  { title: 'Candidates', content: 'Candidates content' },
+  { title: 'Companies', content: 'Companies content' },
+  { title: 'Education', content: 'Education content' },
+];
+
+const itemsWithIconMock = [
+  {
+    title: 'Candidates',
+    content: 'Candidates content',
+    icon: <Icon name="star" />,
+  },
+  {
+    title: 'Companies',
+    content: 'Companies content',
+    badge: <Badge number={4} />,
+  },
+  {
+    title: 'Education',
+    content: 'Education content',
+    badge: <Badge number={4} />,
+    icon: <Icon name="star" />,
+  },
+];
+
+const Template = args => {
+  const { items = itemsMock, ...rest } = args;
+
+  return (
+    <TabbedView {...rest}>
+      {items.map(({ title, content, ...itemRest }) => (
+        <Tab key={title} title={title} {...itemRest}>
+          {content}
+        </Tab>
+      ))}
+    </TabbedView>
+  );
+};
 
 export const Default = Template.bind({});
 
@@ -19,7 +50,14 @@ export const WithSkin = Template.bind({});
 WithSkin.args = {
   skin: 'primary',
 };
+
 export const ActiveTab = Template.bind({});
 ActiveTab.args = {
   activeTab: 'Companies',
+};
+
+export const WithIconAndBadge = Template.bind({});
+WithIconAndBadge.args = {
+  skin: 'primary',
+  items: itemsWithIconMock,
 };
