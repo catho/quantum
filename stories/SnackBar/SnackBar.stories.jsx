cce77b1ffe1889c433d93b7c50db67fe573da08a
Marcos Conceicao <marcos.conceicao@catho.com>
2022-10-24 17:18:41 -0300

    docs(SnackBar): Updated documentation template

---
+++ stories/SnackBar/SnackBar.stories.jsx
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+import SnackBar from '../../components/SnackBar';
+import { Button } from '../../components';
+
+const Template = args => {
+  const { actionTrigger, ...rest } = args;
+  const [openSnackBar, setOpenSnackbar] = useState(false);
+
+  return (
+    <>
+      <Button onClick={() => setOpenSnackbar(true)}>Open snackbar</Button>
+      {openSnackBar && (
+        <SnackBar
+          onClose={() => setOpenSnackbar(false)}
+          actionTrigger={
+            actionTrigger && {
+              ...actionTrigger,
+              callbackFn: () => setOpenSnackbar(false),
+            }
+          }
+          {...rest}
+        />
+      )}
+    </>
+  );
+};
+
+export const Default = Template.bind({});
+
+export const WithText = Template.bind({});
+WithText.args = {
+  text:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+};
+
+export const WithSkin = Template.bind({});
+WithSkin.args = {
+  skin: 'success',
+};
+
+export const WithHideIcon = Template.bind({});
+WithHideIcon.args = {
+  skin: 'success',
+  hideIcon: true,
+};
+
+export const WithSecondsToClose = Template.bind({});
+WithSecondsToClose.args = {
+  secondsToClose: 3,
+};
+
+export const Inverted = Template.bind({});
+Inverted.args = {
+  inverted: true,
+  actionTrigger: {
+    title: 'HIDE',
+    callbackFn: () => {},
+  },
+};
+
+export const WithActionTrigger = Template.bind({});
+WithActionTrigger.args = {
+  actionTrigger: {
+    title: 'HIDE',
+    callbackFn: () => {},
+  },
+};
