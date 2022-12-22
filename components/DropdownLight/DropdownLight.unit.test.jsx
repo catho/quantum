6278ba32375449aef7c7e6f7df962108635a2a60
douglasjr97 <falcao.douglas97@gmail.com>
2022-12-22 16:33:12 -0300

    test: (QTM-428) - Improve from covarage tests

--- components/DropdownLight/DropdownLight.unit.test.jsx
+++ components/DropdownLight/DropdownLight.unit.test.jsx
@@ -1,14 +1,40 @@
 // Generated with scripts/create-component.js
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import DropdownLight from './DropdownLight';
 
-import { itemsStringMock } from '../../stories/DropdownLight/mock';
+import {
+  itemsStringMock,
+  itemsObjectMock,
+} from '../../stories/DropdownLight/mock';
 
 describe('<DropdownLight />', () => {
   it('should match the snapshots', () => {
     expect(
       render(<DropdownLight items={itemsStringMock} />).asFragment(),
     ).toMatchSnapshot();
+  });
+
+  it('should return a value from item, using a object items', () => {
+    render(<DropdownLight items={itemsObjectMock} />);
+
+    const dropdown = screen.getByRole('combobox');
+    userEvent.selectOptions(dropdown, 'Lemon');
+
+    const lemonItem = itemsObjectMock[0].value.toString();
+
+    expect(dropdown.value).toEqual(lemonItem);
+  });
+
+  it('should return a value from item, using a string items', () => {
+    render(<DropdownLight items={itemsStringMock} />);
+
+    const dropdown = screen.getByRole('combobox');
+    userEvent.selectOptions(dropdown, 'Lemon');
+
+    const lemonItem = itemsStringMock[0];
+
+    expect(dropdown.value).toEqual(lemonItem);
   });
 });
