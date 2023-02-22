87064f2f4ab928603357bf1e7f54f7aabd3ef8f6
douglasjr97 <falcao.douglas97@gmail.com>
2023-02-22 12:44:38 -0300

    chore: refactored preconnect and preload

--- components/GlobalStyle/sub-components/Typography.jsx
+++ components/GlobalStyle/sub-components/Typography.jsx
@@ -90,17 +90,21 @@ ul, ol {
 const Typography = () => (
   <>
     <Style />
+
     <link
+      rel="preconnect"
+      href="https://fonts.gstatic.com"
       crossOrigin="anonymous"
+    />
+    <link
       rel="preload"
       href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,600,700,400i,600i,700i&display=swap"
       as="font"
-      type="font/woff2"
+      crossOrigin="anonymous"
     />
     <link
       href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,600,700,400i,600i,700i&display=swap"
       rel="stylesheet"
-      as="style"
     />
   </>
 );
