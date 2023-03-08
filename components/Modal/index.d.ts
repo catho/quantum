fcbc0da84ffc212446186ce8cd5ea3b6c77b2cc9
douglasjr97 <falcao.douglas97@gmail.com>
2023-03-08 18:04:50 -0300

    chore: Modal - remove static remnants

--- components/Modal/index.d.ts
+++ components/Modal/index.d.ts
@@ -1,13 +1,4 @@
-import { ComponentType, ReactNode, TouchEvent, MouseEventHandler } from 'react';
-
-interface StaticProps {
-  theme?: {
-    spacing?: {};
-  };
-}
-
-export type ModalHeader = ComponentType<StaticProps>;
-export type ModalContent = ComponentType<StaticProps>;
+import { ReactNode, TouchEvent, MouseEventHandler } from 'react';
 
 export interface ModalProps {
   children?: ReactNode[] | ReactNode;
@@ -24,7 +15,6 @@ export interface ModalProps {
   };
 }
 
-  
   interface ModalHeaderProps {
     theme?: {
       spacing?: {};
@@ -50,9 +40,6 @@ interface ModalFooterProps {
     spacing?: {};
   };
 }
-
-
-
 
 interface CompoundedModal extends React.FunctionComponent<ModalProps> {
   Header: React.FunctionComponent<ModalHeaderProps>;
