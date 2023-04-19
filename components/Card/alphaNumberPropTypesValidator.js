ad0f537015a33421c1376cb32f5c0b6870357f96
Marcos Conceicao <marcos.conceicao@catho.com>
2023-04-19 14:39:58 -0300

    feat(Card.jsx): Added property to adjust Card background opacity

---
+++ components/Card/alphaNumberPropTypesValidator.js
@@ -0,0 +1,19 @@
+export const isValidAlphaNumber = value => {
+  if (typeof value === 'number' && value >= 0 && value <= 1) {
+    return true;
+  }
+
+  return false;
+};
+
+export default (props, propName, componentName) => {
+  const alphaNumber = props[propName];
+
+  if (alphaNumber && !isValidAlphaNumber(alphaNumber)) {
+    return new Error(
+      `Invalid prop \`${propName}\` supplied to ${componentName}. Required \`number\` in the range [0, 1].`,
+    );
+  }
+
+  return null;
+};
