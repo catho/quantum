e06aa30ba72083db5a52e75e8d4f9837eeddc728
Marcos Conceicao <marcos.conceicao@catho.com>
2023-06-26 12:22:59 -0300

    chore: Adapting import of components and icons from the material to the mui

--- components/Avatar/Avatar.jsx
+++ components/Avatar/Avatar.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import Person from '@material-ui/icons/Person';
+import Person from '@mui/icons-material/Person';
 import Icon from '../Icon/Icon';
 import { colors } from '../shared/theme';
 
