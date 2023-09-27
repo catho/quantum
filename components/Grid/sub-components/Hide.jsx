0b2016f10b3eb8914dbff8ab12108fb9d90b27c9
Marcos Conceicao <marcos.conceicao@catho.com>
2023-09-27 15:09:23 -0300

    Mock Row, Hide e Container para server components

--- components/Grid/sub-components/Hide.jsx
+++ components/Grid/sub-components/Hide.jsx
@@ -1,35 +1,3 @@
-import PropTypes from 'prop-types';
-import styled from 'styled-components';
-import { hideQueries } from './shared/media';
-import theme from '../../shared/theme';
-
-const Hide = styled.div`
-  ${({ theme: { breakpoints }, xsmall, small, medium, large }) => {
-    let style = '';
-
-    const hideBreakpoint = hideQueries(breakpoints);
-
-    style += xsmall ? hideBreakpoint.xsmall() : '';
-    style += small ? hideBreakpoint.small() : '';
-    style += medium ? hideBreakpoint.medium() : '';
-    style += large ? hideBreakpoint.large() : '';
-
-    return style;
-  }}
-`;
-
-Hide.defaultProps = { theme };
-
-Hide.propTypes = {
-  large: PropTypes.bool,
-  medium: PropTypes.bool,
-  small: PropTypes.bool,
-  xsmall: PropTypes.bool,
-  theme: PropTypes.shape({
-    breakpoints: PropTypes.object,
-  }),
-};
-
-Hide.displayName = 'Hide';
+const Hide = ({ children }) => <div>{children}</div>;
 
 export default Hide;
--- components/Grid/sub-components/index.js
+++ components/Grid/sub-components/index.js
@@ -1,1 +1,4 @@
 export { default as Col } from './Col';
+export { default as Hide } from './Hide';
+export { default as Container } from './Container';
+export { default as Row } from './Row';
