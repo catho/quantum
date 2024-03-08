38059ae3cdee4378d7afb6d85a0e6edcca286743
Marcos Conceicao <marcos.conceicao@catho.com>
2024-03-08 18:09:09 -0300

    build(jest-config.js): Injecting component css styles into the ducument.head of the jest testing env

--- jest-config.js
+++ jest-config.js
@@ -1,6 +1,7 @@
 import 'jest-styled-components';
 import '@testing-library/jest-dom';
 import 'snapshot-diff/extend-expect';
+import './scripts/injectCssInDocument';
 
 import matchMediaMock from './components/shared/helpers';
 
