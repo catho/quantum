0e44d85bccdb7ce4be6c33e663bc17ac5952806f
Marcos Conceicao <marcos.conceicao@catho.com>
2024-04-05 14:59:26 -0300

    docs(main.js): postcss-loader for storybook load styles from postcss settings

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,11 @@
 const stories = require('./stories');
+
+const isRuleForStyles = (rule) =>
+  rule.test instanceof RegExp &&
+  (rule.test.test('.css') ||
+    rule.test.test('.scss') ||
+    rule.test.test('.less'));
+
 const config = {
   stories,
   staticDirs: ['static'],
@@ -17,6 +24,39 @@ const config = {
   framework: {
     name: '@storybook/react-webpack5',
     options: {},
+  },
+  webpackFinal: async (config) => {
+    const rulesWithoutCssRuleDefault = config.module.rules.filter(
+      (rule) => typeof rule === 'object' && !isRuleForStyles(rule),
+    );
+
+    const rulesWithPostCSSLoader = [...rulesWithoutCssRuleDefault];
+    rulesWithPostCSSLoader.push({
+      test: /\.((c|sa|sc)ss)$/i,
+      use: [
+        'style-loader',
+        {
+          loader: 'css-loader',
+          options: {
+            importLoaders: 1,
+            modules: {
+              localIdentName: '[local]___[hash:base64:5]',
+            },
+          },
+        },
+        {
+          loader: 'postcss-loader',
+          options: {
+            implementation: require.resolve('postcss'),
+          },
+        },
+      ],
+    });
+
+    return {
+      ...config,
+      module: { ...config.module, rules: [...rulesWithPostCSSLoader] },
+    };
   },
 };
 
