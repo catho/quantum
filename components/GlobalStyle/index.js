73d406b174faa44d8f9766e1d0056e88f71d46de
Marcos Conceicao <marcos.conceicao@catho.com>
2024-05-15 12:45:20 -0300

    chore: Removing unused files and functions

--- components/GlobalStyle/index.js
+++ components/GlobalStyle/index.js
@@ -3,7 +3,6 @@ import Typography from './sub-components/Typography';
 import Title from './sub-components/Title';
 import Subtitle from './sub-components/Subtitle';
 import Blockquote from './sub-components/Blockquote';
-import CSSVariables from './sub-components/CSSVariables';
 
-export { Typography, Title, Subtitle, Blockquote, CSSVariables };
+export { Typography, Title, Subtitle, Blockquote };
 export default GlobalStyle;
