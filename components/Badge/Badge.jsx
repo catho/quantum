3597ef7e26ba1922e6ae472e9f2b9a47b7c393a1
Marcos Conceicao <marcos.conceicao@catho.com>
2024-05-28 09:40:29 -0300

    feat(Badge.jsx): Converted styles from styled components to CSS modules

--- components/Badge/Badge.jsx
+++ components/Badge/Badge.jsx
@@ -1,117 +1,33 @@
 import PropTypes from 'prop-types';
-import styled, { css } from 'styled-components';
-import {
-  components,
-  spacing,
-  baseFontSize as defaultBaseFontSize,
-} from '../shared/theme';
-
-const getColors = ({
-  skin,
-  theme: {
-    components: {
-      badge: {
-        skins: { [skin]: badgeColor },
-      },
-    },
-  },
-  inverted,
-}) => {
-  if (inverted) {
-    const [text, background] = [badgeColor.background, badgeColor.text];
-    return css`
-      background-color: ${background};
-      color: ${text};
-    `;
-  }
-  return css`
-    background-color: ${badgeColor.background};
-    color: ${badgeColor.text};
-  `;
-};
-
-const BadgeWrapper = styled.div`
-  display: inline-block;
-
-  ${(props) =>
-    props.originalChildren
-      ? `position: relative;`
-      : `margin-left: 8px; margin-right: 8px;`}
-`;
-
-const StyledBadge = styled.span`
-  border-radius: 8px;
-  box-sizing: border-box;
-  display: flex;
-  font-weight: bold;
-  height: 24px;
-  line-height: 24px;
-  min-width: 32px;
-  align-items: center;
-  justify-content: center;
-
-  ${({
-    value,
-    theme: {
-      spacing: { xxxsmall, xxsmall },
-      baseFontSize,
-    },
-    number,
-  }) => {
-    const padding =
-      !Number.isInteger(value) || number >= 10 ? xxsmall : xxxsmall;
-
-    return `
-        font-size: ${baseFontSize * 0.75}px;
-        padding-left: ${padding}px;
-        padding-right: ${padding}px;
-      `;
-  }}
-
-  ${(props) =>
-    props.originalChildren &&
-    `
-    position: absolute;
-    right: -10px;
-    top: -10px;
-  `}
-
-  ${(props) =>
-    props.dot &&
-    `
-    right: -2px;
-    top: 0;
-    height: 12px;
-    width: 12px;
-    min-width: 12px;
-  `}
-
-  ${getColors}
-`;
+import classNames from 'classnames';
+import styles from './Badge.module.css';
 
 /** This components is used to display only `Numbers`. If you want to pass a string, use `<Tag />` component instead */
-const Badge = ({ children, number, skin, inverted, dot, theme }) => {
+const Badge = ({ className, children, number, skin, inverted, dot }) => {
   const value = number > 99 ? '99+' : number;
+  const wrapperClass = classNames(
+    styles.wrapper,
+    {
+      [styles['wrapper-content']]: children,
+    },
+    className,
+  );
+
+  const badgeClass = classNames(styles.badge, styles[`badge-skin-${skin}`], {
+    [styles['badge-xxsmall']]: !Number.isInteger(value),
+    [styles['badge-content']]: children,
+    [styles['badge-dot']]: dot,
+    [styles[`badge-skin-${skin}-inverted`]]: inverted,
+  });
 
   return (
-    <BadgeWrapper value={value} originalChildren={children}>
-      <StyledBadge
-        skin={skin}
-        inverted={inverted}
-        theme={theme}
-        value={value}
-        originalChildren={children}
-        dot={dot}
-      >
-        {dot || value}
-      </StyledBadge>
+    <div className={wrapperClass}>
+      <span className={badgeClass}>{dot || value}</span>
       {children}
-    </BadgeWrapper>
+    </div>
   );
 };
 
-BadgeWrapper.displayName = 'BadgeWrapper';
-StyledBadge.displayName = 'StyledBadge';
 Badge.displayName = 'Badge';
 
 Badge.propTypes = {
@@ -134,13 +50,6 @@ Badge.propTypes = {
   inverted: PropTypes.bool,
   /** Shows only a dot, without any number. */
   dot: PropTypes.bool,
-  theme: PropTypes.shape({
-    baseFontSize: PropTypes.number,
-    spacing: PropTypes.object,
-    components: PropTypes.shape({
-      badge: PropTypes.object,
-    }),
-  }),
 };
 
 Badge.defaultProps = {
@@ -149,13 +58,6 @@ Badge.defaultProps = {
   children: '',
   number: 0,
   dot: false,
-  theme: {
-    baseFontSize: defaultBaseFontSize,
-    spacing,
-    components: {
-      badge: components.badge,
-    },
-  },
 };
 
 export default Badge;
