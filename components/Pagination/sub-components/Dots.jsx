3cbcc82900505207a2cc8c4591ba22b2d575947b
Marcos Conceicao <marcos.conceicao@catho.com>
2024-10-18 10:47:45 -0300

    feat(Pagination.jsx): Migrated to css modules

--- components/Pagination/sub-components/Dots.jsx
+++ components/Pagination/sub-components/Dots.jsx
@@ -1,64 +1,17 @@
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import classNames from 'classnames';
+import styles from './Dots.module.css';
 
-import { colors, spacing } from '../../shared/theme';
+const Dots = ({ className, ...props }) => {
+  const wrapperClass = classNames(styles['wrapper-dot'], className);
+  const dotsClass = classNames(styles['pagination-dot']);
 
-const Wrapper = styled.div`
-  display: inline-block;
-  margin: ${({
-    theme: {
-      spacing: { xxsmall },
-    },
-  }) => `0 ${xxsmall}px`};
-  min-width: 22px;
-`;
-
-const Dot = styled.span`
-  position: relative;
-
-  ${({
-    theme: {
-      spacing: { xsmall, medium },
-      colors: {
-        neutral: { 300: neutral300 },
-      },
-    },
-  }) => `
-    &, &:after, &:before {
-      background-color: ${neutral300};
-      border-radius: 50%;
-      display: inline-block;
-      height: 5px;
-      width: 5px;
-    }
-
-    &:after, &:before {
-      content: '';
-      position: absolute;
-      top: 0;
-    }
-
-    &:after {
-      left: ${xsmall}px;
-    }
-
-    &:before {
-      left: ${medium}px;
-    }
-  `}
-`;
-
-const Dots = ({
-  theme = {
-    colors,
-    spacing,
-  },
-  ...props
-}) => (
-  <Wrapper theme={theme} {...props}>
-    <Dot theme={theme} />
-  </Wrapper>
-);
+  return (
+    <div className={wrapperClass} {...props}>
+      <span className={dotsClass} />
+    </div>
+  );
+};
 
 Dots.propTypes = {
   theme: PropTypes.shape({
